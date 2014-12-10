a409bc106cd885ef15e528a4d201341b369da50a
carlos calderon <cadermino@gmail.com>
2014-12-10 15:58:09 -0500

    ecohuerta

---
+++ ecohuerta/sites/all/themes/front/templates/menu-lateral-page-ecohuerta.php
@@ -0,0 +1,16 @@
+<ul class="nav nav-point">
+  <li class="<?php if(strcmp(request_uri(),'/acerca-de-ecohuerta')==0){print active;}?>">
+    <div class="nav-point-text-arrow">
+      <a class="nav-point-text"  href="/acerca-de-ecohuerta">Acerca de Ecohuerta</a>
+      <div class="nav-point-arrow nav-point-arrow-style"></div>
+    </div>
+    <a class="nav-point-link" href="/acerca-de-ecohuerta" rel="tipsy" original-title="Acerca de ecohuerta"></a>
+  </li>
+  <li class="<?php if(strcmp(request_uri(),'/recetas')==0){print active;}?>">
+    <div class="nav-point-text-arrow">
+      <a class="nav-point-text"  href="/recetas">Recetas</a>
+      <div class="nav-point-arrow nav-point-arrow-style"></div>
+    </div>
+    <a class="nav-point-link" href="/recetas" rel="tipsy" original-title="Recetas"></a>
+  </li>
+</ul>
