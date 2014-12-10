6691eaace7da6e86ca542eed039c1cb184040b92
carlos calderon <cadermino@gmail.com>
2014-12-10 15:39:02 -0500

    ecohuerta

---
+++ ecohuerta/sites/all/themes/front/templates/menu-lateral-page-ecohuerta.php
@@ -0,0 +1,16 @@
+<ul class="nav nav-point">
+    <li class="<?php if(strcmp(request_uri(),'/sobre-ecohuerta')==0){print active;}?>">
+      <div class="nav-point-text-arrow">
+        <a class="nav-point-text"  href="<?php print $base_path; ?>sobre-ecohuerta">Sobre Ecohuerta</a>
+        <div class="nav-point-arrow nav-point-arrow-style"></div>
+      </div>
+      <a class="nav-point-link" href="<?php print $base_path; ?>sobre-ecohuerta" rel="tipsy" original-title="Iniciativas UTEC"></a>
+    </li>
+    <li class="<?php if(strcmp(request_uri(),'/recetas')==0){print active;}?>">
+      <div class="nav-point-text-arrow">
+        <a class="nav-point-text"  href="<?php print $base_path; ?>recetas">Recetas</a>
+        <div class="nav-point-arrow nav-point-arrow-style"></div>
+      </div>
+      <a class="nav-point-link" href="<?php print $base_path; ?>recetas" rel="tipsy" original-title="Retos de la ingeniería"></a>
+    </li>
+</ul>
