35eee6ca9769fa49968e6e1f0e1bcf7a07b957db
carlos calderon <cadermino@gmail.com>
2014-12-11 14:44:26 -0500

    ecohuerta

--- ecohuerta/sites/all/themes/front/templates/menu-lateral-page-ecohuerta.php
+++ ecohuerta/sites/all/themes/front/templates/menu-lateral-page-ecohuerta.php
@@ -1,16 +1,16 @@
 <ul class="nav nav-point">
   <li class="<?php if(strcmp(request_uri(),'/ecohuerta-video-lechugas-hidroponicas')==0){print active;}?>">
     <div class="nav-point-text-arrow">
-      <a class="nav-point-text"  href="/ecohuerta-video-lechugas-hidroponicas">Sobre Ecohuerta</a>
+      <a class="nav-point-text"  href="<?php print $base_path; ?>ecohuerta-video-lechugas-hidroponicas">Sobre Ecohuerta</a>
       <div class="nav-point-arrow nav-point-arrow-style"></div>
     </div>
-    <a class="nav-point-link" href="/ecohuerta-video-lechugas-hidroponicas" rel="tipsy" original-title="Sobre ecohuerta"></a>
+    <a class="nav-point-link" href="<?php print $base_path; ?>ecohuerta-video-lechugas-hidroponicas" rel="tipsy" original-title="Sobre ecohuerta"></a>
   </li>
   <li class="<?php if(strcmp(request_uri(),'/ecohuerta-recetas')==0){print active;}?>">
     <div class="nav-point-text-arrow">
-      <a class="nav-point-text"  href="/ecohuerta-recetas">Recetas</a>
+      <a class="nav-point-text"  href="<?php print $base_path; ?>ecohuerta-recetas">Recetas</a>
       <div class="nav-point-arrow nav-point-arrow-style"></div>
     </div>
-    <a class="nav-point-link" href="/ecohuerta-recetas" rel="tipsy" original-title="Recetas"></a>
+    <a class="nav-point-link" href="<?php print $base_path; ?>ecohuerta-recetas" rel="tipsy" original-title="Recetas"></a>
   </li>
 </ul>
