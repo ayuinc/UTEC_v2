dc3b1f53ee0517ac677332821f9eaf0dbcc8dac3
carlos calderon <cadermino@gmail.com>
2014-12-16 17:31:19 -0500

    homepage

--- sites/all/themes/utec_theme/template.php
+++ sites/all/themes/utec_theme/template.php
@@ -1,8 +1,59 @@
 <?php
 
-/**
- * @file
- * template.php
- */
+	drupal_add_js(path_to_theme() . '/js/app/vendor/jquery-2.1.1.min.js', array(
+	  'scope' => 'footer',
+	  'group' => JS_THEME,
+	  'every_page' => TRUE,
+	  'weight' => 1,
+	));
 
-//drupal_add_js('sites/all/themes/utec_theme/js/app/lib/nav-test.js', array('type' => 'file', 'scope' => 'footer', 'weight' => 5));
+	drupal_add_js(path_to_theme() . '/js/app/vendor/TweenMax.min.js', array(
+	  'scope' => 'footer',
+	  'group' => JS_THEME,
+	  'every_page' => TRUE,
+	  'weight' => 2,
+	));
+
+	drupal_add_js(path_to_theme() . '/js/app/vendor/ScrollToPlugin.min.js', array(
+	  'scope' => 'footer',
+	  'group' => JS_THEME,
+	  'every_page' => TRUE,
+	  'weight' => 3,
+	));
+
+	drupal_add_js(path_to_theme() . '/js/app/vendor/jquery.scrollmagic.min.js', array(
+	  'scope' => 'footer',
+	  'group' => JS_THEME,
+	  'every_page' => TRUE,
+	  'weight' => 4,
+	));
+
+	drupal_add_js(path_to_theme() . '/js/app/lib/nav-control.js', array(
+	  'scope' => 'footer',
+	  'group' => JS_THEME,
+	  'every_page' => TRUE,
+	  'weight' => 5,
+	));
+
+	drupal_add_js(path_to_theme() . '/js/app/lib/layout-control.js', array(
+	  'scope' => 'footer',
+	  'group' => JS_THEME,
+	  'every_page' => TRUE,
+	  'weight' => 6,
+	));
+
+	drupal_add_js(path_to_theme() . '/js/app/lib/scroll-control.js', array(
+	  'scope' => 'footer',
+	  'group' => JS_THEME,
+	  'every_page' => TRUE,
+	  'weight' => 7,
+	));
+
+	drupal_add_js(path_to_theme() . '/js/app/lib/videojs.js', array(
+	  'scope' => 'footer',
+	  'group' => JS_THEME,
+	  'every_page' => TRUE,
+	  'weight' => 8,
+	));
+
+?>
