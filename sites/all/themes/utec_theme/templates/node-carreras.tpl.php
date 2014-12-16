3dc8ee89a8e31b473699404aa24b209df0e06b95
carlos calderon <cadermino@gmail.com>
2014-12-16 15:48:11 -0500

    homepage

---
+++ sites/all/themes/utec_theme/templates/node-carreras.tpl.php
@@ -0,0 +1,66 @@
+<div class="row  mv-21 ml-0">
+	<h2 class="lead medium"><span class="text-gray-darker">¿Listo para</span> <span class="text-gray">cambiar el mundo?</span></h2>
+	<h4 class="lead mb-42">Éstas son nuestras carreras:</h4>
+	<ul class="grid-list grid-list-5 pv-70 list-carreras text-center">
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-quimica"></i></div>
+				<h2>
+					<a href="#">
+						<span>Ingeniería</span>
+						<span>Química</span>
+						<span>Industrial</span>
+					</a>
+				</h2>
+			</div>
+		</li>
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-industrial"></i></div>
+				<h2>
+					<a href="carreras.html">
+						<span>Ingeniería</span>
+						<span>Industrial</span>
+						<span></span>
+					</a>
+				</h2>
+			</div>
+		</li>
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-energia"></i></div>
+				<h2>
+					<a href="#">
+						<span>Ingeniería</span>
+						<span>De la energía</span>
+						<span></span>
+					</a>
+				</h2>
+			</div>
+		</li>
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-electronica"></i></div>
+				<h2>
+					<a href="#">
+						<span>Ingeniería</span>
+						<span>Electrónica</span>
+						<span></span>
+					</a>
+				</h2>
+			</div>
+		</li>
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-mecanica"></i></div>
+				<h2>
+					<a href="#">
+						<span>Ingeniería</span>
+						<span>Mecánica</span>
+						<span></span>
+					</a>
+				</h2>
+			</div>
+		</li>
+	</ul>
+</div>
---
+++ sites/all/themes/utec_theme/templates/node-home.tpl.php
@@ -0,0 +1,171 @@
+<div class="row  mv-21 ml-0">
+	<h2 class="lead medium mb-0"><span class="text-gray-darker">¿Listo para</span> <span class="text-gray">cambiar el mundo?</span></h2>
+	<h4 class="lead mb-42 ml-7 mt-0">Éstas son nuestras carreras:</h4>
+	<ul class="grid-list grid-list-5 pt-21 list-carreras text-center mb-ch-70 mb-70">
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-quimica"></i></div>
+				<h2>
+					<a href="#">
+						<span>Ingeniería</span>
+						<span>Química</span>
+						<span>Industrial</span>
+					</a>
+				</h2>
+			</div>
+		</li>
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-industrial"></i></div>
+				<h2>
+					<a href="carreras.html">
+						<span>Ingeniería</span>
+						<span>Industrial</span>
+						<span></span>
+					</a>
+				</h2>
+			</div>
+		</li>
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-energia"></i></div>
+				<h2>
+					<a href="#">
+						<span>Ingeniería</span>
+						<span>De la energía</span>
+						<span></span>
+					</a>
+				</h2>
+			</div>
+		</li>
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-electronica"></i></div>
+				<h2>
+					<a href="#">
+						<span>Ingeniería</span>
+						<span>Electrónica</span>
+						<span></span>
+					</a>
+				</h2>
+			</div>
+		</li>
+		<li>
+			<div onclick="goTo()">
+				<div><i class="icon-ing-mecanica"></i></div>
+				<h2>
+					<a href="#">
+						<span>Ingeniería</span>
+						<span>Mecánica</span>
+						<span></span>
+					</a>
+				</h2>
+			</div>
+		</li>
+	</ul>
+</div>
+<h1 class="lead medium mb-0"><span class="text-gray-darker">En la UTEC ingeniería es</span> <span class="text-gray">bienestar.</span></h1>
+<h4 class="lead mt-0 mb-42">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+<div class="anchor-img-grid anchor-img-grid-1 grid-block row ml-0">
+  <div class="col-sm-6">
+    <a href="#" class="anchor-1">
+      <img src="assets/img/anchor-img-grid-sq.png" />
+      <div class="overlay-bg"></div>
+      <div class="overlay-content">
+        <div>
+          <span class="text-top mb-0">Texto superior</span>
+          <span class="text-bottom">Texto inferior</span>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div class="col-sm-6">
+    <div class="clearfix">
+      <a href="#" class="anchor-2">
+        <img src="assets/img/anchor-img-grid-rect.png" />
+        <div class="overlay-bg overlay-bg-rect"></div>
+        <div class="overlay-content">
+          <div>
+            <span class="text-top mb-0">Texto superior</span>
+            <span class="text-bottom">Texto inferior</span>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="clearfix">
+      <div class="col-sm-6">
+        <a href="#" class="anchor-3">
+          <img src="assets/img/anchor-img-grid-sq-1.png" />
+          <div class="overlay-bg"></div>
+          <div class="overlay-content">
+            <div>
+              <span class="text-top mb-0">Texto superior</span>
+              <span class="text-bottom">Texto inferior</span>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-sm-6">
+        <a href="#" class="anchor-4">
+          <img src="assets/img/anchor-img-grid-sq-2.png" />
+          <div class="overlay-bg"></div>
+          <div class="overlay-content">
+            <div>
+              <span class="text-top mb-0">Texto superior</span>
+              <span class="text-bottom">Texto inferior</span>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>          
+</div>
+<div class="pv-35" id="noticias">
+	<h2 class="lead medium mb-0"><span class="text-gray-darker">Noticias: Al día con la</span> <span class="text-gray">UTEC</span></h2>
+	<h4 class="lead mb-56 mt-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+	<div class="banner row mb-14 ml-0" data-href="//link">
+		<div class="col-sm-3 banner-pic" style="background-image: url(assets/img/img-sq.png);">
+			<div class="banner-label bg-gray-darker text-white"><span>Emprendimiento</span></div>
+		</div>
+		<div class="col-sm-9 banner-content">
+			<div>
+				<h2><a href="#">Lorem ipsum dolor sit amet.</a></h2>
+				<p class="lead">Recusandae vitae, temporibus repudiandae veritatis aspernatur nisi harum explicabo possimus illo velit.</p>
+			</div>
+		</div>
+	</div>
+	<div class="banner row ml-0 mb-14" data-href="//link">
+		<div class="col-sm-9 banner-content">
+			<div>
+				<h2><a href="#">Lorem ipsum dolor sit amet.</a></h2>
+				<p class="lead">Recusandae vitae, temporibus repudiandae veritatis aspernatur nisi harum explicabo possimus illo velit.</p>
+			</div>
+		</div>
+		<div class="col-sm-3 banner-pic" style="background-image: url(assets/img/img-sq.png);">
+			<div class="banner-label bg-gray-darker text-white"><span>Innovación</span></div>
+		</div>
+	</div>
+	<div>
+		<button class="btn btn-lg btn-primary see-more uppercase">Ver más noticias</button>
+	</div>
+</div>
+<div class="pv-21">
+	<h2 class="lead medium mb-0"><span class="text-gray-darker">Blog</span> <span class="text-gray">UTEC</span></h2>
+	<h4 class="lead mt-0 mb-56">Lorem ipsum sit amet, consectetur adipisicing elit.</h4>
+	<div class="banner row ml-0" data-href="//link">
+		<div class="col-sm-3 banner-pic" style="background-image: url(assets/img/img-sq.png);"></div>
+		<div class="col-sm-9 banner-content">
+			<div>
+  			<div class="user-card user-card-round">
+  				<div class="user-pic"><img src="assets/img/user.jpg" alt=""></div>
+  				<div class="user-info"><h4>Alberto Iturralde</h4><small>Ingeniero Mecánico</small></div>
+  			</div>
+  			<h2><a href="#">Lorem ipsum dolor sit amet.</a></h2>
+				<p class="lead">Recusandae vitae, temporibus repudiandae veritatis aspernatur nisi harum explicabo possimus illo velit.</p>
+				<div class="text-right pv-14">
+					<button class="see-more btn btn-lg btn-primary uppercase">Leer más</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
