9b4cbbd7563e93b8ab275e21c7856448ee2ad1d8
carlos calderon <cadermino@gmail.com>
2014-12-17 17:34:52 -0500

    homepage

--- sites/all/themes/utec_theme/node--en_la_utec.tpl.php
+++ sites/all/themes/utec_theme/node--en_la_utec.tpl.php
@@ -1,63 +1,61 @@
 <?php 
 
-	// $theme_path = drupal_get_path('theme', 'utec_theme');
-	// $video_name = $node->field_video['und']['0']['filename'];
-	
-	// $wrapper = entity_metadata_wrapper('node', $node);
- //  $formtype = field_get_items('node', $node, 'field_bloque_derecho');
-
   $fields = entity_load('field_collection_item');
-  // $track_2 = entity_load('field_collection_item', array($node->field_track[LANGUAGE_NONE][1]['value']));
-// $field_bloque_derecho
-// $field_bloque_izquierdo
-// $field_bloque_derecho_inferior
-// $field_bloque_derecho_inferior_de
 
-  $value = $fields[1]->field_texto_superior_1;
-	die(print_r($value));
+  $derecho_superior_txt1 = $fields[1]->field_texto_superior_1['und'][0]['value'];
+  $derecho_superior_txt2 = $fields[1]->field_texto_superior_2['und'][0]['value'];
+  $derecho_superior_link = $fields[1]->field_link['und'][0]['value'];
+  $derecho_superior_img = $fields[1]->field_imagen['und'][0]['filename'];
 
-  $item = field_collection_field_get_entity(1);
-	// die(print_r($value));
+  $derecho_inferior_izq_txt1 = $fields[2]->field_texto_superior_1['und'][0]['value'];
+  $derecho_inferior_izq_txt2 = $fields[2]->field_texto_superior_2['und'][0]['value'];
+  $derecho_inferior_izq_link = $fields[2]->field_link['und'][0]['value'];
+  $derecho_inferior_izq_img  = $fields[2]->field_imagen['und'][0]['filename'];
 
-  foreach($formtype as $itemid) { 
-    $item = field_collection_field_get_entity($itemid);
-	  print $item->field_reference_title['und'][0]['safe_value'];
-	  print $item->field_reference_location['und'][0]['safe_value'];
-	}
+  $derecho_inferior_der_txt1 = $fields[3]->field_texto_superior_1['und'][0]['value'];
+  $derecho_inferior_der_txt2 = $fields[3]->field_texto_superior_2['und'][0]['value'];
+  $derecho_inferior_der_link = $fields[3]->field_link['und'][0]['value'];
+  $derecho_inferior_der_img  = $fields[3]->field_imagen['und'][0]['filename'];
 
+  $izquierdo_txt1 = $fields[4]->field_texto_superior_1['und'][0]['value'];
+  $izquierdo_txt2 = $fields[4]->field_texto_superior_2['und'][0]['value'];
+  $izquierdo_link = $fields[4]->field_link['und'][0]['value'];
+  $izquierdo_img  = $fields[4]->field_imagen['und'][0]['filename'];
+
+  die(print_r($derecho_superior_img));
 
 ?>
 <h1 class="lead medium mb-0"><span class="text-gray-darker">En la UTEC ingeniería es</span> <span class="text-gray">bienestar.</span></h1>
 	<h4 class="lead mt-0 mb-42">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
 	<div class="anchor-img-grid anchor-img-grid-1 grid-block row ml-0">
-    <div class="col-sm-6">
-      <a href="#" class="anchor-1">
-        <img src="assets/img/anchor-img-grid-sq.png" />
+		<div class="col-sm-6">
+      <a href="<?php print $base_path; ?>/<?php print $izquierdo_link; ?>" class="anchor-1">
+        <img src="sites/default/files/<?php print $izquierdo_img; ?>" />
         <div class="overlay-bg"></div>
         <div class="overlay-content">
           <div>
-            <span class="text-top mb-0">Texto superior</span>
-            <span class="text-bottom">Texto inferior</span>
+            <span class="text-top mb-0"><?php print $izquierdo_txt1; ?></span>
+            <span class="text-bottom"><?php print $izquierdo_txt2; ?></span>
           </div>
         </div>
       </a>
     </div>
     <div class="col-sm-6">
       <div class="clearfix">
-        <a href="#" class="anchor-2">
-          <img src="assets/img/anchor-img-grid-rect.png" />
+        <a href="<?php print $base_path; ?>/<?php print $derecho_superior_link; ?>" class="anchor-2">
+          <img src="sites/default/files/<?php print $derecho_superior_img; ?>" />
           <div class="overlay-bg overlay-bg-rect"></div>
           <div class="overlay-content">
             <div>
-              <span class="text-top mb-0">Texto superior</span>
-              <span class="text-bottom">Texto inferior</span>
+              <span class="text-top mb-0"><?php print $derecho_superior_txt1; ?></span>
+              <span class="text-bottom"><?php print $derecho_superior_txt2; ?></span>
             </div>
           </div>
         </a>
       </div>
       <div class="clearfix">
         <div class="col-sm-6">
-          <a href="#" class="anchor-3">
+          <a href="<?php print $base_path; ?>/<?php print $derecho_superior_link; ?>" class="anchor-3">
             <img src="assets/img/anchor-img-grid-sq-1.png" />
             <div class="overlay-bg"></div>
             <div class="overlay-content">
