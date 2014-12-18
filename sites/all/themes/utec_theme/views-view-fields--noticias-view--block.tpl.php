f0e5ec28115be80d76d4cf98280e93095effb56d
carlos calderon <cadermino@gmail.com>
2014-12-18 06:40:27 -0500

    homepage

--- sites/all/themes/utec_theme/views-view-fields--noticias-view--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--noticias-view--block.tpl.php
@@ -1,7 +1,7 @@
 <?php //die(print_r($fields)); ?>
 <?php //kpr($fields); ?>
 
-<!-- <div class="pv-35" id="noticias">
+<div class="pv-35" id="noticias">
 	<h2 class="lead medium mb-0"><span class="text-gray-darker">Noticias: Al día con la</span> <span class="text-gray">UTEC</span></h2>
 	<h4 class="lead mb-56 mt-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
 
@@ -32,4 +32,4 @@
 	<div>
 		<button class="btn btn-lg btn-primary see-more uppercase">Ver más noticias</button>
 	</div>
-</div> -->
+</div>
