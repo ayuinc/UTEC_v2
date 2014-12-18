65181fa80f26e00213c437b370484e71ea78b4ce
carlos calderon <cadermino@gmail.com>
2014-12-18 06:52:10 -0500

    homepage

--- sites/all/themes/utec_theme/views-view.tpl.php
+++ sites/all/themes/utec_theme/views-view.tpl.php
@@ -27,7 +27,7 @@
  * @ingroup views_templates
  */
 ?>
-<?php kpr($rows) ?>
+<?php print_r($rows); ?>
 <div class="pv-35" id="noticias">
   <h2 class="lead medium mb-0"><span class="text-gray-darker">Noticias: Al día con la</span> <span class="text-gray">UTEC</span></h2>
   <h4 class="lead mb-56 mt-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
