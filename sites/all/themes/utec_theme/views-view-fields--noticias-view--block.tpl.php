e253a2716d11933b439212f562141cc9065423cd
carlos calderon <cadermino@gmail.com>
2014-12-18 08:15:54 -0500

    homepage

--- sites/all/themes/utec_theme/views-view-fields--noticias-view--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--noticias-view--block.tpl.php
@@ -5,10 +5,8 @@
 	$body = $fields['body']->content;
 	$image = $fields['field_imagen']->content;
 	$category = $fields['field_categor_a']->content;
-	$count = 0;
 ?>
 <?php if ($count == 0) :?>
-	<?php $count = 1; ?>
 <div class="banner row mb-14 ml-0" data-href="//link">
 	<div class="col-sm-3 banner-pic" style="background-image: url(<?php print $image ?>);">
 		<div class="banner-label bg-gray-darker text-white"><span><?php print $category ?></span></div>
--- sites/all/themes/utec_theme/views-view-unformatted--noticias-view--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--noticias-view--block.tpl.php
@@ -11,9 +11,11 @@
 	<h2 class="lead medium mb-0"><span class="text-gray-darker">Noticias: Al día con la</span> <span class="text-gray">UTEC</span></h2>
 	<h4 class="lead mb-56 mt-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
     <?php foreach ($rows as $id => $row): ?>
-		  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-		    <?php print $row; ?>
-		  </div>
+		    <?php
+		    	$count = 0; 
+			    print $row; 
+			    $count = 1;
+		    ?>
 		<?php endforeach; ?>
 	<div>
 		<button class="btn btn-lg btn-primary see-more uppercase">Ver más noticias</button>
