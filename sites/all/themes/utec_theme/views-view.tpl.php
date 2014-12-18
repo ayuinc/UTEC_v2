d677dd7b069a3b82020e49236d64faf368ab4ac9
carlos calderon <cadermino@gmail.com>
2014-12-18 06:55:39 -0500

    homepage

--- sites/all/themes/utec_theme/views-view.tpl.php
+++ sites/all/themes/utec_theme/views-view.tpl.php
@@ -27,36 +27,5 @@
  * @ingroup views_templates
  */
 ?>
-<?php print_r($rows); ?>
-<div class="pv-35" id="noticias">
-  <h2 class="lead medium mb-0"><span class="text-gray-darker">Noticias: Al día con la</span> <span class="text-gray">UTEC</span></h2>
-  <h4 class="lead mb-56 mt-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+<?php print $rows; ?>
 
-  <div class="banner row mb-14 ml-0" data-href="//link">
-    <div class="col-sm-3 banner-pic" style="background-image: url(assets/img/img-sq.png);">
-      <div class="banner-label bg-gray-darker text-white"><span>Emprendimiento</span></div>
-    </div>
-    <div class="col-sm-9 banner-content">
-      <div>
-        <h2><a href="#">Lorem ipsum dolor sit amet.</a></h2>
-        <p class="lead">Recusandae vitae, temporibus repudiandae veritatis aspernatur nisi harum explicabo possimus illo velit.</p>
-      </div>
-    </div>
-  </div>
-  
-  <div class="banner row ml-0 mb-14" data-href="//link">
-    <div class="col-sm-9 banner-content">
-      <div>
-        <h2><a href="#">Lorem ipsum dolor sit amet.</a></h2>
-        <p class="lead">Recusandae vitae, temporibus repudiandae veritatis aspernatur nisi harum explicabo possimus illo velit.</p>
-      </div>
-    </div>
-    <div class="col-sm-3 banner-pic" style="background-image: url(assets/img/img-sq.png);">
-      <div class="banner-label bg-gray-darker text-white"><span>Innovación</span></div>
-    </div>
-  </div>
-
-  <div>
-    <button class="btn btn-lg btn-primary see-more uppercase">Ver más noticias</button>
-  </div>
-</div>
