384be0543994967f69ebc69a55337157c58182fe
carlos calderon <cadermino@gmail.com>
2014-12-18 07:44:28 -0500

    homepage

--- sites/all/themes/utec_theme/views-view-fields--noticias-view--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--noticias-view--block.tpl.php
@@ -1,35 +1,36 @@
 <?php //die(print_r($fields)); ?>
-<?php kpr($fields); ?>
-
-<div class="pv-35" id="noticias">
-	<h2 class="lead medium mb-0"><span class="text-gray-darker">Noticias: Al día con la</span> <span class="text-gray">UTEC</span></h2>
-	<h4 class="lead mb-56 mt-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
-
-	<div class="banner row mb-14 ml-0" data-href="//link">
-		<div class="col-sm-3 banner-pic" style="background-image: url(assets/img/img-sq.png);">
-			<div class="banner-label bg-gray-darker text-white"><span>Emprendimiento</span></div>
-		</div>
-		<div class="col-sm-9 banner-content">
-			<div>
-				<h2><a href="#">Lorem ipsum dolor sit amet.</a></h2>
-				<p class="lead">Recusandae vitae, temporibus repudiandae veritatis aspernatur nisi harum explicabo possimus illo velit.</p>
-			</div>
-		</div>
+<?php 
+	kpr($fields); 
+	$title = $fields['title']->content;
+	$body = $fields['body']->content;
+	$image = $fields['field_imagen']->content;
+	$category = $fields['field_categor_a']->content;
+	$count = 0;
+?>
+<?php if ($count == 0) :?>
+	<?php $count == 1; ?>
+<div class="banner row mb-14 ml-0" data-href="//link">
+	<div class="col-sm-3 banner-pic" style="background-image: url(sites/default/files/noticias/<?php print $image ?>);">
+		<div class="banner-label bg-gray-darker text-white"><span><?php print $category ?></span></div>
 	</div>
-	
-	<div class="banner row ml-0 mb-14" data-href="//link">
-		<div class="col-sm-9 banner-content">
-			<div>
-				<h2><a href="#">Lorem ipsum dolor sit amet.</a></h2>
-				<p class="lead">Recusandae vitae, temporibus repudiandae veritatis aspernatur nisi harum explicabo possimus illo velit.</p>
-			</div>
+	<div class="col-sm-9 banner-content">
+		<div>
+			<h2><a href="#"><?php print $title ?></a></h2>
+			<p class="lead"><?php print $body ?></p>
 		</div>
-		<div class="col-sm-3 banner-pic" style="background-image: url(assets/img/img-sq.png);">
-			<div class="banner-label bg-gray-darker text-white"><span>Innovación</span></div>
-		</div>
-	</div>
-
-	<div>
-		<button class="btn btn-lg btn-primary see-more uppercase">Ver más noticias</button>
 	</div>
 </div>
+<?php endif; ?>
+<?php if ($count != 0) :?>
+<div class="banner row ml-0 mb-14" data-href="//link">
+	<div class="col-sm-9 banner-content">
+		<div>
+			<h2><a href="#"><?php print $title ?></a></h2>
+			<p class="lead"><?php print $body ?></p>
+		</div>
+	</div>
+	<div class="col-sm-3 banner-pic" style="background-image: url(assets/img/img-sq.png);">
+		<div class="banner-label bg-gray-darker text-white"><span><?php print $category ?></span></div>
+	</div>
+</div>
+<?php endif; ?>
