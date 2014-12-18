8b27fa8e324ef418fe6745bb33156ba903b488ed
carlos calderon <cadermino@gmail.com>
2014-12-18 10:47:18 -0500

    homepage

--- sites/all/themes/utec_theme/views-view-unformatted--noticias-view--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--noticias-view--block.tpl.php
@@ -10,9 +10,9 @@
 <div class="pv-35" id="noticias">
 	<h2 class="lead medium mb-0"><span class="text-gray-darker">Noticias: Al día con la</span> <span class="text-gray">UTEC</span></h2>
 	<h4 class="lead mb-56 mt-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+		<?php global $count; ?>
     <?php foreach ($rows as $id => $row): ?>
 		    <?php
-		    global $count;
 		    	$count = 0;
 			    print $row;
 			    $count = 1;
