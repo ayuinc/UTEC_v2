f8f65e5bd3e988d854ea0a5da737289f3512061e
carlos calderon <cadermino@gmail.com>
2014-12-18 14:19:48 -0500

    homepage

--- sites/all/themes/utec_theme/views-view-fields--blog--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--blog--block.tpl.php
@@ -5,23 +5,24 @@
 	$image = $fields['field_imagen']->content;
 	$category = $fields['field_categor_a']->content;
 	$texto_corto = $fields['field_texto_corto']->content;
+	$path = $fields['path']->content;
 	$user_id = $fields['uid']->content;
 
 	global $user;
 	$user_fields = user_load($user_id);
-	print_r($user_fields);
-	// $username = $user->name;
+	$user_name = $user->name;
+	$user_image = $user->picture;
 ?>
 <div class="pv-21">
 	<h2 class="lead medium mb-0"><span class="text-gray-darker">Blog</span> <span class="text-gray">UTEC</span></h2>
 	<h4 class="lead mt-0 mb-56">Lorem ipsum sit amet, consectetur adipisicing elit.</h4>
-	<div class="banner row ml-0" data-href="//link">
-		<div class="col-sm-3 banner-pic" style="background-image: url(assets/img/img-sq.png);"></div>
+	<div class="banner row ml-0" data-href="<?php print $path; ?>">
+		<div class="col-sm-3 banner-pic" style="background-image: url(<?php print $image; ?>);"></div>
 		<div class="col-sm-9 banner-content">
 			<div>
   			<div class="user-card user-card-round">
-  				<div class="user-pic"><img src="<?php print $image; ?>" alt=""></div>
-  				<div class="user-info"><h4>Alberto Iturralde</h4><small>Ingeniero Mecánico</small></div>
+  				<div class="user-pic"><img src="<?php print $user_image; ?>" alt=""></div>
+  				<div class="user-info"><h4><?php print $user_name; ?></h4><small>Ingeniero Mecánico</small></div>
   			</div>
   			<h2><a href="#"><?php print $title; ?></a></h2>
 				<p class="lead"><?php print $texto_corto; ?></p>
--- sites/all/themes/utec_theme/views-view-fields--noticias-view--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--noticias-view--block.tpl.php
@@ -6,27 +6,28 @@
 	$image = $fields['field_imagen']->content;
 	$category = $fields['field_categor_a']->content;
 	$texto_corto = $fields['field_texto_corto']->content;
+	$path = $fields['path']->content;
 	global $count;
 ?>
 <?php //die(var_dump($count)); ?>
 <?php if ($count == 0) :?>
-<div class="banner row mb-14 ml-0" data-href="//link">
+<div class="banner row mb-14 ml-0" data-href="<?php print $path; ?>">
 	<div class="col-sm-3 banner-pic" style="background-image: url(<?php print $image ?>);">
 		<div class="banner-label bg-gray-darker text-white"><span><?php print $category ?></span></div>
 	</div>
 	<div class="col-sm-9 banner-content">
 		<div>
-			<h2><a href="#"><?php print $title ?></a></h2>
+			<h2><a href="<?php print $path; ?>"><?php print $title ?></a></h2>
 			<p class="lead"><?php print $texto_corto ?></p>
 		</div>
 	</div>
 </div>
 <?php endif; ?>
 <?php if ($count == 1) :?>
-<div class="banner row ml-0 mb-14" data-href="//link">
+<div class="banner row ml-0 mb-14" data-href="<?php print $path; ?>">
 	<div class="col-sm-9 banner-content">
 		<div>
-			<h2><a href="#"><?php print $title ?></a></h2>
+			<h2><a href="<?php print $path; ?>"><?php print $title ?></a></h2>
 			<p class="lead"><?php print $texto_corto ?></p>
 		</div>
 	</div>
