b5dc12a9e39124663562571aa3d58fc7074806c8
carlos calderon <cadermino@gmail.com>
2014-12-22 16:41:26 -0500

    ecohuerta

--- ecohuerta/sites/all/themes/front/templates/page--node--264.tpl.php
+++ ecohuerta/sites/all/themes/front/templates/page--node--264.tpl.php
@@ -215,6 +215,7 @@
                 </h3>
               </div>
               <div class="project-desc">
+                <?php print_r($ingredientes); ?>
                 <?php foreach ($ingredientes as $key => $value) : ?>
                   <p>
                    <?php print($value[$key]['value']); ?>
@@ -237,6 +238,7 @@
                 </p> -->
               </div>
               <div class="prep">
+                <?php print_r($preparacion); ?>
                 <?php foreach ($preparacion as $key => $value) : ?>
                   <?php print($value[$key]['value']); ?>
                 <?php endforeach; ?>
