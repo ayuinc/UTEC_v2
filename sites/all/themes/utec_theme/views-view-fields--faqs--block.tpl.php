d8bb97fcdda804d89a727e895163db51e57e994e
carlos calderon <cadermino@gmail.com>
2014-12-23 09:18:17 -0500

    views wrappers

--- sites/all/themes/utec_theme/views-view-fields--faqs--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--faqs--block.tpl.php
@@ -13,11 +13,9 @@
 	// kpr($fields); 
 	$title = $fields['title']->content;
 	$field_descripcion_corta_pregunta = $fields['field_descripcion_corta_pregunta']->content;
-	global $count;
 ?>
-<?php //die(var_dump($count)); ?>
 
-		<li>
-			<h5><?php print $title ?></h5>
-			<p><?php print $field_descripcion_corta_pregunta ?></p>
-		</li>
+<li>
+	<h5><?php print $title ?></h5>
+	<p><?php print $field_descripcion_corta_pregunta ?></p>
+</li>
--- sites/all/themes/utec_theme/views-view-unformatted--faqs--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--faqs--block.tpl.php
@@ -11,13 +11,13 @@
 	<h1 class="light">Preguntas frecuentes</h1>
 	<div class="separator-gray separator-lg"></div>
 	<div class="list-custom pv-42 lead-ch p-ch-gray">
-	<ul>
-		<?php //global $count; ?>
-    <?php foreach ($rows as $id => $row): ?>
-		    <?php
-			    print $row;
-		    ?>
-		<?php endforeach; ?>
-	</ul>
+		<ul>
+			<?php //global $count; ?>
+	    <?php foreach ($rows as $id => $row): ?>
+			    <?php
+				    print $row;
+			    ?>
+			<?php endforeach; ?>
+		</ul>
 	</div>
 </div>
