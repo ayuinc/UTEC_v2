c6381eedcba17a8d48029b5c8d92eb60f5b7d448
Mercedes Zubieta <mercedes.zubieta@gmail.com>
2014-12-23 18:30:27 -0500

    agregando titulo a la receta

--- ecohuerta/sites/all/themes/front/templates/page--node--264.tpl.php
+++ ecohuerta/sites/all/themes/front/templates/page--node--264.tpl.php
@@ -207,8 +207,8 @@
 
         <div class="desc-container">        
           <div class="desc-content"> 
-            <div style="position: relative; bottom: 53px; color: white;">
-              <h2> <?php print $nombre_receta ?></h2>
+            <div style="position: relative; bottom: 53px; color: white; font-size: 22px;">
+             <?php print $nombre_receta ?>
             </div>
             <img class="project-close"  src="<?php print $path; ?>/images/icons/close_light_blue.jpg" alt="project-close" />
             <div class="recetas-cont">
