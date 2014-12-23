6cbeba96bd671fa20218aac38182e64c5c5a3fd8
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-23 15:11:39 -0500

    Seccion Generica

--- sites/all/themes/utec_theme/node--generico.tpl.php
+++ sites/all/themes/utec_theme/node--generico.tpl.php
@@ -2,21 +2,17 @@
 // $video_name = $node->field_video['und']['0']['filename'];
 
 $titulo = $node->title;   
-$field_descripcion_corta_alto = $node->field_descripcion_corta_alto['und']['0']['value'];
-$field_como_postular_alto = $node->field_como_postular_alto['und']['0']['value'];
-$field_opcion_1_alto = $node->field_opcion_1_alto['und']['0']['value'];
-$field_opcion_2_alto = $node->field_opcion_1_alto['und']['0']['value'];
-$field_opcion_3_alto = $node->field_opcion_1_alto['und']['0']['value'];
-$field_opcion_4_alto = $node->field_opcion_1_alto['und']['0']['value'];
-$field_opcion_5_alto = $node->field_opcion_1_alto['und']['0']['value'];
-$field_opcion_6_alto = $node->field_opcion_1_alto['und']['0']['value'];
-$field_costa_alto = $node->field_costa_alto['und']['0']['value'];
-$field_temario_alto = $node->field_temario_alto['und']['0']['value'];
+$field_descripcion_corta_generica = $node->field_descripcion_corta_generica['und']['0']['value'];
+$field_contenido_generica = $node->field_contenido_generica['und']['0']['value'];
+
 
 ?>
 <div class="container-sm">
 	<h1 class="light">Charlas informativas</h1>
 	<div class="separator-gray separator-lg"></div>
+	<div>
+		<?php print $field_contenido_generica ?>
+	</div>
 </div>
 
 
