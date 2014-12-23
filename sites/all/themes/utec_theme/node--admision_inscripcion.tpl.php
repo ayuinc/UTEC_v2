c47b0562ef34b50bc1da7b56df0336dc7a1a7e26
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-23 17:40:07 -0500

    Seccion inscripcion

---
+++ sites/all/themes/utec_theme/node--admision_inscripcion.tpl.php
@@ -0,0 +1,56 @@
+<?php 
+// $video_name = $node->field_video['und']['0']['filename'];
+
+$titulo = $node->title;   
+$field_descripcion_corta_generica = $node->field_descripcion_corta_generica['und']['0']['value'];
+$field_contenido_generica = $node->field_contenido_generica['und']['0']['value'];
+
+
+?>
+<div class="container-sm">
+	<h1 class="light">Inscripción</h1>
+	<div class="separator-gray separator-lg"></div>
+	<div class="list-custom-left normalize-text lead-ch mb-ch-21">
+		<ul>
+			<li class="bg-primary text-white uppercase text-center p-21">
+				Pago
+			</li>
+			<li class="pl-21">									
+				<p><strong>Al contado:</strong> S/.3,500</p>
+				<p><strong>Financiado:</strong> 5 cuotas de S/.800</p>
+			</li>
+		</ul>
+		<ul>
+			<li class="bg-gray text-white uppercase text-center p-21">
+				Forma de pago
+			</li>
+			<li class="pl-21">									
+				<p>Depósito en el Banco del Crédito BCP</p>
+				<p><strong>Cuenta en moneda nacional:</strong> 193-1976235-0-68</p>
+			</li>
+		</ul>
+	</div>
+	<div class="list-custom pv-42">
+		<ul>
+			<li>Presentar voucher de depósito</li>
+			<li>Copia del documento</li>
+			<li>Dos fotografías</li>
+			<li>Adicionalmente, lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam mollitia repudiandae dolore tempora doloremque quasi facilis inventore doloribus minima dolores sapiente asperiores, est laudantium fugit, recusandae ipsa, cum ratione architecto.</li>
+			<li>El ingreso a UTEC por esta lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis necessitatibus nulla asperiores eum? Vel totam praesentium atque, facilis sunt, minus sed, quasi nesciunt inventore voluptatum minima accusantium obcaecati maiores! Iure.</li>
+		</ul>
+		<a href="#" class="btn btn-primary see-more btn-lg uppercase">Completar el formulario</a>
+	</div>
+	<div class="lead-ch p-ch-gray li-ch-gray">
+		<h3 class="thin">Condiciones para el ingreso</h3>
+		<p>Para ingresar bajo esta modalidad se debe cumplir obligatoriamente con los siguientes requisitos:</p>
+		<div class="list-custom">
+			<ul>
+				<li>Asistencia mínima del 85% durante todo el ciclo académico</li>
+				<li>Obtener, como promedio general de todos los cursos, nota final aprobatoria</li>
+				<li>Obtener una vacante de las dispuestas para esta modalidad de ingreso</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
