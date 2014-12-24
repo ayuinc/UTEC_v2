ad57534de0085098ebb8847ef421c3828545bb23
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-24 13:22:26 -0500

    Cambios Navegacion y footer

--- sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
+++ sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
@@ -1,6 +1,6 @@
 <div class="row  mv-70 ml-0">
 	<h2 class="lead medium mb-0"><span class="text-gray-darker">¿Listo para</span> <span class="text-gray">cambiar el mundo?</span></h2>
-	<h4 class="lead mb-0 mt-0">Éstas son nuestras carreras:</h4>
+	<h4 class="lead mb-35 mt-0">Éstas son nuestras carreras:</h4>
 	<ul class="grid-list grid-list-5 list-carreras text-center">
 		<li>
 			<div onclick="goTo()">
