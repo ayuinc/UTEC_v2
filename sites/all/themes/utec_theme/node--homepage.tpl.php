2eb106720d7599da325d3cd03cb71bad2d5dd994
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-24 13:25:41 -0500

    Cambios Navegacion y footer

--- sites/all/themes/utec_theme/node--homepage.tpl.php
+++ sites/all/themes/utec_theme/node--homepage.tpl.php
@@ -4,7 +4,7 @@ $video_name = $node->field_video['und']['0']['filename'];
 
 ?>
 <!-- HERO-UNIT -->
-<video
+<!--<video
 	id="videocover"
 	autoplay
 	loop
@@ -12,12 +12,12 @@ $video_name = $node->field_video['und']['0']['filename'];
   controls
   preload="auto">
   <!-- <source src="assets/video/video_UTEC-pedal.webm" type='video/webm' /> -->
-  <source src="sites/default/files/<?php echo $video_name ?>" type='video/mp4' />
+  <!--<source src="sites/default/files/<?php echo $video_name ?>" type='video/mp4' />-->
   <!-- <source src="assets/video/video_UTEC-pedal.ogg" type='video/ogg' /> -->
-  <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
-</video>
+  <!--<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+</video>-->
 <!-- <div class="video-placeholder" style="background: url(assets/img/project-bg-1.jpg) no-repeat center top;"></div> -->
-<div class="overlay flex-middle">
+<!--<div class="overlay flex-middle">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-6">
@@ -28,5 +28,43 @@ $video_name = $node->field_video['und']['0']['filename'];
 </div>
 <a class="scroll-down scroll-down-sq size lg" href="#sec-1" rel="nofollow">
 	<i class="icon-arrows-down bg-white-op text-gray"></i>
-</a>
+</a>-->
 <!-- END:HERO-UNIT --> 
+
+
+				<div class="hero-unit video-bg"> 
+					<div class="bg-img-block bg-img-block-lg flex-middle" style="background-image: url(assets/img/bg-header-3.jpg);">
+						<div class="container">
+							<div class="row">
+								<div class="col-sm-6">
+									<h2 class="thin text-white lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit repellat dolorem alias minus accusamus.</h2>
+								</div>
+							</div>
+						</div>
+						<a class="scroll-down scroll-down-sq size lg" href="#sec-1" rel="nofollow">
+							<i class="icon-arrows-down bg-primary text-white"></i>
+						</a>
+					</div>
+					<video
+						id="videocover"
+						autoplay
+						loop
+	          class="video-js videocover vjs-default-skin"
+	          controls
+	          preload="auto">
+	          <source src="assets/video/video_UTEC-pedal.webm" type='video/webm' />
+	          <source src="sites/default/files/<?php echo $video_name ?>" type='video/mp4' />
+	          <source src="assets/video/video_UTEC-pedal.ogg" type='video/ogg' />
+	          <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+	        </video>
+	        <!-- <div class="video-placeholder" style="background: url(assets/img/project-bg-1.jpg) no-repeat center top;"></div> -->
+	        <div class="overlay flex-middle">
+	        	<div class="container">
+							<div class="row">
+								<div class="col-sm-6">
+									<h2 class="thin text-white lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit repellat dolorem alias minus accusamus.</h2>
+								</div>
+							</div>
+						</div>
+	        </div>
+				</div>
