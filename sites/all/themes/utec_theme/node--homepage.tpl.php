b503e87a1fd057c2f3abfdd654bcf5cf4ea97573
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-24 13:28:14 -0500

    Cambios Navegacion y footer

--- sites/all/themes/utec_theme/node--homepage.tpl.php
+++ sites/all/themes/utec_theme/node--homepage.tpl.php
@@ -12,7 +12,7 @@ $video_name = $node->field_video['und']['0']['filename'];
   controls
   preload="auto">
   <!-- <source src="assets/video/video_UTEC-pedal.webm" type='video/webm' /> -->
-  <!--<source src="sites/default/files/<?php echo $video_name ?>" type='video/mp4' />-->
+  <source src="sites/default/files/<?php echo $video_name ?>" type='video/mp4' />-->
   <!-- <source src="assets/video/video_UTEC-pedal.ogg" type='video/ogg' /> -->
   <!--<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 </video>-->
@@ -57,7 +57,7 @@ $video_name = $node->field_video['und']['0']['filename'];
 	          <source src="assets/video/video_UTEC-pedal.ogg" type='video/ogg' />
 	          <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 	        </video>
-	        <!-- <div class="video-placeholder" style="background: url(assets/img/project-bg-1.jpg) no-repeat center top;"></div> -->
+	        <div class="video-placeholder" style="background: url(assets/img/project-bg-1.jpg) no-repeat center top;"></div>
 	        <div class="overlay flex-middle">
 	        	<div class="container">
 							<div class="row">
