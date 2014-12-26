e20b0d1db10b23f71db68909bc8143668562b0cc
carlos calderon <cadermino@gmail.com>
2014-12-26 12:24:40 -0500

    carreras utec

--- sites/all/themes/utec_theme/node--carreras_home.tpl.php
+++ sites/all/themes/utec_theme/node--carreras_home.tpl.php
@@ -12,6 +12,9 @@ $menciones_titulo = $node->field_menciones_titulo  ['und']['0']['value'];
 $menciones_txt = $node->field_menciones_body['und']['0']['value'];
 $menciones_link = $node->field_menciones_link  ['und']['0']['value'];
 $profesor_titulo = $node->field_profesor['und']['0']['value'];
+
+print_r($node);
+
 $profesor_txt = $node->field_profesor_texto['und']['0']['value'];
 $profesor_link = $node->field_profesor_link ['und']['0']['value'];
 $perfil_txt = $node->field_perfil_egresado_texto['und']['0']['value'];
