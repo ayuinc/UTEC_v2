b73006fcad1968ca8b1fe6b0527cea95feccee7f
carlos calderon <cadermino@gmail.com>
2014-12-26 15:27:48 -0500

    carreras utec

--- sites/all/themes/utec_theme/node--carreras_home.tpl.php
+++ sites/all/themes/utec_theme/node--carreras_home.tpl.php
@@ -101,7 +101,7 @@ $perfil_link = $node->field_perfil_egresado_link['und']['0']['value'];
       <?php $count = 0; ?>  
       <?php foreach ($node->field_profesor['und'] as $key => $value) :?>
         <?php   
-          die(print_r($value);
+          die(print_r($node->field_profesor['und']);
           $name = $value['und'][$count]['entity']->name; 
           $pic = $value['und'][$count]['entity']->picture; 
           $desc = $value['und'][$count]['entity']->field_descripci_n['und'][0]['value']; 
