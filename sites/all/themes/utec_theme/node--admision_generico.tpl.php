055b113670fcb2ba530adebfa6c9a13b56b7a07e
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-26 11:07:40 -0500

    Cambios en homepage

--- sites/all/themes/utec_theme/node--admision_generico.tpl.php
+++ sites/all/themes/utec_theme/node--admision_generico.tpl.php
@@ -9,8 +9,10 @@ $field_contenido_generica = $node->field_contenido_generica['und']['0']['value']
 ?>
 <div class="container-sm">
 	<h1 class="light"><?php print $title ?></h1>
-	<p class="lead"><?php print $field_descripcion_corta_generica ?></p>
 	<div class="separator-gray separator-lg"></div>
+	<?php if ($field_descripcion_corta_generica!='') { ?>
+		<p class="lead"><?php print $field_descripcion_corta_generica ?></p>
+	<?php } ?>
 	<p class="lead text-gray mb-42"><?php print $field_contenido_generica ?></p>
 </div>
 
