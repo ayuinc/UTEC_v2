c9b4134d4a4643e23cd7acf6b438bddb932c19b0
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-26 19:48:30 -0500

    Cambios en Video home

--- sites/all/themes/utec_theme/node--homepage.tpl.php
+++ sites/all/themes/utec_theme/node--homepage.tpl.php
@@ -4,7 +4,7 @@ $video_name = $node->field_video['und']['0']['filename'];
 
 ?>
 <div class="hero-unit video-bg"> 
-	<div class="bg-img-block bg-img-block-lg flex-middle" style="background-image: url(http://104.131.4.19/sites/all/themes/utec_theme/assets/img/bg-header-3.jpg);">
+	<div class="bg-img-block bg-img-block-lg flex-middle">
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-6">
@@ -24,9 +24,9 @@ $video_name = $node->field_video['und']['0']['filename'];
     class="video-js videocover vjs-default-skin"
     controls
     preload="auto">
-    <source src="http://104.131.4.19/sites/all/themes/utec_theme/assets/video/UTEC_FULLHD_MP4.webm" type='video/webm' />
-    <source src="http://104.131.4.19/sites/all/themes/utec_theme/assets/video/UTEC_FULLHD_MP4.mp4" type='video/mp4' />
-    <source src="http://104.131.4.19/sites/all/themes/utec_theme/assets/video/UTEC_FULLHD_MP4.ogg" type='video/ogg' />
+    <source src="/sites/all/themes/utec_theme/assets/video/UTEC_FULLHD_MP4.webm" type='video/webm' />
+    <source src="/sites/all/themes/utec_theme/assets/video/UTEC_FULLHD_MP4.mp4" type='video/mp4' />
+    <source src="/sites/all/themes/utec_theme/assets/video/UTEC_FULLHD_MP4.ogg" type='video/ogg' />
     <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
   </video>
   <!--<div class="video-placeholder" style="background: url(http://104.131.4.19/sites/all/themes/utec_theme/assets/img/project-bg-1.jpg) no-repeat center top;"></div>
