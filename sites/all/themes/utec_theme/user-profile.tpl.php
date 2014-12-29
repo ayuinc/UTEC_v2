7504eab320155f810a58a2404cb322dbb22ad447
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-29 16:45:42 -0500

    Cambios Somos Utec Profesores

---
+++ sites/all/themes/utec_theme/user-profile.tpl.php
@@ -0,0 +1,51 @@
+	<ul class="grid-list grid-list-4 grid-list-centered grid-list-anchors pv-21">
+		<li class="mb-ch-14">
+			<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
+			<div>
+				<a href="#" class="thin">Rosa María Llosa Demartini</a>
+				<span>Miembro del consejo directivo</span>
+			</div>
+		</li>
+		<li class="mb-ch-14">
+			<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
+			<div>
+				<a href="#" class="thin">Rosa María Llosa Demartini</a>
+				<span>Miembro del consejo directivo</span>
+			</div>
+		</li>
+		<li class="mb-ch-14">
+			<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
+			<div>
+				<a href="#" class="thin">Rosa María Llosa Demartini</a>
+				<span>Miembro del consejo directivo</span>
+			</div>
+		</li>
+		<li class="mb-ch-14">
+			<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
+			<div>
+				<a href="#" class="thin">Rosa María Llosa Demartini</a>
+				<span>Miembro del consejo directivo</span>
+			</div>
+		</li>
+		<li class="mb-ch-14">
+			<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
+			<div>
+				<a href="#" class="thin">Rosa María Llosa Demartini</a>
+				<span>Miembro del consejo directivo</span>
+			</div>
+		</li>
+		<li class="mb-ch-14">
+			<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
+			<div>
+				<a href="#" class="thin">Rosa María Llosa Demartini</a>
+				<span>Miembro del consejo directivo</span>
+			</div>
+		</li>
+		<li class="mb-ch-14">
+			<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
+			<div>
+				<a href="#" class="thin">Rosa María Llosa Demartini</a>
+				<span>Miembro del consejo directivo</span>
+			</div>
+		</li>
+	</ul>
