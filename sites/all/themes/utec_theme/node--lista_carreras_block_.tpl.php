24586352bdfe04a26be0a8770f5059228d8f840c
Ricardo Díaz <ricardotk002@gmail.com>
2014-12-29 16:52:05 -0500

    video full screen y espacios en las secciones del home

--- sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
+++ sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
@@ -1,4 +1,4 @@
-<div class="pvp-7">
+<div class="pv-14">
 	<div class="container">
 		<h2 class="lead medium mb-7"><span class="text-gray-darker">¿Listo para</span> <span class="text-gray">cambiar el mundo?</span></h2>
 		<h4 class="lead mb-35">Éstas son nuestras carreras:</h4>
