c349f091fc7b980a8023ae9a2e8a14b52a48af88
carlos calderon <cadermino@gmail.com>
2014-12-29 13:36:06 -0500

    malla

--- sites/all/themes/utec_theme/views-view-fields--carreras-malla-curricular--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--carreras-malla-curricular--block.tpl.php
@@ -10,6 +10,8 @@
   $titulo = $fields['title']->content; 
 
   $bloque = $fields['field_numero_de_bloque']->content;
+  
+  $total_creditos = $fields['field_creditos_del_bloque']->content;
 
   $curso = $fields['field_cursos']->content;
 
@@ -19,8 +21,8 @@
   <ul class="grid-list grid-list-5 malla-curricular malla-curricular-primary">
     <li>
       <ul class="head">
-        <li>01</li>
-        <li>20<br>Créditos</li>
+        <li><?php print $bloque; ?></li>
+        <li><?php print $total_creditos; ?><br>Créditos</li>
       </ul>
       <div class="body">
         <?php print $curso ?>
