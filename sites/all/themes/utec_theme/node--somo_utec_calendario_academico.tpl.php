0312e8ea129830f6665cc954207068689f400d29
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-29 12:40:18 -0500

    Cambios Somos Utec Calnedario

--- sites/all/themes/utec_theme/node--somo_utec_calendario_academico.tpl.php
+++ sites/all/themes/utec_theme/node--somo_utec_calendario_academico.tpl.php
@@ -1,14 +1,14 @@
 <?php $title = $node->title; ?>  
+<?php $field_titulo_del_evento = $node->field_titulo_del_evento['und']['0']['value']; ?>
 <?php $field_introduccion = $node->field_introduccion['und']['0']['value']; ?>
 <?php $field_foto = $node->field_foto['und']['0']['value']; ?>
 <?php $field_contenido = $node->field_contenido['und']['0']['value']; ?>
 
 <div class="container-sm">
-	<h1 class="light">Calendario Académico;</h1>
+	<h1 class="light">Calendario Académico</h1>
 	<div class="separator-gray separator-lg"></div>
 	<div class="pv-42 lead-ch">
-		<h3>Lorem ipsum dolor sit amet, consectetur so adipisicing elit, sed sil do eiusmod
-		tempor.</h3>
+		<h3><?php print $field_titulo_del_evento ?></h3>
 	</div>
 	<div>
 		<img src="<?php print $field_foto ?>" class="img-responsive">
