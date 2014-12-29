089a18879ecfbed4cfc5b4b03919eaa123e98e9f
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-29 17:35:50 -0500

    Cambios Somos Utec Profesores

--- sites/all/themes/utec_theme/user-profile.tpl.php
+++ sites/all/themes/utec_theme/user-profile.tpl.php
@@ -3,7 +3,7 @@
 <li class="mb-ch-14">
 	<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
 	<div>
-		<a href="#" class="thin">Rosa María Llosa Demartini</a>
+		<a href="#" class="thin"><?php print $field_descripci_n ?></a>
 		<span>Miembro del consejo directivo</span>
 	</div>
 </li>
