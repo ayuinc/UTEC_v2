c665af0b72b3df2a8f4b960bbdf3996cb6b94e3a
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-29 18:24:25 -0500

    Cambios Somos Utec Profesores

--- sites/all/themes/utec_theme/node--somos_utec_profesores.tpl.php
+++ sites/all/themes/utec_theme/node--somos_utec_profesores.tpl.php
@@ -9,21 +9,20 @@
 	<h1 class="light">Profesores</h1>
 	<div class="separator-gray separator-lg"></div>
 	<p class="lead text-gray"><?php print $field_introduccion_su_profesores ?></p>
-	<ul class="pv-35 grid-list grid-list-2">
+	<ul class="grid-list grid-list-4 grid-list-centered grid-list-anchors pv-21">
     <?php foreach ($node->field_lista_profesores['und'] as $key => $value) :?>
-      <?php $name = $value['entity']->name; ?>   
-			<?php $pic = $value['entity']->picture->filename; ?>
-      <?php $desc = $value['entity']->field_descripci_n['und'][0]['value']; ?>
-			<li data-href="//">
-				<div class="user-card user-card-round size lg">
-					<div class="user-pic"><img src="/<?php if($pic != ''){print($pathfile.'pictures/'.$pic);} else {print($pathfile.'pictures/'.'user.jpg'); }?>" alt=""></div>
-					<div class="user-info">
-						<!--<a href="#">Rosa María Llosa Demartini</a>-->
-						<?php print $name ?> 
-						<span><?php print $desc?></span>
+    <?php $name = $value['entity']->name; ?>   
+		<?php $pic = $value['entity']->picture->filename; ?>
+    <?php $desc = $value['entity']->field_descripci_n['und'][0]['value']; ?>		
+			<li class="mb-ch-14">
+				<div class="grid-list-pic"><img src="/<?php if($pic != ''){print($pathfile.'pictures/'.$pic);} else {print($pathfile.'pictures/'.'user.jpg'); }?>" alt="" width="98px" height="auto" class="img-circle"></div>
+				<div>
+					<!--<a href="#">Rosa María Llosa Demartini</a>-->
+					<?php print $name ?> 					
+					<span><?php print $desc?></span>
 				</div>
 			</li>
-    <?php endforeach ?>
-	</ul>
+		<?php endforeach ?>	
+	</ul>	
 </div>
 
