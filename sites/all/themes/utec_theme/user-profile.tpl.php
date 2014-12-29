07120f8a215634bbe281810d6f129fe90de5bb2a
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-29 17:08:28 -0500

    Cambios Somos Utec Profesores

--- sites/all/themes/utec_theme/user-profile.tpl.php
+++ sites/all/themes/utec_theme/user-profile.tpl.php
@@ -1,4 +1,4 @@
-<?php print_r ($user_profile); ?>
+<?php print render($user_profile); ?>
 
 <li class="mb-ch-14">
 	<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
