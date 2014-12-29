d9e44262c2053f9974b21adb76111a80952876f0
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-29 13:23:48 -0500

    Cambios Somos Utec autoridades

--- sites/all/themes/utec_theme/node--somos_utec_autoridades.tpl.php
+++ sites/all/themes/utec_theme/node--somos_utec_autoridades.tpl.php
@@ -1,3 +1,7 @@
+<?php global $base_url; ?>
+<?php global $theme_path; ?>
+<?php $path = $base_url.'/'.$theme_path; ?>
+<?php $pathfile= variable_get('file_public_path', conf_path() . '/files/'); ?>
 <?php $title = $node->title; ?>  
 <?php $field_introduccion_autoridades = $node->field_introduccion_autoridades['und']['0']['value']; ?>
 
@@ -6,22 +10,20 @@
 <div class="container-sm">
 	<h1 class="light">Autoridades</h1>
 	<div class="separator-gray separator-lg"></div>
-	<!--<p class="lead">Exposición de proyectos egresados</p>-->
 	<p><?php print $field_introduccion_autoridades ?></p>
 	<ul class="grid-list grid-list-2">
     <?php foreach ($node->field_profesor['und'] as $key => $value) :?>
       <?php $name = $value['entity']->name; ?>   
 			<?php $pic = $value['entity']->picture->filename; ?>
       <?php $desc = $value['entity']->field_descripci_n['und'][0]['value']; ?>
-      <?php //die(print_r($value)); ?>
 			<li data-href="//">
 				<div class="user-card user-card-round size lg">
-					<div class="user-pic"><img src="assets/img/user.jpg" alt=""></div>
+					<div class="user-pic"><img src="/<?php if($pic != ''){print($pathfile.'pictures/'.$pic);} else {print($pathfile.'pictures/'.'user.jpg'); }?>" alt=""></div>
 					<div class="user-info">
 						<!--<a href="#">Rosa María Llosa Demartini</a>-->
 						<?php print $name ?> 
 						<span><?php print $desc?></span>
-						<a href="mailto:rmaria@utec.edu.pe">rmaria@utec.edu.pe</a></div>
+						<a href="mailto:<?php print $mail ?>"><?php print $mail ?></a></div>
 				</div>
 			</li>
     <?php endforeach ?>
