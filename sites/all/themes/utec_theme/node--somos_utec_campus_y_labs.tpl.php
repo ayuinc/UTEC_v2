4285f0f9cb32210e9b7c6a3de7a4bb0e26c55bbd
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-29 14:08:17 -0500

    Cambios Somos Utec campus y labs

---
+++ sites/all/themes/utec_theme/node--somos_utec_campus_y_labs.tpl.php
@@ -0,0 +1,32 @@
+<?php global $base_url; ?>
+<?php global $theme_path; ?>
+<?php $path = $base_url.'/'.$theme_path; ?>
+<?php $pathfile= variable_get('file_public_path', conf_path() . '/files/'); ?>
+<?php $title = $node->title; ?>  
+<?php $field_introduccion_autoridades = $node->field_introduccion_autoridades['und']['0']['value']; ?>
+
+<div class="container-sm">
+	<h1 class="light">Campus y laboratorios</h1>
+	<div class="separator-gray separator-sm"></div>
+	<div class="pv-ch-42 lead-ch p-ch-gray">
+		<h3>Lorem ipsum dolor sit amet, consectetur so adipisicing elit, sed sil do eiusmod
+		tempor.</h3>
+		<img src="assets/img/bg_header-1.png" class="img-responsive">
+		<h3>Lorem ipsum dolor sit amet, consectetur so adipisicing elit, sed sil do eiusmod
+		tempor.</h3>
+		<img src="assets/img/bg_header-1.png" class="img-responsive">
+		<h3>Lorem ipsum dolor sit amet, consectetur so adipisicing elit, sed sil do eiusmod
+		tempor.</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed sil do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua.
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed sil do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua.
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit.</p>
+		<h3>Lorem ipsum dolor sit amet, consectetur so adipisicing elit, sed sil do eiusmod
+		tempor.</h3>
+	</div>
+</div>
+
