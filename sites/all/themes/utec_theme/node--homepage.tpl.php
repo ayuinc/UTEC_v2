d343f9511280ae3ff59499f5925b1c09dd3255f3
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-30 12:23:14 -0500

    Cambios overlay

--- sites/all/themes/utec_theme/node--homepage.tpl.php
+++ sites/all/themes/utec_theme/node--homepage.tpl.php
@@ -27,7 +27,7 @@
     <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
   </video>
   <div class="video-placeholder" style="background: url(http://104.131.4.19/sites/all/themes/utec_theme/assets/img/project-bg-1.jpg) no-repeat center top;"></div>
-  <div class="flex-middle">
+  <div class="overlay flex-middle">
   	<div class="container">
 			<div class="row mt-70 pt-70">
 				<div class="col-sm-8 col-sm-offset-2">
@@ -36,6 +36,5 @@
 				</div>
 			</div>
 		</div>
-		<div class="overlay"></div>
   </div>
 </div>
