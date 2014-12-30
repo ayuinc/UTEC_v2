b5a7020a5e153162c101adff25000e9d656d8edc
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-30 14:58:12 -0500

    Cambios overlay

--- sites/all/themes/utec_theme/node--homepage.tpl.php
+++ sites/all/themes/utec_theme/node--homepage.tpl.php
@@ -29,7 +29,7 @@
   </video>
   <div class="video-placeholder" style="background: url(http://104.131.4.19/sites/all/themes/utec_theme/assets/img/project-bg-1.jpg) no-repeat center top;"></div>
   <div class="flex-middle">
-  	<div class="container">
+  	<div class="container relative">
 			<div class="row mt-70 pt-70">
 				<div class="col-sm-8 col-sm-offset-2">
 					<h1 class="thin text-white lead text-center mt-56 mb-0 pt-70">Descubre el Ingenio en Acción</h1>
