36e642a584a2b6ad8d8da47a947afa25ae33ef20
carlos calderon <cadermino@gmail.com>
2014-12-30 15:22:21 -0500

    convenios

--- sites/all/themes/utec_theme/node--convenio_de_intercambio.tpl.php
+++ sites/all/themes/utec_theme/node--convenio_de_intercambio.tpl.php
@@ -8,16 +8,13 @@
   // $cuerpo = $node->body['und']['0']['value'];
 
   // $fields = entity_load('field_collection_item');
-  // print_r($fields);
+  
 
   // $link = $fields[5]->field_link_['und'][0]['value'];
   //$logo  = $fields[5]->field_imagen['und'][0]['filename'];
 
-
-  // $node25 = node_load(25);
-  // $track_1 = entity_load('field_collection_item', array($node25->field_track[LANGUAGE_NONE][0]['value']));
-  // $track_2 = entity_load('field_collection_item', array($node25->field_track[LANGUAGE_NONE][1]['value']));
-
+  $fields = entity_load('field_collection_item', array($node->field_convenio['und'][0]['value']));
+  print_r($fields);
 ?>
 
 <div class="container-sm">
