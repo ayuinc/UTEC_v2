e08736f5255094e561889be1fb64d8c18bba028e
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-29 20:55:14 -0500

    Cambios Somos Utec metodologia

--- sites/all/themes/utec_theme/node--megamenu.tpl.php
+++ sites/all/themes/utec_theme/node--megamenu.tpl.php
@@ -1,8 +1,15 @@
-<?php 
+<?php $theme_path = drupal_get_path('theme', 'utec_theme'); ?>
 
-	$theme_path = drupal_get_path('theme', 'utec_theme');
+<!-- Google Tag Manager -->
+  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MHF4TM"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  '//www.googletagmanager.com/gtm.js?id='+i+dl+'&nc='+new Date().getTime();f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-MHF4TM');</script>
+<!-- End Google Tag Manager -->
 
-?>
 <!-- HEADER -->
 <div id="header"> 
 	<div class="tool-bar bg-gray-lighter pt-sm pb-sm">
