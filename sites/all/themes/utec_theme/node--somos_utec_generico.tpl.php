4b14cca54587d284cd4ec77c57a44cb9e3993d21
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-29 19:13:09 -0500

    Cambios Somos Utec Profesores

--- sites/all/themes/utec_theme/node--somos_utec_generico.tpl.php
+++ sites/all/themes/utec_theme/node--somos_utec_generico.tpl.php
@@ -3,11 +3,12 @@
 <?php $path = $base_url.'/'.$theme_path; ?>
 <?php $pathfile= variable_get('file_public_path', conf_path() . '/files/somos-utec-generico/'); ?>
 
-<?php $title = $node->title['und']['0']['value']; ?>  
+<?php $title = $node->title; ?>  
 <?php $field_introduccion_su_generico = $node->field_introduccion_su_generico['und']['0']['value']; ?>
 <?php $field_contenido_su_generico = $node->field_contenido_su_generico['und']['0']['value']; ?>
 <?php $field_foto_su_generico = $node->field_foto_su_generico['und']['0']['filename']; ?>
 
+<?php print_r ($node); ?>
 
 
 
