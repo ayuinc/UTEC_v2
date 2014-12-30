4afb077b007017e3d2e85b1fde5e23f496abcac3
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2014-12-30 11:22:02 -0500

    Actualizacion de videos

--- sites/all/themes/utec_theme/node--homepage.tpl.php
+++ sites/all/themes/utec_theme/node--homepage.tpl.php
@@ -21,9 +21,9 @@
     class="video-js videocover vjs-default-skin"
     controls
     preload="auto">
-    <source src="/sites/all/themes/utec_theme/assets/video/UTEC_FULLHD_MP4.webm" type='video/webm' />
-    <source src="/sites/all/themes/utec_theme/assets/video/UTEC_FULLHD_MP4.mp4" type='video/mp4' />
-    <source src="/sites/all/themes/utec_theme/assets/video/UTEC_FULLHD_MP4.ogg" type='video/ogg' />
+    <source src="/sites/all/themes/utec_theme/assets/video/UTEC_home.webm" type='video/webm' />
+    <source src="/sites/all/themes/utec_theme/assets/video/UTEC_home.mp4" type='video/mp4' />
+    <source src="/sites/all/themes/utec_theme/assets/video/UTEC_home.ogg" type='video/ogg' />
     <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
   </video>
   <div class="video-placeholder" style="background: url(http://104.131.4.19/sites/all/themes/utec_theme/assets/img/project-bg-1.jpg) no-repeat center top;"></div>
