35537e0ee3cf2023259f16b4f76fe023c4031bb8
carlos calderon <cadermino@gmail.com>
2015-01-06 16:52:40 -0500

    carreras

--- sites/all/themes/utec_theme/views-view-unformatted--investigacion-proyectos--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--investigacion-proyectos--block.tpl.php
@@ -7,30 +7,26 @@
  * @ingroup views_templates
  */
 ?>
-<div class="container-sm">
+
+<div class="container-sm mb-35">
 	<h1 class="light">Proyectos</h1>
-	<div class="separator-gray separator-sm"></div>
-	<h3></h3>
-	<div class="pv-ch-42">
-		<div class="table-responsive table-custom pv-42">
-			<table class="table mt-35">
-				<thead>
-					<tr>
-						<td class="medium">Investigador</td>
-						<td class="medium">Tipo</td>
-						<td class="medium">Departamento</td>
-						<td class="medium">Publicado en</td>
-						<td class="medium">Título</td>
-						<td class="medium">Año</td>
-						<td class="medium">Estado</td>
-					</tr>
-				</thead>
-				<tbody>
-		    <?php foreach ($rows as $id => $row): ?>
-			    <?php print $row;?>
-				<?php endforeach; ?>
-				</tbody>
-			</table>
-		</div>
-	</div>
+	<div class="separator-gray separator-lg"></div>
+	<table class="table mt-35">
+		<thead>
+			<tr>
+				<td class="medium">Investigador</td>
+				<td class="medium">Tipo</td>
+				<td class="medium">Departamento</td>
+				<td class="medium">Publicado en</td>
+				<td class="medium">Título</td>
+				<td class="medium">Año</td>
+				<td class="medium">Estado</td>
+			</tr>
+		</thead>
+		<tbody>
+    <?php foreach ($rows as $id => $row): ?>
+	    <?php print $row;?>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
 </div>
