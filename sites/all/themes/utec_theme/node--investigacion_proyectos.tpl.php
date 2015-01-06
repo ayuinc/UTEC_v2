8c3aba8057fb4bba71d4ec945ca1e461ef6ad6ad
carlos calderon <cadermino@gmail.com>
2015-01-06 17:35:40 -0500

    carreras

--- sites/all/themes/utec_theme/node--investigacion_proyectos.tpl.php
+++ sites/all/themes/utec_theme/node--investigacion_proyectos.tpl.php
@@ -9,22 +9,16 @@
 	$tipo = $node->field_tipo_['und']['0']['value'];		 		
 	$departamento = $node->field_departamento['und']['0']['value'];		 		
 	$publicado = $node->field_publicado_en['und']['0']['value'];		 		
-	$anio = $node->field_a_o['und']['0']['value'];		 		
-	$estado = $node->field_estado['und']['0']['value'];
+	$anio = $node->field_a_o['und']['0']['value'];
 	$link = $node->field_link['und']['0']['value'];
 	
 	// print_r($node);
 ?>
 
-<tr>
-	<td class="highlight"><?php print $investigador ?></td>
-	<td class="text-gray"><?php print $tipo ?></td>
-	<td class="text-gray"><?php print $departamento ?></td>
-	<td class="text-gray"><?php print $publicado ?></td>
-	<td class="text-gray"><a href="<?php print $link ?>" target="_blank"><?php print $titulo ?></a></td>
-	<td class="text-gray"><?php print $anio ?></td>
-	<td class="text-gray"><?php print $estado ?></td>
-</tr>
+<h3><a href="<?php print $link ?>" target="_blank"><?php print $titulo ?></a></h3>
+<div><span>Investigador: <?php print $investigador ?></span><span>Año: <?php print $anio ?></span></div>
+<div><span>Tipo: <?php print $tipo ?></span><span>Departamento: <?php print $departamento ?></span></div>
+<div><span>Publicado en: <?php print $publicado ?></span></div>
 
 
 
--- sites/all/themes/utec_theme/views-view-unformatted--investigacion-proyectos--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--investigacion-proyectos--block.tpl.php
@@ -11,22 +11,7 @@
 <div class="container-sm mb-35">
 	<h1 class="light">Proyectos</h1>
 	<div class="separator-gray separator-lg"></div>
-	<table class="table mt-35">
-		<thead>
-			<tr>
-				<td class="medium">Investigador</td>
-				<td class="medium">Tipo</td>
-				<td class="medium">Departamento</td>
-				<td class="medium">Publicado en</td>
-				<td class="medium">Título</td>
-				<td class="medium">Año</td>
-				<td class="medium">Estado</td>
-			</tr>
-		</thead>
-		<tbody>
     <?php foreach ($rows as $id => $row): ?>
 	    <?php print $row;?>
 		<?php endforeach; ?>
-		</tbody>
-	</table>
 </div>
