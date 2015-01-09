a6b9232365377b38c36a9b07d4965e65f65739d1
carlos calderon <cadermino@gmail.com>
2015-01-09 14:07:59 -0500

    metas

--- ecohuerta/sites/all/themes/front/templates/page--node--264.tpl.php
+++ ecohuerta/sites/all/themes/front/templates/page--node--264.tpl.php
@@ -3,7 +3,7 @@
   global $theme_path;
   $path = $base_url.'/'.$theme_path;
   $pathfile= variable_get('file_public_path', conf_path() . '/files/');
-  // render($content['metatags']);
+  
   render($page['content']['metatags']; 
 
 ?>
