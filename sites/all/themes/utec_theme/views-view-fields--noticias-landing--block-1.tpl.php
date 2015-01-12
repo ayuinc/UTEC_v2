b68335a696ddfba7b6a0cea644be2266baf00193
carlos calderon <cadermino@gmail.com>
2015-01-12 16:10:26 -0500

    no0ticias landing

--- sites/all/themes/utec_theme/views-view-fields--noticias-landing--block-1.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--noticias-landing--block-1.tpl.php
@@ -1,4 +1,9 @@
 <?php 
+global $base_url;
+  global $theme_path;
+  $path = $base_url.'/'.$theme_path;
+  $pathfile= variable_get('file_public_path', conf_path() . '/files/');
+
 	$title = $fields['title']->content;
 	$body = $fields['body']->content;
 	$image = $fields['field_imagen']->content;
