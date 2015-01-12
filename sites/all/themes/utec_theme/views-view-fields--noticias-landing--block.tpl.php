4c24991746591c3939719b64ad2f12d309856c7f
carlos calderon <cadermino@gmail.com>
2015-01-12 17:30:03 -0500

    no0ticias landing

--- sites/all/themes/utec_theme/views-view-fields--noticias-landing--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--noticias-landing--block.tpl.php
@@ -8,7 +8,7 @@
 	$user_id = $fields['uid']->content;
 
 ?>
-<div class="item <?php print $field->class ?>">
+<div class="item <?php print_r($fields->class) ?>">
 	<img src="<?php print $image ?>" alt="..." height="479" width="720">
 	<h3 class="light"><em><?php print $title ?></em></h3>
 	<p class="text-gray"><?php print $texto_corto ?></p>
