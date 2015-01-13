59f02a876c819b03ff6ae96715ed616fb8001267
carlos calderon <cadermino@gmail.com>
2015-01-13 11:59:32 -0500

    no0ticias landing

--- sites/all/themes/utec_theme/views-view-unformatted--perfil-de-usuarios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--perfil-de-usuarios--block.tpl.php
@@ -7,16 +7,6 @@
  * @ingroup views_templates
  */
 ?>
-	<h3 class="thin lead">Malla curricular</h3>
-	<div class="separator-gray separator-lg"></div>
-	<h3 class="lead thin">Ciclo 2015 - I</h3>
-	<div class="mb-ch-70">
-		<ul class="grid-list grid-list-5 malla-curricular malla-curricular-primary">
-    <?php foreach ($rows as $id => $row): ?>
-	    <?php print $row;?>
-		<?php endforeach; ?>
-		</ul>	
-	</div>
 
 <div class="bg-img-block bg-img-block-lg flex-middle" style="background-image: url(assets/img/bg-header-3.jpg);">
 	<div class="container-sm text-center text-white">
