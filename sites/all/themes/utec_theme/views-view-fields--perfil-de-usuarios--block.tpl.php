0353b106c6f5e1f619d6ec1397a38b8ebe7d37c1
carlos calderon <cadermino@gmail.com>
2015-01-12 20:53:32 -0500

    no0ticias landing

---
+++ sites/all/themes/utec_theme/views-view-fields--perfil-de-usuarios--block.tpl.php
@@ -0,0 +1,29 @@
+<?php 
+	global $base_url;
+  global $theme_path;
+  $path = $base_url.'/'.$theme_path;
+  $pathfile= variable_get('file_public_path', conf_path() . '/files/');
+
+  kpr($fields);
+	$title = $fields['title']->content;
+	$body = $fields['body']->content;
+	$image = $fields['field_imagen']->content;
+	$category = $fields['field_categor_a']->content;
+	$texto_corto = $fields['field_texto_corto']->content;
+	$path = $fields['path']->content;
+	$user_id = $fields['uid']->content;
+	$created = $fields['created']->content;
+
+?>
+<li class="banner banner-label-bottom mb-14 ml-0" data-href="<?php print $path ?>">
+	<div class="banner-pic" style="background-image: url(<?php print $image?>);">
+		<div class="banner-label bg-primary text-white"><span><?php print $category ?></span></div>
+	</div>
+	<div class="banner-content">
+		<div>
+			<h2><a href="#"><?php print $title ?></a></h2>
+			<em class="text-primary"><?php print $created ?></em>
+			<p class="lead"><?php print $texto_corto ?></p>
+		</div>
+	</div>
+</li>
