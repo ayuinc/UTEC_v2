4c494346799d166c8ea082bea9a2d32d72b743b4
carlos calderon <cadermino@gmail.com>
2015-01-12 19:19:55 -0500

    no0ticias landing

---
+++ sites/all/themes/utec_theme/node--noticia.tpl.php
@@ -0,0 +1,19 @@
+<?php 
+  global $base_url;
+  global $theme_path;
+  $path = $base_url.'/'.$theme_path;
+  $pathfile= variable_get('file_public_path', conf_path() . '/files/');
+
+	$titulo = $node->title;	 		
+	$cuerpo = $node->body['und']['0']['value'];	
+	$imagen = $node->field_imagen['und']['0']['filename'];
+?>
+
+<div class="container-sm">
+	<h1 class="light"><?php print $titulo ?></h1>
+	<div class="separator-gray separator-sm"></div>
+	<div class="pv-ch-42 lead-ch p-ch-gray">
+		<img src="<?php print $imagen ?>" alt="#" class="img-responsive">
+		<p><?php print $cuerpo ?></p>
+	</div>
+</div>
--- sites/all/themes/utec_theme/views-view-fields--noticias-landing--block-1.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--noticias-landing--block-1.tpl.php
@@ -1,5 +1,5 @@
 <?php 
-global $base_url;
+	global $base_url;
   global $theme_path;
   $path = $base_url.'/'.$theme_path;
   $pathfile= variable_get('file_public_path', conf_path() . '/files/');
