106f7dd11a0ec18d64986b93096dbac2259463a5
carlos calderon <cadermino@gmail.com>
2015-01-12 19:55:20 -0500

    no0ticias landing

--- sites/all/themes/utec_theme/node--noticia.tpl.php
+++ sites/all/themes/utec_theme/node--noticia.tpl.php
@@ -7,7 +7,6 @@
 	$titulo = $node->title;	 		
 	$cuerpo = $node->body['und']['0']['value'];	
 	$imagen = $node->field_imagen['und']['0']['filename'];
-	print_r($node->field_imagen);
 ?>
 
 <div class="container-sm">
@@ -15,6 +14,30 @@
 	<div class="separator-gray separator-sm"></div>
 	<div class="pv-ch-42 lead-ch p-ch-gray">
 		<img src="<?php print '/'.$pathfile.'noticias/'.$imagen ?>" alt="#" class="img-responsive">
+		<div id="carousel-custom" class="carousel carousel-custom slide mb-42" data-ride="carousel">
+			<!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+		  	<?php $images = $node->field_imagen['und']; $count=0; ?>
+			  <?php foreach ($images as $image): ?>
+			  <div class="item <?php if($count == 0){print 'active';} ?>">
+			    <img src="<?php print '/'.$pathfile.'noticias/'.$image['filename'] ?>" alt="..." height="479" width="720">
+					<h3 class="light"><em><?php print $title ?></em></h3>
+					<p class="text-gray"><?php print $texto_corto ?></p>
+			  </div>
+			  <?php $count++; ?>
+				<?php endforeach; ?>
+			</div>
+			<!-- Controls -->
+		  <a class="left carousel-control" href="#carousel-custom" role="button" data-slide="prev">
+		    <span class="icon-prev" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel-custom" role="button" data-slide="next">
+		    <span class="icon-next" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>	
+		</div>
 		<p><?php print $cuerpo ?></p>
 	</div>
 </div>
+
