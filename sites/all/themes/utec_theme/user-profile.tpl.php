d75caf45462f8b898a8c40b0f6990b2ee4ee8084
carlos calderon <cadermino@gmail.com>
2015-01-12 20:15:02 -0500

    no0ticias landing

--- sites/all/themes/utec_theme/user-profile.tpl.php
+++ sites/all/themes/utec_theme/user-profile.tpl.php
@@ -1,5 +1,6 @@
 
-<?php render(field_view_field('user', $user, 'field_descripci_n')); ?>
+<?php //render(field_view_field('user', $user, 'field_descripci_n')); ?>
+<?php print_r(render($user_profile)); ?>
 
 <li class="mb-ch-14">
 	<div class="grid-list-pic"><img src="assets/img/user.jpg" alt="" width="98px" height="auto" class="img-circle"></div>
