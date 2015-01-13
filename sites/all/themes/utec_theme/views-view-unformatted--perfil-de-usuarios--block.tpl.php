c5de0a567f71b337ffead998c86a1ea657b15af1
carlos calderon <cadermino@gmail.com>
2015-01-13 12:15:17 -0500

    no0ticias landing

--- sites/all/themes/utec_theme/views-view-unformatted--perfil-de-usuarios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--perfil-de-usuarios--block.tpl.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-<div class="bg-img-block bg-img-block-lg flex-middle" style="background-image: url(assets/img/bg-header-3.jpg);">
+<div class="bg-img-block bg-img-block-lg flex-middle" style="background-image: url(assets/img/bg-header-3.jpg); background-color:#000;">
 	<div class="container-sm text-center text-white">
 		<?php foreach ($rows as $id => $row): ?>
 	    <?php print $row;?>
