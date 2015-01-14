e5e4e3c928221cc4396538e8529efc76209f7b99
carlos calderon <cadermino@gmail.com>
2015-01-14 16:43:42 -0500

    traducciones

---
+++ sites/all/themes/utec_theme/node--blog_categorias.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>	
+
+<div class="sidebar-block">
+	<div>
+		<div class="title">
+			<h3>Explora<br><span>por</span> temas</h3>
+		</div>
+		<ul class="categories-list">
+			<?php 
+				$terms = taxonomy_vocabulary_machine_name_load('blog_tags');
+				print_r($terms);
+			?>
+			<?php //foreach ($rows as $id => $row): ?>
+		    <?php //print $row;?>
+			<?php //endforeach; ?>
+		</ul>
+	</div>
+</div>
