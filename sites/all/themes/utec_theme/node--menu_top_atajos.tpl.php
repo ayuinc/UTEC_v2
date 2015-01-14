bcbac3b14c8f862cf4b74aab864e33f0ed59e61e
Angelpm28 <angelpa38@gmail.com>
2015-01-14 15:41:44 -0500

    Pagina enlaces directos

---
+++ sites/all/themes/utec_theme/node--menu_top_atajos.tpl.php
@@ -0,0 +1,36 @@
+<?php 
+  global $base_url;
+  global $theme_path;
+  $path = $base_url.'/'.$theme_path;
+  $pathfile = variable_get('file_public_path', conf_path() . '/files/');
+
+  $titulo = $node->title;
+  $cuerpo = $node->body['und']['0']['value'];
+  $fids   = $node->field_menu_top_campos['und'];
+  // print_r($fids);
+  
+?>
+
+<div class="container-sm">
+  <h1 class="light"><?php print $titulo ?></h1>
+  <div class="separator-gray separator-sm"></div>
+  <h3><?php print $cuerpo ?></h3>
+
+  <div class="col-sm-6 list-custom mb-ch-42">
+        <div>
+          <ul>
+            <?php foreach ($fids as $fid) : ?>
+              <?php 
+              // print_r($fid['value']);
+                $num = $fid['value'];
+                $field = entity_load('field_collection_item', array($fid['value']));
+                $nombre = $field[$num]->field_nombre['und'][0]['value'];
+                $link = $field[$num]->field_link_atajo['und'][0]['value'];
+              ?>
+              <li><a href="<?php print $link ?>"><?php print $nombre ?></a></li>
+              <?php endforeach; ?>  
+          </ul>
+        </div>
+  </div>
+
+</div>
