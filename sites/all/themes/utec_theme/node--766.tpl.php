3429e2d12046decc4aef786ce5dfd379d158c883
Angelpm28 <angelpa38@gmail.com>
2015-01-14 20:24:08 -0500

    prueba formulario v1

--- sites/all/themes/utec_theme/node--766.tpl.php
+++ sites/all/themes/utec_theme/node--766.tpl.php
@@ -183,3 +183,80 @@
 </div>
 
 
+<?php
+
+session_start();
+
+//consumir web service:
+$client = new nusoap_client( "http://192.168.200.13:8380/inscripcionutecws3/services/InscripcionServicePort?wsdl", true ); 
+
+$programacion = "2015-1";
+$origen ="landing-examen-admision";
+
+$v0 = "EXAMEN";
+$v1 = "ANGEL EXAMEN";
+$v2 = "aaaa";
+$v3 = "BBBB";
+$v4 = "";
+$v5 = "";
+$v6 = "M";
+$v7 = "WWW";
+$v8 = "";
+$v9 = "";
+$v10 = "";
+$v11 = "";
+$v12 = "";
+$v13 = "";
+$v14 = "";
+$v15 = "";
+$v16 = "";
+$v17 = ""; //utf8_decode($direccion);
+$v18 = "AAA";
+$v19 = ""; //utf8_decode($celular);
+$v20 = "";
+$v21 = "1111";
+$v22 = "";
+$v23 = "1111";//24
+$v24 = "";//25
+$v25 = "11111";//26
+$v26 = "48500";//27
+$v27 = "N";
+$v28 = "";
+$v29 = "";
+$v30 = "";
+$v31 = "";
+$v32 = "";
+$v33 = "";
+$v34 = "1111";
+$v35 = $programacion;
+$v36 = "";
+$v37 = $origen;
+$v38 = "";
+
+
+$param = array('familia' => $v0, 'nombres' => $v1, 'apelPat' => $v2, 'apelMat' => $v3, 'tipDoc' => $v4, 'numDoc' => $v5, 'genero' => $v6, 'carrera1' => $v7, 'carrera2' => $v8, 'paisNaci' => $v9, 'ciudadNaci' => $v10,
+                                        'diaNaci' => $v11, 'mesNaci' => $v12, 'anhoNaci' => $v13, 'dptoActual' => $v14, 'provActual' => $v15, 'distActual' => $v16, 'direccion' => $v17, 'email' => $v18, 'celular1' => $v19, 'celular2' => $v20,
+                                        'telfFijo' => $v21, 'anhoEstudios' => $v22, 'colegio' => $v23, 'rendimiento' => $v24, 'ip' => $v25, 'codActividad' => $v26, 'esEstudiante' => $v27, 'gradoInstruccion' => $v28, 'gradoColegio' => $v29, 'movilOperador' => $v30,
+                                        'codPais' => $v31, 'numRPM' => $v32, 'comoSupoNosotros' => $v33, 'codigoProducto' => $v34, 'codigoProgramacion' => $v35, 'comentario' => $v36, 'origenOportunidad' => $v37, 'univProcedencia' => $v38);
+
+
+
+$response = $client->call( 'registrarInscripcion', $param );
+
+$error = $client->getError();
+
+if($error){
+
+	//Flash::setError("<p class='nom'>Error enviando el formulario por CRM, vuelve a intentarlo porfavor</p>");
+	echo "ERROR";
+	//log4php
+	
+}else{
+
+	ECHO "exito";
+  //$message.="  <p>Comunícate con UTEC más fácil:<br/><a href='http://guia.com.pe/estara/estara_popup.asp?advertiseId=411234&status=P&phone=51013731000&addressId=876727&' target='_blank'><img src='img/llama_gratis.jpg' alt='LLama gratis'></a></p>";
+  //Flash::setInfo($message);
+}
+
+//header( 'Location: https://app.utec.edu.pe/centro-preuniversitario/' ) ;
+?>
