2cae963991c50ac83be1d9a23b6c6c165990e775
Angelpm28 <angelpa38@gmail.com>
2015-01-15 18:16:14 -0500

    Servicio soap v15

--- registro.php
+++ registro.php
@@ -8,17 +8,18 @@ require_once( 'nusoap2/nusoap.php' );
 //consumir web service:
 $client = new nusoap_client( "http://app.utec.edu.pe/inscripcionutecws3/services/InscripcionServicePort?wsdl", true ); 
 
+
 $programacion = "2015-1";
 $origen ="landing-examen-admision";
 
 $v0 = "EXAMEN";
 $v1 = "Angel AYU 123"; //utf8_decode($_POST['nombres']);
-$v2 = "Pacheco"; //utf8_decode($_POST['apellidos']);
-$v3 = "M"; //utf8_decode($_POST['apellidom']);
+$v2 = "Pacheco"; //utf8_decode($_POST['apellido']);
+$v3 = "Masias"; //utf8_decode($_POST['apellidom']);
 $v4 = "";
 $v5 = "";
 $v6 = "M";
-$v7 = "14864";
+$v7 = "123"; //utf8_decode($carreras[$carrera_elegida]);
 $v8 = "";
 $v9 = "";
 $v10 = "";
@@ -28,13 +29,13 @@ $v13 = "";
 $v14 = "";
 $v15 = "";
 $v16 = "";
-$v17 = "123"; //utf8_decode($direccion);
-$v18 = "angelpa38@gmail.com"; //utf8_decode($_POST['email']);
+$v17 = ""; //utf8_decode($direccion);
+$v18 = "angelpa38@gmail.com"; //utf8_decode($_POST['emailx']);
 $v19 = ""; //utf8_decode($celular);
 $v20 = "";
-$v21 = "123333"; //$_POST['telefono'];
+$v21 = "12345678"; //$_POST['telefono'];
 $v22 = "";
-$v23 = "";//utf8_decode($_POST['colegio']);//24
+$v23 = "12"; //utf8_decode($_POST['colegio']);//24
 $v24 = "";//25
 $v25 = $_SERVER['REMOTE_ADDR'];//26
 $v26 = "48500";//27
@@ -56,7 +57,6 @@ $param = array('familia' => $v0, 'nombres' => $v1, 'apelPat' => $v2, 'apelMat' =
                                         'diaNaci' => $v11, 'mesNaci' => $v12, 'anhoNaci' => $v13, 'dptoActual' => $v14, 'provActual' => $v15, 'distActual' => $v16, 'direccion' => $v17, 'email' => $v18, 'celular1' => $v19, 'celular2' => $v20,
                                         'telfFijo' => $v21, 'anhoEstudios' => $v22, 'colegio' => $v23, 'rendimiento' => $v24, 'ip' => $v25, 'codActividad' => $v26, 'esEstudiante' => $v27, 'gradoInstruccion' => $v28, 'gradoColegio' => $v29, 'movilOperador' => $v30,
                                         'codPais' => $v31, 'numRPM' => $v32, 'comoSupoNosotros' => $v33, 'codigoProducto' => $v34, 'codigoProgramacion' => $v35, 'comentario' => $v36, 'origenOportunidad' => $v37, 'univProcedencia' => $v38);
-
 
 
 $response = $client->call( 'registrarInscripcion', $param );
