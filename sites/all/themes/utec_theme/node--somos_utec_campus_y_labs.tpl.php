fff836c218e01e47c8b105d658a6efd99a1cac4f
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-16 11:39:17 -0500

    Cambios

--- sites/all/themes/utec_theme/node--somos_utec_campus_y_labs.tpl.php
+++ sites/all/themes/utec_theme/node--somos_utec_campus_y_labs.tpl.php
@@ -4,7 +4,7 @@
 <?php $pathfile= variable_get('file_public_path', conf_path() . '/files/campus-labs/'); ?>
 <?php $title = $node->title; ?>  
 <?php $field_introduccion_campus = $node->field_introduccion_campus['und']['0']['value']; ?>
-<?php $field_foto_campus = $node->field_foto_campus['und']['0']['value']; ?>
+<?php $field_foto_campus = $node->field_foto_campus['und']['0']['filename']; ?>
 <?php $field_contenido_campus = $node->field_contenido_campus['und']['0']['value']; ?>
 <?php $field_introduccion_labs = $node->field_introduccion_labs['und']['0']['value']; ?>
 <?php $field_foto_labs = $node->field_foto_labs['und']['0']['filename']; ?>
@@ -17,13 +17,13 @@
 		<h3 class="text-gray-darker"><?php print t('Campus') ?></h3>
 		<p><?php print $field_introduccion_campus ?></p>
 		<?php if ($field_foto_campus!='') { ?>
-			<img src="<?php print('/'.$pathfile.$field_foto_campus)?>" class="img-responsive">
+			<img src="<?php print('/'.$pathfile.$field_foto_campus)?>" class="img-responsive pv-14">
 		<?php } ?>
 		<p><?php print $field_contenido_campus ?></p>
 		<h3 class="text-gray-darker"><?php print t('Labs') ?></h3>
 		<p><?php print $field_introduccion_labs ?></p>
 		<?php if ($field_foto_labs!='') { ?>
-			<img src="<?php print('/'.$pathfile.$field_foto_labs)?>" class="img-responsive">
+			<img src="<?php print('/'.$pathfile.$field_foto_labs)?>" class="img-responsive pv-14">
 		<?php } ?>
 		<p><?php print $field_contenido_labs ?></p>
 	</div>
