4027a404efca8bed8b0e98889cce2478c1d0c672
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-16 17:59:54 -0500

    Cambio

--- sites/all/themes/utec_theme/node--menumobile.tpl.php
+++ sites/all/themes/utec_theme/node--menumobile.tpl.php
@@ -129,9 +129,9 @@
     <li>
       <a href="/admision/pre-inscripcion"><?php print t('Registration') ?></a>
     </li>
-    <li>
+    <!--<li>
       <a href="/admision/calendario-academico"><span>Calendario</span> de admisión</a>
-    </li>
+    </li>-->
     <li>
       <a href="/admision/visita-utec"><span>Visita</span> UTEC</a>
     </li>
