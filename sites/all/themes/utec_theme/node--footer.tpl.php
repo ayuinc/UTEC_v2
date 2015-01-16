aab97c95edf17ab58a053ca851c853bf4aa7781d
andreadelrio <delrio.andre@gmail.com>
2015-01-16 15:54:23 -0500

    translations links

--- sites/all/themes/utec_theme/node--footer.tpl.php
+++ sites/all/themes/utec_theme/node--footer.tpl.php
@@ -29,19 +29,19 @@
         <li>
           <ul>
             <li class="title text-gray-lighter"><?php print t('We are UTEC') ?></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/metodologia-educativa" class="atm-menu-inferior"><small><?php print t('Educational Methodology') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/vida-estudiantil" class="atm-menu-inferior"><small><?php print t('Student Life') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/campus-y-laboratorios" class="atm-menu-inferior"><small><?php print t('Campus and Labs') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/ingenio-en-accion" class="atm-menu-inferior"><small><?php print t('Ingenio en Acción') ?></small></a></li>
+            <li><a href="/somos-utec/metodologia-educativa" class="atm-menu-inferior"><small><?php print t('Educational Methodology') ?></small></a></li>
+            <li><a href="/somos-utec/vida-estudiantil" class="atm-menu-inferior"><small><?php print t('Student Life') ?></small></a></li>
+            <li><a href="/somos-utec/campus-y-laboratorios" class="atm-menu-inferior"><small><?php print t('Campus and Labs') ?></small></a></li>
+            <li><a href="/somos-utec/ingenio-en-accion" class="atm-menu-inferior"><small><?php print t('Ingenio en Acción') ?></small></a></li>
             <li><a href="http://utecventures.com/" target="_blank" class="atm-menu-inferior"><small><?php print t('UTEC Ventures') ?></small></a></li>
             <!--<li><a href="#" class="atm-menu-inferior"><small>Calendario Académico</small></a></li>-->
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/respaldo" class="atm-menu-inferior"><small><?php print t('Backing') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/autoridades" class="atm-menu-inferior"><small><?php print t('Authorities') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/consejo-directivo" class="atm-menu-inferior"><small><?php print t('Board') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/profesores" class="atm-menu-inferior"><small><?php print t('Faculty') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/noticias" class="atm-menu-inferior"><small></small><small><?php print t('News') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/eventos-utec" class="atm-menu-inferior"><small><?php print t('Utec Events') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/somos-utec/trabaja-en-utec" class="atm-menu-inferior"><small><?php print t('Work at Utec') ?></small></a></li>
+            <li><a href="/somos-utec/respaldo" class="atm-menu-inferior"><small><?php print t('Backing') ?></small></a></li>
+            <li><a href="/somos-utec/autoridades" class="atm-menu-inferior"><small><?php print t('Authorities') ?></small></a></li>
+            <li><a href="/somos-utec/consejo-directivo" class="atm-menu-inferior"><small><?php print t('Board') ?></small></a></li>
+            <li><a href="/somos-utec/profesores" class="atm-menu-inferior"><small><?php print t('Faculty') ?></small></a></li>
+            <li><a href="/somos-utec/noticias" class="atm-menu-inferior"><small></small><small><?php print t('News') ?></small></a></li>
+            <li><a href="/somos-utec/eventos-utec" class="atm-menu-inferior"><small><?php print t('Utec Events') ?></small></a></li>
+            <li><a href="/somos-utec/trabaja-en-utec" class="atm-menu-inferior"><small><?php print t('Work at Utec') ?></small></a></li>
           </ul>
         </li>
         <!--<li>
@@ -58,43 +58,43 @@
         <li>
           <ul>
             <li class="title text-gray-lighter"><?php print t('Research') ?></li>
-            <li><a href="/<?php print $language->prefix ?>/investigacion/proyectos" class="atm-menu-inferior"><small><?php print t('Projects') ?></small></a></li>
+            <li><a href="/investigacion/proyectos" class="atm-menu-inferior"><small><?php print t('Projects') ?></small></a></li>
             <!--<li><a href="#"><small>Publicaciones</small></a></li>
             <li><a href="#"><small>Consultorías</small></a></li>-->
-            <li><a href="/<?php print $language->prefix ?>/investigacion/empresas-y-clientes"><small><?php print t('Companies and Clients') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/investigacion/contacto"><small><?php print t('Contact us') ?></small></a></li>
+            <li><a href="/investigacion/empresas-y-clientes"><small><?php print t('Companies and Clients') ?></small></a></li>
+            <li><a href="/investigacion/contacto"><small><?php print t('Contact us') ?></small></a></li>
 
           </ul>
         </li>
         <li>
           <ul>
             <li class="title text-gray-lighter"><?php print t('Degrees') ?></li>
-            <li><a href="/<?php print $language->prefix ?>/carreras/ingenieria-quimica-industrial" class="atm-menu-inferior"><small><?php print t('Industrial and Chemical Engineering') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/carreras/ingenieria-electronica" class="atm-menu-inferior"><small><?php print t('Electronic Engineering') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/carreras/ingenieria-mecanica" class="atm-menu-inferior"><small><?php print t('Mechanical Engineering') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/carreras/ingenieria-de-la-energia" class="atm-menu-inferior"><small><?php print t('Energy Engineering') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/carreras/ingenieria-industrial" class="atm-menu-inferior"><small><?php print t('Industrial Engineering') ?></small></a></li>
-            <!--<li><a href="/<?php print $language->prefix ?>/carreras/menciones" class="atm-menu-inferior"><small><?php print t('Mentions') ?></small></a></li>-->
-            <li><a href="/<?php print $language->prefix ?>/carreras/escuela-de-desarrollo" class="atm-menu-inferior"><small><?php print t('SPD') ?></small></a></li>
+            <li><a href="/carreras/ingenieria-quimica-industrial" class="atm-menu-inferior"><small><?php print t('Industrial and Chemical Engineering') ?></small></a></li>
+            <li><a href="/carreras/ingenieria-electronica" class="atm-menu-inferior"><small><?php print t('Electronic Engineering') ?></small></a></li>
+            <li><a href="/carreras/ingenieria-mecanica" class="atm-menu-inferior"><small><?php print t('Mechanical Engineering') ?></small></a></li>
+            <li><a href="/carreras/ingenieria-de-la-energia" class="atm-menu-inferior"><small><?php print t('Energy Engineering') ?></small></a></li>
+            <li><a href="/carreras/ingenieria-industrial" class="atm-menu-inferior"><small><?php print t('Industrial Engineering') ?></small></a></li>
+            <!--<li><a href="/carreras/menciones" class="atm-menu-inferior"><small><?php print t('Mentions') ?></small></a></li>-->
+            <li><a href="/carreras/escuela-de-desarrollo" class="atm-menu-inferior"><small><?php print t('SPD') ?></small></a></li>
           </ul>
         </li>
         <li>
           <ul>
             <li class="title text-gray-lighter"><?php print t('Admissions') ?></li>
-            <li><a href="/<?php print $language->prefix ?>/admision/modalidades" class="atm-menu-inferior"><small><?php print t('Admissions Methods') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/admision/charlas-informativas" class="atm-menu-inferior"><small><?php print t('Informative Talks') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/admision/calendario-academico"><small><?php print t('Admission Calendar') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/admision/modalidades" class="atm-menu-inferior"><small><?php print t('Pre-registration') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/admision/informacion-economica" class="atm-menu-inferior"><small><?php print t('Financial Information') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/admision/visita-utec" class="atm-menu-inferior"><small><?php print t('Visit UTEC') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/admision/orientacion-vocacional" class="atm-menu-inferior"><small><?php print t('Vocational Guidance') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/admision/preguntas-frecuentes" class="atm-menu-inferior"><small><?php print t('FAQ') ?></small></a></li>
+            <li><a href="/admision/modalidades" class="atm-menu-inferior"><small><?php print t('Admissions Methods') ?></small></a></li>
+            <li><a href="/admision/charlas-informativas" class="atm-menu-inferior"><small><?php print t('Informative Talks') ?></small></a></li>
+            <li><a href="/admision/calendario-academico"><small><?php print t('Admission Calendar') ?></small></a></li>
+            <li><a href="/admision/modalidades" class="atm-menu-inferior"><small><?php print t('Pre-registration') ?></small></a></li>
+            <li><a href="/admision/informacion-economica" class="atm-menu-inferior"><small><?php print t('Financial Information') ?></small></a></li>
+            <li><a href="/admision/visita-utec" class="atm-menu-inferior"><small><?php print t('Visit UTEC') ?></small></a></li>
+            <li><a href="/admision/orientacion-vocacional" class="atm-menu-inferior"><small><?php print t('Vocational Guidance') ?></small></a></li>
+            <li><a href="/admision/preguntas-frecuentes" class="atm-menu-inferior"><small><?php print t('FAQ') ?></small></a></li>
           </ul>
         </li>
         <li>
           <ul>
             <li class="title text-gray-lighter"><?php print t('Agreements') ?></li>
-            <li><a href="/<?php print $language->prefix ?>/convenios/convenios-de-intercambio" class="atm-menu-inferior"><small><?php print t('Exchange Agreements') ?></small></a></li>
+            <li><a href="/convenios/convenios-de-intercambio" class="atm-menu-inferior"><small><?php print t('Exchange Agreements') ?></small></a></li>
           </ul>
         </li>  
         <li>
@@ -102,8 +102,8 @@
             <li class="title text-gray-lighter"><?php print t('SPD') ?></li>
             <!--<li><a href="/edp/cursos/diplomados" class="atm-menu-inferior"><small>Diplomados</small></a></li>
             <li><a href="/edp/cursos/cursos-online" class="atm-menu-inferior"><small>Cursos Online</small></a></li>-->
-            <li><a href="/<?php print $language->prefix ?>/edp/cursos/cursos-cortos" class="atm-menu-inferior"><small><?php print t('Short Courses') ?></small></a></li>
-            <li><a href="/<?php print $language->prefix ?>/edp/calendario" class="atm-menu-inferior"><small><?php print t('Calendar') ?></small></a></li>
+            <li><a href="/edp/cursos/cursos-cortos" class="atm-menu-inferior"><small><?php print t('Short Courses') ?></small></a></li>
+            <li><a href="/edp/calendario" class="atm-menu-inferior"><small><?php print t('Calendar') ?></small></a></li>
             <!--<li><a href="/edp/cursos/cursos-adhoc" class="atm-menu-inferior"><small>Cursos Adoc</small></a></li>
             <li><a href="/convenios" class="atm-menu-inferior"><small>Cursos Adoc</small></a></li>-->
           </ul>
