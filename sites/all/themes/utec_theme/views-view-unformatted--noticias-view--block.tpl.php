9cd25b45aa4e247924b3a61e3bb367a1b6cb6b1f
Gus <yo@soygus.com>
2015-01-16 15:02:11 -0500

    NOTICIAS: removed ver más button

--- sites/all/themes/utec_theme/views-view-unformatted--noticias-view--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--noticias-view--block.tpl.php
@@ -9,7 +9,7 @@
 ?>
 <div class="pv-28" id="noticias">
 	<h3 class="lead medium mb-0"><span class="text-gray-darker"><?php print t('New: Up to date with') ?></span> <span class="text-gray">UTEC</span></h3>
-	<h4 class="lead mb-56 mt-0"><?php print t('We encourage entrepreneurship and innovation.') ?></h4>
+	<h4 class="lead mb-56 mt-0"><a href="/somos-utec/noticias" data-href="/somos-utec/noticias"><?php print t('We encourage entrepreneurship and innovation.') ?></a></h4>
 		<?php //global $count; ?>
     <?php foreach ($rows as $id => $row): ?>
 		    <?php
@@ -18,7 +18,4 @@
 			    $count = 1;
 		    ?>
 		<?php endforeach; ?>
-	<div>
-		<a href="/somos-utec/noticias" data-href="/somos-utec/noticias" class="btn btn-lg btn-primary see-more uppercase"><?php print t('View more news') ?></a>
-	</div>
 </div>
