a371728b6209cf983a8256fa5af0be327dba83a8
carlos calderon <cadermino@gmail.com>
2015-01-19 16:37:54 -0500

    traduccion firlds

--- sites/all/themes/utec_theme/views-view-fields--perfil-de-usuarios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--perfil-de-usuarios--block.tpl.php
@@ -1,6 +1,7 @@
 <?php 
 	global $base_url;
   global $theme_path;
+  global $language;
   $path = $base_url.'/'.$theme_path;
   $pathfile= variable_get('file_public_path', conf_path() . '/files/');
 
@@ -9,6 +10,8 @@
 	$picture = $fields['picture']->content;
 	$descripcion = $fields['field_descripci_n']->content;
 	$resenia = $fields['field_rese_a']->content;
+	$descripcion_ingles = $fields['field_descripci_n_ingles']->content;
+	$resenia_ingles = $fields['field_rese_a_ingles']->content;
 	$twitter = $fields['field_twitter']->content;
 
 ?>
@@ -18,5 +21,10 @@
 </div>
 <h3 class="thin"><?php print $name ?></h3>
 <a href="#" rel="nofollow" class="block"><em><?php if($twitter){print $twitter;} else print $mail;  ?></em></a>
-<p><?php print $descripcion ?></p>
+<?php if ($language->prefix == 'en') : ?>
+<p><?php print $descripcion_ingles ?></p>
+<p><?php print $resenia_ingles ?></p>
+<?php elseif ($language->prefix == 'es') : ?>
+	<p><?php print $descripcion ?></p>
 <p><?php print $resenia ?></p>
+<?php endif; ?>
