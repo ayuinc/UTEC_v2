412ce1f7d35fe3b3f51732636b617f05cb9ad5f3
carlos calderon <cadermino@gmail.com>
2015-01-19 17:38:42 -0500

    traduccion firlds

--- sites/all/themes/utec_theme/node--footer.tpl.php
+++ sites/all/themes/utec_theme/node--footer.tpl.php
@@ -7,23 +7,23 @@
 					<h3 class="thin text-white pt-7"><i class="icon-subscribe mr-7"></i><?php print t('Subscribe to UTEC') ?></h3>
 				</li>
 				<li>
-					<div class="form-subscribe">
+					<!-- <div class="form-subscribe">
 						<form action="" class="form-inline mb-ch-0">
 							<div class="form-group">
-								<label for="subscribe_name" class="sr-only"><?php print t('Name') ?></label>
-                <?php if ($language->prefix == 'en'): ?>
+								<label for="subscribe_name" class="sr-only"><?php //print t('Name') ?></label>
+                <?php //if ($language->prefix == 'en'): ?>
 								  <input type="text" class="form-control" width="210" height="auto" placeholder="Name">
-                <?php elseif ($language->prefix == 'es'): ?>
+                <?php //elseif ($language->prefix == 'es'): ?>
                   <input type="text" class="form-control" width="210" height="auto" placeholder="Nombre">
-                <?php endif ?>
+                <?php //endif ?>
 							</div>
 							<div class="form-group">
-								<label for="subscribe_email" class="sr-only"><?php print t('Email') ?></label>
+								<label for="subscribe_email" class="sr-only"><?php //print t('Email') ?></label>
 								<input type="text" class="form-control" width="210" height="auto" placeholder="E-mail">
 								<button class="btn btn-outline btn-white"><i class="icon-arrows-right"></i></button>
 							</div>
 						</form>
-					</div>
+					</div> -->
 				</li>
 			</ul>
 		</div>
