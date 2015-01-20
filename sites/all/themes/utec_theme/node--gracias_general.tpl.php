0f3bcd58bae1af398c314a64567c7f57218edb1b
Angelpm28 <angelpa38@gmail.com>
2015-01-20 14:52:51 -0500

    Formularios v2

--- sites/all/themes/utec_theme/node--gracias_general.tpl.php
+++ sites/all/themes/utec_theme/node--gracias_general.tpl.php
@@ -1,9 +1,13 @@
-<?php $titulo = $node->title; ?>   
-<?php $body = $node->body['und']['0']['value']; ?>
+<?php 
+$titulo = $node->title;   
+$titulo_usuario = $node->field_titulo_usuario['und']['0']['value'];
+$body = $node->body['und']['0']['value']; 
+
+?>
 
 
 <div class="container-sm mb-56">
-	<h1 class="light"><?php print $title ?></h1>
+	<h1 class="light"><?php print $titulo_usuario ?></h1>
 	<div class="separator-gray separator-lg"></div>
 	<?php print $body ?>	
 </div>
