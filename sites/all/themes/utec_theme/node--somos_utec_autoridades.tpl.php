d2698aee49589737501a86613eb73f4f3a6dbdfa
carlos calderon <cadermino@gmail.com>
2015-01-19 20:18:55 -0500

    traduccion firlds

--- sites/all/themes/utec_theme/node--somos_utec_autoridades.tpl.php
+++ sites/all/themes/utec_theme/node--somos_utec_autoridades.tpl.php
@@ -16,13 +16,18 @@
 			<?php $pic = $value['entity']->picture->filename; ?>
 			<?php $uid = $value['entity']->uid; ?>
       <?php $desc = $value['entity']->field_descripci_n['und'][0]['value']; ?>
+      <?php $desc_ingles = $value['entity']->field_descripci_n_ingles['und'][0]['value']; ?>
 			<li data-href="/user/<?php print $uid ?>">
 				<div class="user-card user-card-round size lg">
 					<div class="user-pic"><img src="/<?php if($pic != ''){print($pathfile.'pictures/'.$pic);} else {print($pathfile.'pictures/'.'user.jpg'); }?>" alt=""></div>
 					<div class="user-info">
 						<!--<a href="#">Rosa María Llosa Demartini</a>-->
 						<a href="/user/<?php print $uid ?>"><?php print $name ?></a>
+						<?php if ($language->prefix == 'en') : ?>
+						<span><?php print $desc_ingles?></span>
+						<?php elseif ($language->prefix == 'es') : ?>
 						<span><?php print $desc?></span>
+						<?php endif; ?>
 						<a href="mailto:<?php print $mail ?>"><?php print $mail ?></a></div>
 				</div>
 			</li>
