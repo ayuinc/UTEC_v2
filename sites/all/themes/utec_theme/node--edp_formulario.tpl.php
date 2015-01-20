47735a8334c202c35aa0e2beb12e21e780068e3a
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-19 20:27:56 -0500

    Cambios formulario

--- sites/all/themes/utec_theme/node--edp_formulario.tpl.php
+++ sites/all/themes/utec_theme/node--edp_formulario.tpl.php
@@ -23,12 +23,16 @@
 <div class="container-sm form-custom mt-35"> 
 	<form action="" data-submit="Formulario de inscripcion">
 	<div class="row">
-		<div class="form-group col-sm-6">
+		<div class="form-group col-sm-5">
 			<p><?php print t('Company') ?></p>
+		</div>
+		<div class="form-group col-sm-1">
 			<input id="company" type="radio" name="optionedp" class="form-control">
 		</div>
-		<div class="form-group col-sm-6">
+		<div class="form-group col-sm-5">
 			<p><?php print t('Person') ?></p>
+		</div>
+		<div class="form-group col-sm-1">
 			<input id="person" type="radio" name="optionedp" class="form-control">
 		</div>
 	</div>
