5eb138b16ef3d3dbb643bc649df0f789772a6d47
Ricardo Díaz <ricardotk002@gmail.com>
2015-01-20 12:59:35 -0500

    profesores alias

--- sites/all/themes/utec_theme/node--somos_utec_profesores.tpl.php
+++ sites/all/themes/utec_theme/node--somos_utec_profesores.tpl.php
@@ -21,7 +21,7 @@
 			<li class="mb-ch-14" data-href="/user/<?php print $uid ?>">
 				<div class="grid-list-pic"><img src="/<?php if($pic != ''){print($pathfile.'pictures/'.$pic);} else {print($pathfile.'pictures/'.'user.jpg'); }?>" alt="" width="180px" height="auto" class="img-circle"></div>
 				<div>
-					<?php $profesor_url = "/" . $language->prefix . drupal_get_path_alias('user/'.$uid, $language->prefix); ?>
+					<?php $profesor_url = "/" . $language->prefix . "/" . drupal_get_path_alias('user/'.$uid, $language->prefix); ?>
 					<a href="<?php print $profesor_url; ?>"><?php print $name ?></a>
 					<?php if ($language->prefix == 'en') : ?>
 					<span><?php print $desc_ingles?></span>
