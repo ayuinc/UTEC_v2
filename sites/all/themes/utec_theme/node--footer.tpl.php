54c5b784d70805cd581868ea7541d829dca04bbe
andreadelrio <delrio.andre@gmail.com>
2015-01-19 20:08:07 -0500

    updating footer

--- sites/all/themes/utec_theme/node--footer.tpl.php
+++ sites/all/themes/utec_theme/node--footer.tpl.php
@@ -113,8 +113,8 @@
         </li>
         <li>
           <ul>
-            <li class="title text-gray-lighter"><?php print t('Admissions') ?></li>
               <?php if ($language->prefix == 'en'): ?>
+            <li class="title text-gray-lighter">Admissions</li>
             <li><a href="/<?php print $language->prefix ?>/methods-of-admission" class="atm-menu-inferior"><small>Modalidades de Admisión</small></a></li>
             <li><a href="/<?php print $language->prefix ?>/admissions-talks" class="atm-menu-inferior"><small><?php print t('Informative Talks') ?></small></a></li>
             <li><a href="/<?php print $language->prefix ?>/admissions-calendar"><small><?php print t('Admissions Calendar') ?></small></a></li>
@@ -123,6 +123,7 @@
             <li><a href="/<?php print $language->prefix ?>/advising-and-counseling" class="atm-menu-inferior"><small><?php print t('Guidance') ?></small></a></li>
             <li><a href="/<?php print $language->prefix ?>/admissions-faq" class="atm-menu-inferior"><small><?php print t('FAQ') ?></small></a></li>
               <?php elseif ($language->prefix == 'es'): ?>
+            <li class="title text-gray-lighter">Modalidades de Admisión</li>
             <li><a href="/<?php print $language->prefix ?>/modalidades-de-admision" class="atm-menu-inferior"><small>Admissions</small></a></li>
             <li><a href="/<?php print $language->prefix ?>/charlas-informativas" class="atm-menu-inferior"><small><?php print t('Informative Talks') ?></small></a></li>
             <li><a href="/<?php print $language->prefix ?>/calendario-de-admision"><small><?php print t('Admissions Calendar') ?></small></a></li>
