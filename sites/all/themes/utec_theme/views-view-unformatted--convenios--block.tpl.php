ca5671c063d85bba6efe40c71358b651617d56a4
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-20 16:24:21 -0500

    Cambios nav

--- sites/all/themes/utec_theme/views-view-unformatted--convenios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--convenios--block.tpl.php
@@ -3,7 +3,7 @@
 ?>
 
   <div class="container-sm">
-    <h1 class="light">Convenios de Intercambio</h1>
+    <h1 class="light"><?php print t('Alliances') ?></h1>
     <div class="separator-gray separator-sm"></div>
     <h3 class="text-justify"><?php print t('Your admission to UTEC means more than an opportunity to attend one of the best engineering schools in Latin America.  We have an established international network of partner universities, companies, and organizations that includes some of the best academic institutions in the world.') ?></h3>
   </div>
