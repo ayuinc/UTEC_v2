e25982bb480d5283e8ec0c3739aca6e53ae6c31e
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-19 20:47:49 -0500

    Cambios formulario

--- sites/all/themes/utec_theme/node--edp_formulario.tpl.php
+++ sites/all/themes/utec_theme/node--edp_formulario.tpl.php
@@ -291,19 +291,3 @@
 	</div>			
 </div>	
 
-<script>
-	$( document ).ready(function() {
-		$('#company_form').hide();
-		$('#person_form').hide();
-		
-		$("#company").click(function() {
-			$('#company_form').show();
-			$('#person_form').hide();
-		});	
-		
-		$("#person").click(function() {
-			$('#company_form').hide();
-			$('#person_form').show();
-		});		
-	});
-</script>
--- sites/all/themes/utec_theme/node--footer.tpl.php
+++ sites/all/themes/utec_theme/node--footer.tpl.php
@@ -189,3 +189,20 @@
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
 </script>
+
+<script>
+	$( document ).ready(function() {
+		$('#company_form').hide();
+		$('#person_form').hide();
+		
+		$("#company").click(function() {
+			$('#company_form').show();
+			$('#person_form').hide();
+		});	
+		
+		$("#person").click(function() {
+			$('#company_form').hide();
+			$('#person_form').show();
+		});		
+	});
+</script>
