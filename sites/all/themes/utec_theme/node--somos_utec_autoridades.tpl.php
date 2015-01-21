daca5f3f1095a5b728f23652d7c4b383f156648b
carlos calderon <cadermino@gmail.com>
2015-01-21 15:22:17 -0500

    cambios de lenguaje

--- sites/all/themes/utec_theme/node--somos_utec_autoridades.tpl.php
+++ sites/all/themes/utec_theme/node--somos_utec_autoridades.tpl.php
@@ -13,7 +13,7 @@
 	<div class="separator-gray separator-lg"></div>
 	<p class="lead text-gray"><?php print $field_introduccion_autoridades ?></p>
 	<ul class="pv-35 grid-list grid-list-2 grid-list-1-xs">
-		<?php kpr($node); ?>
+		<?php //kpr($node); ?>
     <?php foreach ($node->field_profesor['und'] as $key => $value) :?>
          
       <?php 
@@ -38,7 +38,7 @@
 			<?php $pic = $value['entity']->picture->filename; ?>
 			<?php $uid = $value['entity']->uid; ?>
       <?php $desc = $value['entity']->field_descripci_n_autoridad['und'][0]['value']; ?>
-      <?php $desc_ingles = $value['entity']->field_descripci_n_autoridad_ingles['und']['0']['value']; ?>
+      <?php $desc_ingles = $value['entity']->field_descripci_n_autoridad_ingl['und']['0']['value']; ?>
       <?php $res = $value['entity']->field_rese_a_autoridad['und']['0']['value']; ?>
       <?php $res_ingles = $value['entity']->field_rese_a_autoridad_ingles['und']['0']['value']; ?>	
 
