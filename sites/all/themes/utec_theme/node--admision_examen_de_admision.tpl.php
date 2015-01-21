17737366898a4cced85eca2db5d8f259acb28905
Angelpm28 <angelpa38@gmail.com>
2015-01-20 23:19:29 -0500

    Formularios v16

--- sites/all/themes/utec_theme/node--admision_examen_de_admision.tpl.php
+++ sites/all/themes/utec_theme/node--admision_examen_de_admision.tpl.php
@@ -7,59 +7,156 @@ $field_como_postula_examen = $node->field_como_postula_examen['und']['0']['value
 $field_costos_examen = $node->field_costos_examen['und']['0']['value'];
 $field_temario_examen = $node->field_temario_examen['und']['0']['value'];
 
+global $language; 
+$idioma = $language->prefix;
+
 ?>
 	<div class="container-sm">
 	<div>
-		<h1 class="light"><?php print $titulo ?></h1>
-		<div class="separator-gray separator-md"></div>
-		<div>
-		<p class="lead text-gray"><?php print $field_descripcion_corta_examen ?></p>
-		<div class="mb-ch-42">
-			<div>
-				<h3><?php //print t('How to apply?') ?></h3>
-				<div class="list-custom">
-					<?php  print $field_como_postula_examen ?>
-				</div>
-			</div>
-			<div>
-				<h3><?php print t('Costs:') ?></h3>
-					<div class="list-custom">
-						<ul><li><?php print $field_costos_examen ?></li></ul>
+				<h1 class="light"><?php print $titulo ?></h1>
+				<div class="separator-gray separator-md"></div>
+				<div>
+				<p class="lead text-gray"><?php print $field_descripcion_corta_examen ?></p>
+				<div class="mb-ch-42">
+					<div>
+						<h3><?php //print t('How to apply?') ?></h3>
+						<div class="list-custom">
+							<?php  print $field_como_postula_examen ?>
+						</div>
 					</div>
-			</div>
-			<div>
-				<h3><?php print t('Agenda:') ?></h3>
-				<div class="list-custom">
-					<?php print $field_temario_examen ?>
+					<div>
+						<h3><?php print t('Costs:') ?></h3>
+							<div class="list-custom">
+								<ul><li><?php print $field_costos_examen ?></li></ul>
+							</div>
+					</div>
+					<div>
+						<h3><?php print t('Agenda:') ?></h3>
+						<div class="list-custom">
+							<?php print $field_temario_examen ?>
+						</div>
+					</div>
+					<div class="gbo calendar">
+						<h3><?php print t('Calendar:') ?></h3>
+						<div class="table-responsive table-custom pv-42">
+							<table class="table">
+								<thead>
+									<tr>
+										<td><?php print t('Event') ?></td>
+										<td><?php print t('Date') ?></td>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td class="highlight text-center"><?php print t('Registration starts') ?></td>
+										<td>01/04/2014</td>
+									</tr>
+									<tr>
+										<td class="highlight text-center"><?php print t('Registration ends') ?></td>
+										<td>01/03/2015</td>
+									</tr>
+									<tr>
+										<td class="highlight text-center"><?php print t('Admissions Exam') ?></td>
+										<td>04/03/2015</td>
+									</tr>
+								</tbody>
+						    </table>
+					    </div>
+					</div>
 				</div>
+				</div>
+	</div>
+
+	<h3><?php print t("For more information:") ?></h3>
+	<form action="/registro.php" data-submit="Formulario de inscripcion" method="post" id="formRegistro" name="formRegistro">
+		<input type="hidden" name="origen" value="<?php print $title; ?>">
+		<input type="hidden" name="idioma" value="<?php print $idioma; ?>">		
+
+		<div class="row">
+			<div class="form-group col-sm-6">
+				<label for="" class="sr-only"><?php print t("Name") ?></label>
+				<input id="nombres" name="nombres" type="text" class="form-control" placeholder="Nombres">
 			</div>
-			<div class="gbo calendar">
-				<h3><?php print t('Calendar:') ?></h3>
-				<div class="table-responsive table-custom pv-42">
-					<table class="table">
-						<thead>
-							<tr>
-								<td><?php print t('Event') ?></td>
-								<td><?php print t('Date') ?></td>
-							</tr>
-						</thead>
-					<tbody>
-						<tr>
-							<td class="highlight text-center"><?php print t('Registration starts') ?></td>
-							<td>01/04/2014</td>
-						</tr>
-						<tr>
-							<td class="highlight text-center"><?php print t('Registration ends') ?></td>
-							<td>01/03/2015</td>
-						</tr>
-						<tr>
-							<td class="highlight text-center"><?php print t('Admissions Exam') ?></td>
-							<td>04/03/2015</td>
-						</tr>
-					</tbody>
-				</table>
+			<div class="form-group col-sm-6">
+				<label for="" class="sr-only"><?php print t("Middle Name") ?></label>
+				<input id="apellidop" name="apellidop" type="text" class="form-control" placeholder="Apellido paterno">
 			</div>
+
+		</div>
+		<div class="row">
+			<div class="form-group col-sm-6">
+				<label for="" class="sr-only"><?php print t("Last Name") ?></label>
+				<input id="apellidom" name="apellidom" type="text" class="form-control" placeholder="Apellido materno">
+			</div>			
+			<div class="form-group col-sm-6">
+				<label for="" class="sr-only"><?php print t('E-Mail') ?></label>
+				<input id="email" name="email" type="text" class="form-control" placeholder="Email">
 			</div>
 		</div>
+		<div class="row">
+			<div class="form-group col-sm-6">
+				<label for="" class="sr-only"><?php print t('Phone') ?></label>
+				<input id="telefono" name="telefono" type="text" class="form-control" placeholder="Teléfono">
+			</div>
+			<div class="form-group col-sm-6">
+				<label for="" class="sr-only"><?php print t('Undergrad Program') ?></label>
+				<select name="carrera" id="carrera" class="form-control select-override">
+					<option value=""><?php print t('Undergrad Program') ?></option>
+				  	<option value="15970"><?php print t('Industrial Engineering') ?></option>
+		            <option value="14864"><?php print t('Mechanical Engineering') ?></option>
+		            <option value="15968"><?php print t('Energy Engineering') ?></option>
+		            <option value="15964"><?php print t('Electronic Engineering') ?></option>
+		            <option value="15966"><?php print t('Industrial and Chemical Engineering') ?></option>
+				</select>
+			</div>
+		</div>		
+		<div class="row">
+			<div class="form-group col-sm-12">
+				<label for="" class="sr-only"><?php print t('Message') ?></label>
+				<textarea class="form-control" placeholder="<?php print t('Message') ?>" name="consulta" id="consulta"></textarea>
+			</div>
+		</div>
+		<div class="row errordiv">
+			<div id="errordiv" class="col-sm-12 form-group"></div>			
+		</div>		
+		<div class="text-right">
+			<button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+		</div>
+	</form>			
 	</div>
-	</div>
+
+<script type="text/javascript">
+jQuery(function() {                        
+    jQuery('#formRegistro').validate( {
+	      rules: {
+	        "nombres": { required: true }, "apellidop": { required: true }, "apellidom": { required: true }, "carrera": { required: true },	
+	        "tipo-documento": { required: true }, "documento": { required: true }, "pais": { required: true }, "ciudad": { required: true },	
+	        "dia": { required: true }, "mes": { required: true }, "ano": { required: true }, "depto": { required: true }, "provincia": { required: true },	
+	        "distrito": { required: true }, "direccion-actual": { required: true },	"email": { required: true }, "telefono": { required: true },	
+			"ano-culminacion": { required: true }, "colegio-procedencia": { required: true }, 
+			"estudios-dpto": { required: true }, "estudios-provincia": { required: true },
+	        "estudios-distrito": { required: true }, "estudios-rendimiento": { required: true }
+	      },
+	      messages: {
+		    "nombres": { required: "Completa tus nombres. <br />" }, "apellidop": { required: "Completa tu apellido paterno. <br /> " },		
+		    "apellidom": { required: "Completa tu apellido materno. <br /> " }, "carrera": { required: "Selecciona tu carrera. <br /> " },	
+		    "tipo-documento": { required: "Selecciona tu tipo de documento. <br /> " }, "documento": { required: "Completa tu documento. <br /> " },	
+		    "pais": { required: "Completa tu país. <br /> " }, "ciudad": { required: "Completa tu ciudad. <br /> " }, 
+		    "dia": { required: "Completa tu día de nacimiento. <br /> " }, "mes": { required: "Completa tu mes de nacimiento. <br /> " },
+		    "ano": { required: "Completa tu año de nacimiento. <br /> " }, "depto": { required: "Completa tu departamento. <br /> " },
+		    "provincia": { required: "Completa tu provincia. <br /> " }, "distrito": { required: "Completa tu distrito. <br /> " },
+		    "direccion-actual": { required: "Completa tu domicilio. <br /> " }, "email": { required: "Completa tu e-mail. <br /> " },
+	      	"telefono": { required: "Completa tu telefono. <br /> " }, "ano-culminacion": { required: "Completa tu año de culminación. <br /> " }, 
+	      	"colegio-procedencia": { required: "Completa tu colegio de procedencia. <br /> " },
+		    "estudios-dpto": { required: "Completa el departamento de tu colegio. <br /> " }, "estudios-provincia": { required: "Completa la provincia de tu colegio. <br /> " },
+		    "estudios-distrito": { required: "Completa el distrito de tu colegio. <br /> " }, "estudios-rendimiento": { required: "Selecciona tu rendimiento académico. <br /> " }
+		  },
+	      errorPlacement: function( error, element ) {
+	        	error.insertAfter( '#errordiv' );
+	      },
+	      submitHandler: function( form ) {                                                 
+	      		form.submit();
+	      }
+    });
+});
+</script>
