39047ca80bf4c6a290e66576c1c4ab9b7ee5c3d3
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-20 23:56:07 -0500

    cambios

--- sites/all/themes/utec_theme/node--938.tpl.php
+++ sites/all/themes/utec_theme/node--938.tpl.php
@@ -27,28 +27,28 @@
 			
 		<div class="row">
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Nombres</label>
-				<input  id="nombres" name="nombres" type="text" class="form-control" placeholder="Nombres">
+				<label for="" class="sr-only">Name</label>
+				<input  id="nombres" name="nombres" type="text" class="form-control" placeholder="Name">
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Apellido paterno</label>
-				<input  id="apellidop" name="apellidop" type="text" class="form-control" placeholder="Apellido paterno">
+				<label for="" class="sr-only">Middle Name</label>
+				<input  id="apellidop" name="apellidop" type="text" class="form-control" placeholder="Middle Name">
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Apellido materno</label>
-				<input  id="apellidom" name="apellidom" type="text" class="form-control" placeholder="Apellido materno">
+				<label for="" class="sr-only">Last Name</label>
+				<input  id="apellidom" name="apellidom" type="text" class="form-control" placeholder="Last Name">
 			</div>
 		</div>
 		<div class="text-gray h4 thin">
-			<label for="">Género:</label>
-			<input  id="femenino" type="radio" checked name="genero" value="F"> Femenino
-			<input  id="masculino" type="radio" name="genero" value="M"> Masculino
+			<label for="">Gender:</label>
+			<input  id="femenino" type="radio" checked name="genero" value="F"> Female
+			<input  id="masculino" type="radio" name="genero" value="M"> Male
 		</div>
 		<div class="row">
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Carrera de interés</label>
+				<label for="" class="sr-only">Interest</label>
 				<select name="carrera" id="carrera" class="form-control select-override">
-					<option value="">Carrera de interés</option>
+					<option value="">Interest</option>
 					<option value="15970"><?php print t('Industrial Engineering') ?></option>
 		            <option value="14864"><?php print t('Mechanical Engineering') ?></option>
 		            <option value="15968"><?php print t('Energy Engineering') ?></option>
@@ -57,27 +57,27 @@
 				</select>
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Documento</label>
+				<label for="" class="sr-only">Document</label>
 				<select name="tipo-documento" id="tipo-documento" class="form-control select-override">
-	            	<option value="">- Selecciona el tipo -</option>
+	            	<option value="">- Select a type -</option>
 	            	<option value="DNI">DNI</option>
 	                <option value="CE">Carné de extranjería</option>
-	                <option value="PA">Pasaporte</option>
-	                <option value="OT">Otro</option>
+	                <option value="PA">Passport</option>
+	                <option value="OT">Other</option>
 				</select>
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Documento</label>
+				<label for="" class="sr-only">Document</label>
 				<input  id="documento" name="documento" type="text" class="form-control" placeholder="Documento">
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-sm-12">
-				<label for="">Lugar de nacimiento</label>
+				<label for="">Place of Birth</label>
 			</div>
 			<div class="form-group col-sm-6">
-				<label for="" class="sr-only">Pais</label>
-				<input  id="pais" name="pais" type="text" class="form-control" placeholder="Pais">
+				<label for="" class="sr-only">Country</label>
+				<input  id="pais" name="pais" type="text" class="form-control" placeholder="Country">
 				<!--
 				<select name="pais" id="pais" class="form-control select-override">
 					<option value="País">País</option>
@@ -85,8 +85,8 @@
 				-->
 			</div>
 			<div class="form-group col-sm-6">
-				<label for="" class="sr-only">Ciudad</label>
-				<input  id="ciudad" name="ciudad" type="text" class="form-control" placeholder="Ciudad">
+				<label for="" class="sr-only">City</label>
+				<input  id="ciudad" name="ciudad" type="text" class="form-control" placeholder="City">
 				<!--
 				<select name="ciudad" id="ciudad" class="form-control select-override">
 					<option value="Ciudad">Ciudad</option>
@@ -96,11 +96,11 @@
 		</div>
 		<div class="row">
 			<div class="col-sm-12">
-				<label for="" class="block">Fecha de nacimiento</label>
+				<label for="" class="block">Date of Birth</label>
 			</div>
 			<div class="form-group col-sm-4">
 				<select name="dia" id="dia" class="form-control select-override">
-					<option value="">Día</option>
+					<option value="">Day</option>
 					<?php for ($x = 1; $x <= 31; $x++) { ?>
     					<?php echo "<option value='$x'>$x</option>";?> 
 					<?php } ?>
@@ -108,16 +108,16 @@
 			</div>
 			<div class="form-group col-sm-4">
 				<select name="mes" id="mes" class="form-control select-override">
-					<option value="">Mes</option>
-					<option value="Enero">Enero</option> <option value="Febrero">Febrero</option> <option value="Marzo">Marzo</option>
-					<option value="Abril">Abril</option> <option value="Mayo">Mayo</option> <option value="Junio">Junio</option>
-					<option value="Julio">Julio</option> <option value="Agosto">Agosto</option> <option value="Setiembre">Setiembre</option>
-					<option value="Octubre">Octubre</option> <option value="Noviembre">Noviembre</option> <option value="Diciembre">Diciembre</option>
+					<option value="">Month</option>
+					<option value="January">January</option> <option value="February">February</option> <option value="March">March</option>
+					<option value="April">April</option> <option value="May">May</option> <option value="June">June</option>
+					<option value="July">July</option> <option value="August">August</option> <option value="September">September</option>
+					<option value="October">October</option> <option value="November">November</option> <option value="December">December</option>
 				</select>
 			</div>
 			<div class="form-group col-sm-4">
 				<select name="ano" id="ano" class="form-control select-override">
-					<option value="">Año</option>
+					<option value="">Year</option>
 					<?php for ($x = 1970; $x <= 2015; $x++) { ?>
     					<?php echo "<option value='$x'>$x</option>";?> 
 					<?php } ?>
@@ -126,11 +126,11 @@
 		</div>
 		<div class="row">
 			<div class="col-sm-12">
-				<label for="" class="block">Domicilio actual</label>
+				<label for="" class="block">Address</label>
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Departamento</label>
-				<input  id="depto" name="depto" type="text" class="form-control" placeholder="Departamento">
+				<label for="" class="sr-only">Region</label>
+				<input  id="depto" name="depto" type="text" class="form-control" placeholder="Region">
 				<!--
 				<select name="depto" id="depto" class="form-control select-override">
 					<option value="Dpto">Dpto</option>
@@ -138,8 +138,8 @@
 				-->
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Provincia</label>
-				<input  id="provincia" name="provincia" type="text" class="form-control" placeholder="Provincia">
+				<label for="" class="sr-only">Province</label>
+				<input  id="provincia" name="provincia" type="text" class="form-control" placeholder="Province">
 				<!--
 				<select name="provincia" id="provincia" class="form-control select-override">
 					<option value="Provincia">Provincia</option>
@@ -147,8 +147,8 @@
 				-->
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Distrito</label>
-				<input  id="distrito" name="distrito" type="text" class="form-control" placeholder="Distrito">
+				<label for="" class="sr-only">District</label>
+				<input  id="distrito" name="distrito" type="text" class="form-control" placeholder="District">
 				<!--
 				<select name="distrito" id="distrito" class="form-control select-override">
 					<option value="Distrito">Distrito</option>
@@ -156,8 +156,8 @@
 				-->
 			</div>
 			<div class="col-sm-12 form-group">
-				<label for="" class="sr-only">Dirección actual</label>
-				<input  id="direccion-actual" name="direccion-actual" type="text" class="form-control" placeholder="Ingresa tu dirección actual">
+				<label for="" class="sr-only">Address</label>
+				<input  id="direccion-actual" name="direccion-actual" type="text" class="form-control" placeholder="Insert your address">
 			</div>
 			<div class="col-sm-7 form-group">
 				<input  id="email" name="email" type="email" class="form-control" placeholder="e-mail">
@@ -165,39 +165,39 @@
 		</div>
 		<div class="row">
 			<div class="col-sm-12">
-				<label for="" class="block">Teléfonos</label>
+				<label for="" class="block">Phone</label>
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Celular 1</label>
-				<input  id="celular-1" name="celular-1" type="text" class="form-control" placeholder="Celular 1">
+				<label for="" class="sr-only">Cell 1</label>
+				<input  id="celular-1" name="celular-1" type="text" class="form-control" placeholder="Cell 1">
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Celular 2</label>
-				<input  id="celular-2" name="celular-2" type="text" class="form-control" placeholder="Celular 2">
+				<label for="" class="sr-only">Cell 2</label>
+				<input  id="celular-2" name="celular-2" type="text" class="form-control" placeholder="Cell 2">
 			</div>
 			<div class="form-group col-sm-4">
 				<label for="" class="sr-only">Teléfono fijo</label>
-				<input  id="telefono-fijo" name="telefono" type="text" class="form-control" placeholder="Teléfono fijo">
+				<input  id="telefono-fijo" name="telefono" type="text" class="form-control" placeholder="Fix phone">
 			</div>
 		</div>
-		<h3 class="light">Estudios</h3>
+		<h3 class="light">Studies</h3>
 		<div class="row">
-			<div class="col-sm-12"><label for="">Año que culminó los estudios secundarios o bachillerato</label></div>
+			<div class="col-sm-12"><label for="">High school graduation year</label></div>
 			<div class="col-sm-3 form-group">
 				<select name="ano-culminacion" id="ano-culminacion" class="form-control select-override">
-					<option value="">Año</option>
+					<option value="">year</option>
 					<?php for ($x = 1970; $x <= 2015; $x++) { ?>
     					<?php echo "<option value='$x'>$x</option>";?> 
 					<?php } ?>
 				</select>
 			</div>
 			<div class="col-sm-9 form-group">
-				<label for="" class="sr-only">Colegio de procedencia</label>
-				<input  id="colegio-procedencia" name="colegio" type="text" class="form-control" placeholder="Colegio de procedencia">
+				<label for="" class="sr-only">High School</label>
+				<input  id="colegio-procedencia" name="colegio" type="text" class="form-control" placeholder="High School">
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Departamento</label>
-				<input  id="estudios-dpto" name="estudios-dpto" type="text" class="form-control" placeholder="Departamento">
+				<label for="" class="sr-only">Department</label>
+				<input  id="estudios-dpto" name="estudios-dpto" type="text" class="form-control" placeholder="Department">
 				<!--
 				<select name="estudios-dpto" id="estudios-dpto" class="form-control select-override">
 					<option value="Dpto">Dpto</option>
@@ -205,8 +205,8 @@
 				-->
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Provincia</label>
-				<input  id="estudios-provincia" name="estudios-provincia" type="text" class="form-control" placeholder="Provincia">
+				<label for="" class="sr-only">Province</label>
+				<input  id="estudios-provincia" name="estudios-provincia" type="text" class="form-control" placeholder="Province">
 				<!--
 				<select name="estudios-provincia" id="estudios-provincia" class="form-control select-override">
 					<option value="Provincia">Provincia</option>
@@ -214,8 +214,8 @@
 				-->
 			</div>
 			<div class="form-group col-sm-4">
-				<label for="" class="sr-only">Distrito</label>
-				<input  id="estudios-distrito" name="estudios-distrito" type="text" class="form-control" placeholder="Distrito">				
+				<label for="" class="sr-only">District</label>
+				<input  id="estudios-distrito" name="estudios-distrito" type="text" class="form-control" placeholder="District">				
 				<!--
 				<select name="estudios-distrito" id="estudios-distrito" class="form-control select-override">
 					<option value="Distrito">Distrito</option>
@@ -224,15 +224,15 @@
 			</div>
 			<div class="form-group col-sm-7">
 				<select name="estudios-rendimiento" id="estudios-rendimiento" class="form-control select-override">
-					<option value="">- Rendimiento Académico -</option>
-	                <option value="Primeros Puestos ( 1° y 2° )">Primeros Puestos ( 1° y 2° )</option>
-	                <option value="Tercio superior">Tercio superior</option>
-	                <option value="Otro">Otro</option>
+					<option value="">- Academic Performance -</option>
+	                <option value="Primeros Puestos ( 1° y 2° )">First place ( 1° y 2° )</option>
+	                <option value="Tercio superior">Third Superior</option>
+	                <option value="Otro">Other</option>
 				</select>
 			</div>
 			<div class="col-sm-12 form-group text-gray h4 thin">
-				<label for="">Es o ha sido estudiante de esta universidad:</label>
-				<input  id="estudiante-si" type="radio" checked name="estudiante" value="S"> Sí
+				<label for="">Have you been a student of this university:</label>
+				<input  id="estudiante-si" type="radio" checked name="estudiante" value="S"> yes
 				<input  id="estudiante-no" type="radio" name="estudiante" value ="N"> No
 			</div>
 		</div>
@@ -240,7 +240,7 @@
 			<div id="errordiv" class="col-sm-12 form-group"></div>			
 		</div>			
 		<div class="text-right">
-			<button  id="" type="submit" class="btn btn-primary btn-lg">Enviar</button>
+			<button  id="" type="submit" class="btn btn-primary btn-lg">Send</button>
 		</div>
 	</form>						
 </div>
