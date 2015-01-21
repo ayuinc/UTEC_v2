54504dde1508686b047fc3f2c01d49f375db7f23
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-20 20:31:10 -0500

    cambios

--- sites/all/themes/utec_theme/node--admision_calendario_academico.tpl.php
+++ sites/all/themes/utec_theme/node--admision_calendario_academico.tpl.php
@@ -2,7 +2,7 @@
 <?php $field_introduccion_calendario = $node->field_introduccion_calendario['und']['0']['value']; ?>
 
 <div class="container-sm">
-	<h1 class="light"><?php print t('Academic Calendar') ?></h1>
+	<h1 class="light"><?php print t('Admission Calendar') ?></h1>
 	<div class="separator-gray separator-lg"></div>
 	<h3><?php print $field_introduccion_calendario ?></h3>	
 	<div class="pv-35">
