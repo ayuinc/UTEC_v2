aa97d5550ab463edfad0a09833c73f6d8006b2e1
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-23 23:43:25 -0500

    Cambios

--- sites/all/themes/utec_theme/node--somos_utec_concejo_directivo.tpl.php
+++ sites/all/themes/utec_theme/node--somos_utec_concejo_directivo.tpl.php
@@ -20,9 +20,9 @@
     <?php $uid = $value['entity']->uid; ?>	
     <?php 
         if ($language->language == 'en') {
-          $profesor_url = "/" . $language->prefix . "/" . drupal_get_path_alias('page/30/consejo-directivo/'.$uid, $language->language); 
+          $profesor_url = "/" . $language->prefix . "/" . drupal_get_path_alias('a-solid-foundation/'.$uid, $language->language); 
         }else
-          $profesor_url = "/" . drupal_get_path_alias('page/30/consejo-directivo/'.$uid, $language->language); 
+          $profesor_url = "/" . drupal_get_path_alias('consejo-directivo/'.$uid, $language->language); 
       ?> 
 			<li class="mb-ch-14">
 				<div class="grid-list-pic"><img src="/<?php if($pic != ''){print($pathfile.'pictures/'.$pic);} else {print($pathfile.'pictures/'.'user.jpg'); }?>" alt="" width="98px" height="auto" class="img-circle"></div>
