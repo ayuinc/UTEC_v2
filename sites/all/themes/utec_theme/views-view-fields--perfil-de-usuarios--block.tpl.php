390730202fb542f0ea77679b44e041760a6f78f5
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-23 22:20:13 -0500

    Cambios

--- sites/all/themes/utec_theme/views-view-fields--perfil-de-usuarios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--perfil-de-usuarios--block.tpl.php
@@ -39,7 +39,7 @@
 <h3 class="thin"><?php print $name ?></h3>
 <a href="#" rel="nofollow" class="block"><em><?php print $twitter; ?></em></a>
 
-<?php if (($name!='Juan Incháustegui Vargas') AND ($name!='Eduardo Hochschild Beeck')) { ?>
+<?php if (($name!='Juan Incháustegui Vargas') AND ($name!='Eduardo Hochschild Beeck') AND ($name!='Mario Rivera Orams') AND ($name!='Dionisio Romero Paoletti') AND ($name!='Roque Benavides') AND ($name!='Oscar Espinosa Bedoya') AND ($name!='Oscar Espinosa Bedoya') AND ($name!='Kim Vandiver') AND ($name!='José Graña Miró Quesada') AND ($name!='Carlos Rodríguez-Pastor') AND ($name!='Mario Testino') AND ($name!='Oscar González Rocha') AND ($name!='Vito Rodríguez Rodríguez') AND ($name!='Ricardo Rizo Patrón de la Piedra') AND ($name!='Javier Durand Planas') AND ($name!='Yamal Zaidan Saba')) { ?>
 <a href="mail:<?php print $mail ?>" rel="nofollow" class="block"><em><?php print $mail ?></em></a>
 <?php } ?>
 
