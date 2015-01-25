71218bc68507c880375447233aeb9326eea2bf8b
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-25 17:14:23 -0500

    Cambios

--- sites/all/themes/utec_theme/node--edp_cursos_cortos_hijos.tpl.php
+++ sites/all/themes/utec_theme/node--edp_cursos_cortos_hijos.tpl.php
@@ -13,11 +13,11 @@
 <div class="container-sm">
 	<h1 class="light"><?php print $titulo ?></h1>
 	<div class="separator-gray separator-sm"></div>
-	<div class="pv-ch-7 lead-ch p-ch-gray">
+	<div class="pv-ch-7 p-ch-gray">
 		<p><?php print $cuerpo ?></p>
 	</div>
 	<?php if ($pdf) :?>
-	<div class="pv-ch-7 lead-ch p-ch-gray">
+	<div class="pv-ch-7 p-ch-gray">
 		<p><?php print t('See PDF:') ?></p>
 		<p><a href="<?php print('/'.$pathfile.$pdf); ?>"><?php print $pdf ?></a></p>
 	</div>
