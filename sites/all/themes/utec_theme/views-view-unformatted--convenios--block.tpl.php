d21bd8979b522f0580b92a36b28265dd6453eb36
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-25 15:06:25 -0500

    Cambios blog

--- sites/all/themes/utec_theme/views-view-unformatted--convenios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--convenios--block.tpl.php
@@ -6,7 +6,7 @@
   </div>
   <div class="container pv-ch-42">
     <div>
-      <ul class="grid-list grid-list-3 grid-list-1-xs grid-list-hover grid-list-hover-white">
+      <ul class="grid-list grid-list-2 grid-list-1-xs grid-list-hover grid-list-hover-white">
 				<?php foreach ($rows as $id => $row): ?>
 			    <?php print $row;?>
         <?php endforeach; ?>
