05b73bf835302dbed9882849bc2501f4b3d8f7ee
carlos calderon <cadermino@gmail.com>
2015-01-28 11:57:08 -0500

    blog posts

--- sites/all/themes/utec_theme/views-view-unformatted--blog--all-posts.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--blog--all-posts.tpl.php
@@ -3,7 +3,6 @@
 <?php elseif ($language->language == 'en'): ?>
 	<h3 class="light">All posts</h3>
 <?php endif ?>	
-<div class="separator-gray separator-sm"></div>
 <ul class="grid-list grid-list-2 row p-ch-7 mb-ch-21 isotope-grid">
 	<?php foreach ($rows as $id => $row): ?>
     <?php print $row;?>
