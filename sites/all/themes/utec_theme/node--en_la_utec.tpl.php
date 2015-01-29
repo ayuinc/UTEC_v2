c43c6742df145abde5bbca5a5749305c91476f28
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-29 18:04:32 -0500

    Cambios

--- sites/all/themes/utec_theme/node--en_la_utec.tpl.php
+++ sites/all/themes/utec_theme/node--en_la_utec.tpl.php
@@ -34,22 +34,18 @@
   $derecho_inferior_der_txt2_ingles = $fields[4]->field_texto_superior_2_ingles['und'][0]['value'];
   $derecho_inferior_der_link = $fields[4]->field_link['und'][0]['value'];
   $derecho_inferior_der_img  = $fields[4]->field_imagen['und'][0]['filename'];
-
 ?>
 <div>
   <div class="container">
     <div>
       <h1 class="lead text-gray-darker pt-21 mb-7 text-switcher">
         <?php print t('At UTEC, engineering') ?> 
-        
         <?php if ($language->language == 'en') { ?>
           <em class="text-gray" style="font-size: 28px;">is</em>
         <?php } elseif ($language->language  != 'en') { ?>
           <em class="text-gray" style="font-size: 28px;">es</em>
         <?php } ?>        
-        
         <p class="text-primary visible-xs-inline-block"><?php print t('well-being') ?></p>
-        
         <?php if ($language->language == 'en') { ?>
           <span class="text-primary hidden-xs" id="text-switcher-word-en"></span>
         <?php } elseif ($language->language  != 'en') { ?>
@@ -57,7 +53,6 @@
         <?php } ?>
       </h1>
     </div>
-  	<!--<h4 class="lead mt-0 mb-42">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>-->
     <div class="anchor-img-grid anchor-img-grid-1">
       <div class="col-sm-6">
         <div data-href="<?php print $base_url; ?>/<?php print $izquierdo_link; ?>" class="anchor-block-1">
--- sites/all/themes/utec_theme/views-view-unformatted--blog--blog-posts-destacados.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--blog--blog-posts-destacados.tpl.php
@@ -17,5 +17,4 @@
 <?php elseif ($language->language == 'en'): ?>
 	<a href="/en/blog/all-posts" class="btn btn-custom btn-primary see-more uppercase">Read all</a>
 <?php endif ?>	
-	
 </div>
