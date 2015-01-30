3f694787a6d8d963550616bab101832aff81df10
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-30 16:01:28 -0500

    Cambios

--- sites/all/themes/utec_theme/node--admision_centro_pre.tpl.php
+++ sites/all/themes/utec_theme/node--admision_centro_pre.tpl.php
@@ -24,13 +24,6 @@ $field_fotos_centro_pre = $node->field_fotos_centro_pre['und']['0']['filename'];
 $modalida_ext_int = $node->field_modalidad_extensiva_intens['und']['0']['value'];
 ?>
 
-
-<?php
-	//$unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' ); $titulo = strtr( $titulo, $unwanted_array );
-?>
-
-<?php print $pathfile ?>
-<?php print $field_fotos_centro_pre ?>
 	<div class="hero-unit"> <!-- HERO -->
 	  <?php if ($language->language == 'en'): ?>
 	  <div data-section-scroll="Overview" id="que-es" class="section-scroll-content bg-img-block bg-img-block-lg" style="background-image: url(<?php print('/'.$pathfile.'/centro-pre/'.$field_fotos_centro_pre)?>);">
