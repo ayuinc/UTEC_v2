04b9f9e6c67a94c183f182533e0bb8e9e7a5b912
carlos calderon <cadermino@gmail.com>
2015-01-30 15:15:49 -0500

    view in node

--- sites/all/themes/utec_theme/node--carreras_home.tpl.php
+++ sites/all/themes/utec_theme/node--carreras_home.tpl.php
@@ -33,7 +33,7 @@
   // set active display on the view
   $view->set_display('block');
   // set any needed arguments
-  $view->set_arguments(array(arg(2)));
+  //$view->set_arguments(array(arg(2)));
   // execute the view
   $view->execute();
   // display the results
