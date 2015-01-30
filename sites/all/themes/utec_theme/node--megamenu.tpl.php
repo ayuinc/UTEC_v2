2e3ee0e8207b6b3f4e4255a1dd893a5db649d33f
Herman Marin <hermanmarin@Hermans-MacBook-Pro.local>
2015-01-30 17:28:25 -0500

    Cambios

--- sites/all/themes/utec_theme/node--megamenu.tpl.php
+++ sites/all/themes/utec_theme/node--megamenu.tpl.php
@@ -568,7 +568,7 @@
 							<?php endif ?>
 							</a>
 						</li>
-						<li>
+						<!--<li>
 							<?php if ($language->language != 'en'): ?>
 								<a href="/edp/cursos-online" class="atm-menu-secundario">
 									<span>Cursos</span>Online
@@ -579,7 +579,7 @@
 								</a>
 							<?php endif ?>
 							</a>
-						</li>
+						</li>-->
 						<li>
 							<?php if ($language->language != 'en'): ?>
 								<a href="/edp/cursos-inhouse" class="atm-menu-secundario">
@@ -1109,7 +1109,7 @@
 							<?php endif ?>
 							</a>
 						</li>
-						<li>
+						<!--<li>
 							<?php if ($language->language != 'en'): ?>
 								<a href="/edp/cursos-online" class="atm-menu-secundario">
 									<span>Cursos</span>Online
@@ -1120,7 +1120,7 @@
 								</a>
 							<?php endif ?>
 							</a>
-						</li>
+						</li>-->
 						<li>
 							<?php if ($language->language != 'en'): ?>
 								<a href="/edp/cursos-inhouse" class="atm-menu-secundario">
