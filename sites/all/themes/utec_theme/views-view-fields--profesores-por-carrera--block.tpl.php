fcbe7bdb7f10a730eb55fddbd021e83d37434946
carlos calderon <cadermino@gmail.com>
2015-02-02 19:37:36 -0500

    profesores

--- sites/all/themes/utec_theme/views-view-fields--profesores-por-carrera--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--profesores-por-carrera--block.tpl.php
@@ -13,9 +13,8 @@
 	$pic = $user->picture->uri;
 	$desc = $user->field_descripci_n['und']['0']['value'];
   $carrera = taxonomy_get_term_by_name($term_name);
-  // $carrera_id = $carrera->tid;
+  $carrera_id = $carrera['0']->tid;
 
-  $carrera_id = array_shift($result[$carrera])->tid
 	// kpr($carrera_id);
 ?>
 
