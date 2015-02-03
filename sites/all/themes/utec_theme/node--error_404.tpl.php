107488411f533e994b584f0468c115258701465a
carlos calderon <cadermino@gmail.com>
2015-02-03 12:22:13 -0500

    profesores

--- sites/all/themes/utec_theme/node--error_404.tpl.php
+++ sites/all/themes/utec_theme/node--error_404.tpl.php
@@ -9,12 +9,12 @@ $body = $node->body['und'][0]['value'];
 
 ?>
 
-<div class="bg-img-block minh-630 flex-middle-center" style="background-image: url(/<?php print file_create_url($imagen); ?>)">
+<div class="bg-img-block minh-630 flex-middle-center" style="background-image: url(<?php print file_create_url($imagen); ?>)">
 	<div class="container-sm text-center lead-ch light">
 		<h1 class="text-white lead light uppercase"><?php print $titulo ?></h1>
 			<?php print $body ?>
 		<div class="pv-35">
-			<a href="#" class="btn btn-xl btn-white btn-outline uppercase bold see-more">Volver a página de inicio</a>
+			<a href="/" class="btn btn-xl btn-white btn-outline uppercase bold see-more">Volver a página de inicio</a>
 		</div>
 	</div>
 </div>
