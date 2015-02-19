8e55b136c86fc67119731d968b03f6cac3ba303e
carlos calderon <cadermino@gmail.com>
2015-02-18 19:26:30 -0500

    bloque sitemap

--- sites/all/themes/utec_theme/js/app/lib/searchform.js
+++ sites/all/themes/utec_theme/js/app/lib/searchform.js
@@ -5,13 +5,13 @@
     console.log($searchForm);
     overlayTempl = '<div class="form-overlay">' + '<div>' + '<a href="#" class="form-overlay-close">X</a>' + '<h3>Procesando información</h3>' + '<div class="mt-42">' + '<span class="spinner animated rotateIn infinite icon-spinner9"></span>' + '</div>' + '</div>' + '</div>';
     if ($searchForm.find('#edit-keys').val().length < 1) {
-      $searchForm.find('#edit-submit').addClass('disabled');
+      $searchForm.find('#edit-submit--2').addClass('disabled');
     }
     $searchForm.find('#edit-keys').on('keyup', function() {
       if ($searchForm.find('#edit-keys').val().length < 1) {
-        $searchForm.find('#edit-submit').addClass('disabled');
+        $searchForm.find('#edit-submit--2').addClass('disabled');
       } else {
-        $searchForm.find('#edit-submit').removeClass('disabled');
+        $searchForm.find('#edit-submit--2').removeClass('disabled');
       }
     });
   });
