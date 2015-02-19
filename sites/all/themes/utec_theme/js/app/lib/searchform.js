3b39c4c31101cbd393733047fa0910a57a89e1ec
carlos calderon <cadermino@gmail.com>
2015-02-18 19:24:47 -0500

    bloque sitemap

--- sites/all/themes/utec_theme/js/app/lib/searchform.js
+++ sites/all/themes/utec_theme/js/app/lib/searchform.js
@@ -1,6 +1,8 @@
 (function() {
   $(document).ready(function() {
+
     $searchForm = $('#search-form');
+    console.log($searchForm);
     overlayTempl = '<div class="form-overlay">' + '<div>' + '<a href="#" class="form-overlay-close">X</a>' + '<h3>Procesando información</h3>' + '<div class="mt-42">' + '<span class="spinner animated rotateIn infinite icon-spinner9"></span>' + '</div>' + '</div>' + '</div>';
     if ($searchForm.find('#edit-keys').val().length < 1) {
       $searchForm.find('#edit-submit').addClass('disabled');
