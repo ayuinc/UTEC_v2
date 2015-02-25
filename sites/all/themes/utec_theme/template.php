a7f7a9881ddfd3bb330f800028699867ea51a2c8
carlos calderon <cadermino@gmail.com>
2015-02-25 15:06:34 -0500

    sitemap

--- sites/all/themes/utec_theme/template.php
+++ sites/all/themes/utec_theme/template.php
@@ -191,5 +191,16 @@
 	//   'every_page' => TRUE,
 	//   'weight' => 18,
 	// ));		
+function utec_theme_site_map_menu_link(array $variables) {
+	$element['#attributes']['class'] = "test";
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
 
 ?>
