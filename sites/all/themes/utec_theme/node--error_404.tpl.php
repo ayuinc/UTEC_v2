531b7879be5f469b0844263fa7bf10f2def840c7
carlos calderon <cadermino@gmail.com>
2015-02-25 17:11:20 -0500

    sitemap

--- sites/all/themes/utec_theme/node--error_404.tpl.php
+++ sites/all/themes/utec_theme/node--error_404.tpl.php
@@ -11,13 +11,14 @@ $body = $node->body['und'][0]['value'];
 
 <div class="bg-img-block minh-630 flex-middle-center" style="background-image: url(<?php print file_create_url($imagen); ?>)">
 	<!-- <div class="container-sm text-center lead-ch light"> -->
-	<div class="container-sm text-center light text-gray ptp-14">
+	<div class="container-sm light text-gray ptp-14">
 		<h1 class="lead light uppercase text-gray-dark h4"><?php print $titulo ?></h1>
 			<?php print $body ?>
 		<div class="pv-35">
 			<a href="/" class="btn btn-xl btn-white btn-outline uppercase bold see-more"><?php print t('Get back to the homepage'); ?></a>
 		</div>
 		<div class="sitemap text-left pv-35" style="display:inline-block">
+			<h4><?php print t('Tambien puedes ir a:'); ?></h4>
 			<?php echo theme('site_map'); ?>
 		</div>
 	</div>
