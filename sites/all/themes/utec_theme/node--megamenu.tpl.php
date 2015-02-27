abad1a76d6aef6aeed95aa9e5314f7d40d9788fc
carlos calderon <cadermino@gmail.com>
2015-02-27 10:18:53 -0500

    menu administrable

--- sites/all/themes/utec_theme/node--megamenu.tpl.php
+++ sites/all/themes/utec_theme/node--megamenu.tpl.php
@@ -1,6 +1,11 @@
 <?php global $language ?>
 <?php $theme_path = drupal_get_path('theme', 'utec_theme'); ?>
 
+<?php 
+	$menus = menu_tree_all_data('main-menu'); 
+	print_r($menus);
+?>
+
 <div id="header" class="hidden-xs hidden-sm"> <!-- HEADER -->
 	<div class="tool-bar bg-gray-lighter pt-sm pb-sm">
 		<div class="container">
