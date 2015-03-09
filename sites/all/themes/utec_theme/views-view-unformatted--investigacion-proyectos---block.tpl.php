371e9886cc7288295694f39f2dce515482569dd1
carlos calderon <cadermino@gmail.com>
2015-03-09 15:15:24 -0500

    edp

--- sites/all/themes/utec_theme/views-view-unformatted--investigacion-proyectos---block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--investigacion-proyectos---block.tpl.php
@@ -11,10 +11,10 @@
 <div class="container-sm mb-35">
 	<h1 class="light"><?php print t('Projects') ?></h1>
 	<div class="separator-gray separator-lg"></div>
-		<?php foreach ($rows as $id => $row): ?>
-	    <?php print $row; break?>
+		<?php //foreach ($rows as $id => $row): ?>
+	    <?php //print $row; break?>
 
-		<?php endforeach; ?>
+		<?php //endforeach; ?>
 		<ul class="grid-list grid-list-2 grid-list-1-xs mb-ch-21 isotope-grid normalize-text list-hover list-hover-gray-lighter p-ch-gray">
 	    <?php foreach ($rows as $id => $row): ?>
 		    <?php if($id != 0) : ?>
