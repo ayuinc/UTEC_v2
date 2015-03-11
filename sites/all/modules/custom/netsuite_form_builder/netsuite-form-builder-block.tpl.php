ab95950746febf7db0680e41fc7630853636de82
Chasky <joel.porras@softwinperu.com>
2015-03-11 17:53:09 -0500

    fix noscript

--- sites/all/modules/custom/netsuite_form_builder/netsuite-form-builder-block.tpl.php
+++ sites/all/modules/custom/netsuite_form_builder/netsuite-form-builder-block.tpl.php
@@ -1,2 +1,2 @@
 <iframe src="about:blank" data-iframe-src="<?php print $path_netsuite . '&compid=' . $comp_id . '&formid=' . $form_id . $parameters . '&origin=' . $origin; ?>"></iframe>
-<noscript><iframe src="<?php print $path_netsuite . '&compid=' . $comp_id . '&formid=' . $form_id . $parameters . '&origin=' . $origin; ?>"></iframe><noscript>
+<noscript><iframe src="<?php print $path_netsuite . '&compid=' . $comp_id . '&formid=' . $form_id . $parameters . '&origin=' . $origin; ?>"></iframe></noscript>
