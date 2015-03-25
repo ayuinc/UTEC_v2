601e98551c73da1bf178d24af19454af06a2ec09
carlos calderon <cadermino@gmail.com>
2015-03-25 17:12:04 -0500

    megamenu administrable

--- sites/all/themes/utec_theme/node--megamenu.tpl.php
+++ sites/all/themes/utec_theme/node--megamenu.tpl.php
@@ -146,7 +146,8 @@
 	    $titulo_ing = $field_ing[$num_ing]->field_titulo_menu['und'][0]['value'];
 	    $cuerpo_ing = $field_ing[$num_ing]->field_cuerpo['und'][0]['value'];
 	    
-	    kpr($titulo_ing);
+	    kpr($num_ing);
+	    kpr($field_ing);
 	  ?>
 	<div class="nav-display" id="nav-display-<?php print $count; ?>">
 		<div class="container relative">
