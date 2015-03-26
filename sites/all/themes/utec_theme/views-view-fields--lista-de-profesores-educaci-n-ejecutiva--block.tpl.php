2ecbc5a5734ee33c9e00c5dfbec3fb9357a7b870
Angelpm28 <angelpa38@gmail.com>
2015-03-26 10:54:39 -0500

    Lista de Profesores Educacion Ejecutiva v4

--- sites/all/themes/utec_theme/views-view-fields--lista-de-profesores-educaci-n-ejecutiva--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--lista-de-profesores-educaci-n-ejecutiva--block.tpl.php
@@ -5,7 +5,7 @@
   $path = $base_url.'/'.$theme_path;
   $pathfile = variable_get('file_public_path', conf_path() . '/files/'); 
 
-  kpr($fields);
+  print_r($fields);
 
 	$uid = $fields['field_profesor']->content;
 	$user = user_load($uid);
