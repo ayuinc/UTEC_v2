e982a9d51e6bc10ead35ebbcb459ccd6d826e9f3
Angelpm28 <angelpa38@gmail.com>
2015-03-26 11:44:20 -0500

    Lista de Profesores Educacion Ejecutiva v9

--- sites/all/themes/utec_theme/views-view-unformatted--lista-de-profesores-educaci-n-ejecutiva--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--lista-de-profesores-educaci-n-ejecutiva--block.tpl.php
@@ -2,7 +2,7 @@
   <h1 class="light"><?php print t('Executive Education Faculty') ?></h1>
   <div class="separator-gray separator-lg"></div>
   <p class="lead text-gray"><?php //print $field_introduccion_autoridades ?></p>
-  <ul class="pv-35 grid-list grid-list-2 grid-list-1-xs">
+  <ul class="grid-list grid-list-3 grid-list-1-xs isotope-grid grid-list-item-height grid-list-anchors pv-21 text-center" style="position: relative; height: 3080px;">
   	<?php foreach ($rows as $id => $row): ?>
 	    <?php print $row;?>
 	  <?php endforeach; ?>
