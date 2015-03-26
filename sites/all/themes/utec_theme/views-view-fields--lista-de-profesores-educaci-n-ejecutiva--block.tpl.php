5539c6209cea1fd7ac9699dff40c309a489695c1
Angelpm28 <angelpa38@gmail.com>
2015-03-26 11:41:41 -0500

    Lista de Profesores Educacion Ejecutiva v9

--- sites/all/themes/utec_theme/views-view-fields--lista-de-profesores-educaci-n-ejecutiva--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--lista-de-profesores-educaci-n-ejecutiva--block.tpl.php
@@ -15,14 +15,12 @@
     $profesor_url = "/" . drupal_get_path_alias('page/67/profesor-edp/'.$uid, $language->language); 
 ?>
 
-<li data-href="<?php print $profesor_url ?>">
-  <div class="user-card user-card-round size lg">
-    <div class="user-pic">
+<li class="mb-ch-14 isotope-item" data-href="<?php print $profesor_url ?>" style="position: absolute; left: 0px; top: 0px;">
+    <div class="grid-list-pic">
       <!--img src="<?php if($pic != ''){ print ($pic); } else { print ($pathfile.'pictures/'.$pic); } ?>" alt="<?php print $name; ?>"-->
       <?php print $pic; ?>
     </div>
     <div class="user-info">
       <a href="<?php print $profesor_url ?>"><?php print $name ?></a>
     </div>
-  </div>
 </li>
