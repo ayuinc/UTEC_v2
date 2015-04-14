a2cc62bb62311b53f773c1455baab2be761b638e
carlos calderon <cadermino@gmail.com>
2015-04-13 19:23:50 -0500

    megamenu

--- sites/all/themes/utec_theme/node--menumobile.tpl.php
+++ sites/all/themes/utec_theme/node--menumobile.tpl.php
@@ -18,8 +18,7 @@
 	$fields_esp = $node->field_menu_content_esp['und'];
 	$fields_ing = $node->field_menu_content_ing['und'];
 
-	$count = 1;
-	$count2 = 1;
+
 ?>
 
 <div class="mobile-nav-display-triggers"> 
@@ -38,68 +37,68 @@
 </div>
 <?php $count = 1; ?>
 <?php foreach ($fields_esp as $index => $fid) : ?>
-<?php 
-  // print_r($fid['value']);
-  $num_esp = $fid['value'];
-  $num_ing = $fields_ing[$index]['value'];
-  $field_esp = entity_load('field_collection_item', array($num_esp));
-  $field_ing = entity_load('field_collection_item', array($num_ing));
+	<?php 
+	  // print_r($fid['value']);
+	  $num_esp = $fid['value'];
+	  $num_ing = $fields_ing[$index]['value'];
+	  $field_esp = entity_load('field_collection_item', array($num_esp));
+	  $field_ing = entity_load('field_collection_item', array($num_ing));
 
-  $titulo_esp = $field_esp[$num_esp]->field_titulo_menu['und'][0]['value'];
-  $cuerpo_esp = $field_esp[$num_esp]->field_cuerpo['und'][0]['value'];
-  
-  $titulo_ing = $field_ing[$num_ing]->field_titulo_menu['und'][0]['value'];
-  $cuerpo_ing = $field_ing[$num_ing]->field_cuerpo['und'][0]['value'];
-?>
-<div class="mobile-nav-display" id="mobile-nav-display-<?php print $count; ?>">
-  <a class="back">Regresar</a>
-	<?php if ($language->language == 'es'): ?>
-		<?php print $titulo_esp ?>
-		<?php print $cuerpo_esp ?>
-	<?php elseif ($language->language == 'en'): ?>
-		<?php print $titulo_ing ?>
-		<?php print $cuerpo_ing ?>
-	<?php endif ?>
-	<ul>
-	<?php foreach ($menus as $index => $menu) :?>
-  <?php if ($menu['#href']) : ?>
-		<?php $count2 = 0; ?>
-		<?php foreach ($menu['#below'] as $child) : ?>
-    <?php if ($child['#href']) : ?>
-      <?php
-        $titles = array();  
-        $titles = explode(" ", $child['#title']);
-        $href = drupal_get_path_alias($child['#href'], $language->language);
-        $classes = $child['#localized_options']['attributes']['class'];
-        $title = $child['#localized_options']['attributes']['title'];
-        $id = $child['#localized_options']['attributes']['id'];
-        $name = $child['#localized_options']['attributes']['name'];
-        $rel = $child['#localized_options']['attributes']['rel'];
-        $target = $child['#localized_options']['attributes']['target'];
-      ?>
-			<?php if ($count2 <= 5) : ?>
-				<li>
-					<a href="/<?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" class="atm-menu-secundario-movil"><span><?php print $titles2[0]; ?></span><?php print $titles2[1]; ?> <?php print $titles2[2]; ?> <?php print $titles2[3]; ?> <?php print $titles2[4]; ?></a>
-				</li>
-			<?php elseif($count2 > 5 && $count2 < 7) : ?>
-			</ul>
-			<ul>
-        <li>
-					<a href="/<?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" class="atm-menu-secundario-movil"><span><?php print $titles2[0]; ?></span><?php print $titles2[1]; ?> <?php print $titles2[2]; ?> <?php print $titles2[3]; ?> <?php print $titles2[4]; ?></a>
-				</li>
-			<?php else : ?>
-        <li>
-					<a href="/<?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" class="atm-menu-secundario-movil"><span><?php print $titles2[0]; ?></span><?php print $titles2[1]; ?> <?php print $titles2[2]; ?> <?php print $titles2[3]; ?> <?php print $titles2[4]; ?></a>
-				</li>
-      <?php endif; ?>
-		<?php endif; ?>
-		<?php $count2++; ?>
-    <?php endforeach; ?>
-    <?php unset($menus[$index]); ?>	
-		<?php break; ?>
-  <?php endif; ?>  
-	<?php endforeach; ?> 
-	</ul>
-	<?php $count++; ?>
-</div>
+	  $titulo_esp = $field_esp[$num_esp]->field_titulo_menu['und'][0]['value'];
+	  $cuerpo_esp = $field_esp[$num_esp]->field_cuerpo['und'][0]['value'];
+	  
+	  $titulo_ing = $field_ing[$num_ing]->field_titulo_menu['und'][0]['value'];
+	  $cuerpo_ing = $field_ing[$num_ing]->field_cuerpo['und'][0]['value'];
+	?>
+	<div class="mobile-nav-display" id="mobile-nav-display-<?php print $count; ?>">
+	  <a class="back">Regresar</a>
+		<?php if ($language->language == 'es'): ?>
+			<?php print $titulo_esp ?>
+			<?php print $cuerpo_esp ?>
+		<?php elseif ($language->language == 'en'): ?>
+			<?php print $titulo_ing ?>
+			<?php print $cuerpo_ing ?>
+		<?php endif ?>
+		<ul>
+		<?php foreach ($menus as $index => $menu) :?>
+	  <?php if ($menu['#href']) : ?>
+			<?php $count2 = 0; ?>
+			<?php foreach ($menu['#below'] as $child) : ?>
+	    <?php if ($child['#href']) : ?>
+	      <?php
+	        $titles = array();  
+	        $titles = explode(" ", $child['#title']);
+	        $href = drupal_get_path_alias($child['#href'], $language->language);
+	        $classes = $child['#localized_options']['attributes']['class'];
+	        $title = $child['#localized_options']['attributes']['title'];
+	        $id = $child['#localized_options']['attributes']['id'];
+	        $name = $child['#localized_options']['attributes']['name'];
+	        $rel = $child['#localized_options']['attributes']['rel'];
+	        $target = $child['#localized_options']['attributes']['target'];
+	      ?>
+				<?php if ($count2 <= 5) : ?>
+					<li>
+						<a href="/<?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" class="atm-menu-secundario-movil"><span><?php print $titles2[0]; ?></span><?php print $titles2[1]; ?> <?php print $titles2[2]; ?> <?php print $titles2[3]; ?> <?php print $titles2[4]; ?></a>
+					</li>
+				<?php elseif($count2 > 5 && $count2 < 7) : ?>
+				</ul>
+				<ul>
+	        <li>
+						<a href="/<?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" class="atm-menu-secundario-movil"><span><?php print $titles2[0]; ?></span><?php print $titles2[1]; ?> <?php print $titles2[2]; ?> <?php print $titles2[3]; ?> <?php print $titles2[4]; ?></a>
+					</li>
+				<?php else : ?>
+	        <li>
+						<a href="/<?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" class="atm-menu-secundario-movil"><span><?php print $titles2[0]; ?></span><?php print $titles2[1]; ?> <?php print $titles2[2]; ?> <?php print $titles2[3]; ?> <?php print $titles2[4]; ?></a>
+					</li>
+	      <?php endif; ?>
+			<?php endif; ?>
+			<?php $count2++; ?>
+	    <?php endforeach; ?>
+	    <?php unset($menus[$index]); ?>	
+			<?php break; ?>
+	  <?php endif; ?>  
+		<?php endforeach; ?> 
+		</ul>
+		<?php $count++; ?>
+	</div>
 <?php endforeach; ?>
