56275a72dbb0fb256fde3a617ea88002ff9845a3
carlos calderon <cadermino@gmail.com>
2015-04-19 22:31:30 -0500

    imagen cache

--- sites/all/themes/utec_theme/node--megamenu.tpl.php
+++ sites/all/themes/utec_theme/node--megamenu.tpl.php
@@ -164,6 +164,7 @@
   						<?php if ($count2 <= 5) : ?>
   							<li>
                   <?php $external = substr($child['#href'], 0, 4); ?>
+                  <?php print_r($external); ?>
   								<a href="<?php if ($external != 'http'){print '/';} ?><?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" 
                      class="<?php foreach ($classes as $key => $class){print $class.' ';
                   } ?>atm-menu-secundario" 
