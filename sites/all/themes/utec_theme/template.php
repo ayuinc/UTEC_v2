4345c73c660bdb2d73cd078c160a08d66aabc1cc
carlos calderon <cadermino@gmail.com>
2015-04-23 18:08:24 -0500

    imagen cache

--- sites/all/themes/utec_theme/template.php
+++ sites/all/themes/utec_theme/template.php
@@ -234,6 +234,7 @@ function utec_theme_menu_link(array $variables) {
 	  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
 	  
   }
+  kpr($element['#attributes']);
 	  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
 
