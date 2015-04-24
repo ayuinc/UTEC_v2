8cb6faa5fdab74379690b3820593e1f09a5eeebb
carlos calderon <cadermino@gmail.com>
2015-04-23 19:09:12 -0500

    imagen cache

--- sites/all/themes/utec_theme/node--megamenu.tpl.php
+++ sites/all/themes/utec_theme/node--megamenu.tpl.php
@@ -232,36 +232,14 @@
             <?php $count2 = 0; ?>
             <?php foreach ($menu['#below'] as $child) : ?>
             <?php if ($child['#href']) : ?>
-              <?php
-                $titles = array();  
-                $titles = explode(" ", $child['#title']);
-                $href = drupal_get_path_alias($child['#href'], $language->language);
-                $classes = $child['#localized_options']['attributes']['class'];
-                $title = $child['#localized_options']['attributes']['title'];
-                $id = $child['#localized_options']['attributes']['id'];
-                $name = $child['#localized_options']['attributes']['name'];
-                $rel = $child['#localized_options']['attributes']['rel'];
-                $target = $child['#localized_options']['attributes']['target'];
-                $external = '';
-                $external = substr($child['#href'], 0, 4);
-              ?>
               <?php if ($count2 <= 5) : ?>
-                <li>
-                  <a href="<?php if ($external != 'http'){print '/';} ?><?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" class="<?php foreach ($classes as $key => $class){print $class.' ';
-                  } ?>atm-menu-secundario" title="<?php print $title; ?>" id="<?php print $id; ?>" name="<?php print $name; ?>" rel="<?php print $rel; ?>" target="<?php print $target; ?>"><span class="<?php if ($classes){print 'text-white';} ?>" style="<?php if ($classes){print 'color:white';} ?>"><?php print $titles[0]; ?></span><?php print $titles[1]; ?> <?php print $titles[2]; ?> <?php print $titles[3]; ?> <?php print $titles[4]; ?></a>
-                </li>
+                <?php print render($child); ?>
               <?php elseif($count2 > 5 && $count2 < 7) : ?>
               </ul>
               <ul>
-                <li>
-                  <a href="<?php if ($external != 'http'){print '/';} ?><?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" class="<?php foreach ($classes as $key => $class){print $class.' ';
-                  } ?>atm-menu-secundario" title="<?php print $title; ?>" id="<?php print $id; ?>" name="<?php print $name; ?>" rel="<?php print $rel; ?>" target="<?php print $target; ?>"><span class="<?php if ($classes){print 'text-white';} ?>" style="<?php if ($classes){print 'color:white';} ?>"><?php print $titles[0]; ?></span><?php print $titles[1]; ?> <?php print $titles[2]; ?> <?php print $titles[3]; ?> <?php print $titles[4]; ?></a>
-                </li>
+                <?php print render($child); ?>
               <?php else : ?>
-                <li>
-                  <a href="<?php if ($external != 'http'){print '/';} ?><?php if($language->language == 'en'){print ($language->language."/");} print $href; ?>" class="<?php foreach ($classes as $key => $class){print $class.' ';
-                  } ?>atm-menu-secundario" title="<?php print $title; ?>" id="<?php print $id; ?>" name="<?php print $name; ?>" rel="<?php print $rel; ?>" target="<?php print $target; ?>"><span class="<?php if ($classes){print 'text-white';} ?>" style="<?php if ($classes){print 'color:white';} ?>"><?php print $titles[0]; ?></span><?php print $titles[1]; ?> <?php print $titles[2]; ?> <?php print $titles[3]; ?> <?php print $titles[4]; ?></a>
-                </li>
+                <?php print render($child); ?>
               <?php endif; ?>
             <?php endif; ?>
             <?php $count2++; ?>
