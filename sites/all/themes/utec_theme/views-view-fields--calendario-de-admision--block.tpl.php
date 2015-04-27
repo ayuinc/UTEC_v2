0c21131d1c41c49203a0957893f9dc4f429a2ac8
carlos calderon <cadermino@gmail.com>
2015-04-27 15:17:57 -0500

    calendario de admision

--- sites/all/themes/utec_theme/views-view-fields--calendario-de-admision--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--calendario-de-admision--block.tpl.php
@@ -9,16 +9,20 @@
 	$eventos = explode(" ", $fields['field_texo']->content);
 	$fechas = explode(" ", $fields['field_fecha']->content);
   // kpr($fields['field_texo']);
-
+	$count = 0;
 ?>
 
+<?php foreach ($eventos as $key => $evento) : ?>
 <tr>
+	<?php if ($count == 0) : ?>
 	<td class="highlight"><?php print $name; ?></td>
-	<?php foreach ($eventos as $key => $evento) : ?>
+	<?php endif; 
+		$count ++;
+	?>
 		<td><?php print $evento; ?></td>
-		<td><?php print $fechas[$key]; ?></td>
-	<?php endforeach; ?>
+		<td><?php print $fechas[$key]; ?></td>	
 </tr>
+<?php endforeach; ?>
 
 
 
