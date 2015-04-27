6c5fe2f4b947709052ebab46444be9ea42d33a48
carlos calderon <cadermino@gmail.com>
2015-04-27 15:12:01 -0500

    calendario de admision

--- sites/all/themes/utec_theme/views-view-fields--calendario-de-admision--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--calendario-de-admision--block.tpl.php
@@ -6,15 +6,18 @@
   $pathfile= variable_get('file_public_path', conf_path() . '/files/');
 
 	$name = $fields['title']->content;
-	$evento = $fields['field_texo']->content;
-	$fecha = $fields['field_fecha']->content;
-  kpr($fields['field_texo']);
+	$eventos = explode(" ", $fields['field_texo']->content);
+	$fechas = explode(" ", $fields['field_fecha']->content);
+  // kpr($fields['field_texo']);
+
 ?>
 
 <tr>
 	<td class="highlight"><?php print $name; ?></td>
-	<td><?php print $evento; ?></td>
-	<td><?php print $fecha; ?></td>
+	<?php foreach ($eventos as $key => $evento) : ?>
+		<td><?php print $evento; ?></td>
+		<td><?php print $fechas[$key]; ?></td>
+	<?php endforeach; ?>
 </tr>
 
 
