e18bcaaf90d1c0dd0e0a389d24b834c0ef3c3d0d
carlos calderon <cadermino@gmail.com>
2015-04-28 17:44:58 -0500

    image cache

--- sites/all/themes/utec_theme/node--edp_curso_corto_banner_venta.tpl.php
+++ sites/all/themes/utec_theme/node--edp_curso_corto_banner_venta.tpl.php
@@ -4,7 +4,7 @@
   $path = $base_url.'/'.$theme_path;
   $pathfile= variable_get('file_public_path', conf_path() . '/files/');
 
-	$imagen = $node->field_banner['und']['0']['filename'];
+	$imagen = $node->field_image['und']['0']['uri'];
 
 	// print_r($node);
 ?>
@@ -15,7 +15,7 @@
 		  <div class="carousel-inner" role="listbox">
 		  	<div class="item active">
 			  	<div class="banner banner-label-bottom mb-14 ml-0">
-						<div class="banner-pic" style="background-image: url(/sites/default/files/<?php print $imagen ?>);">
+						<div class="banner-pic" style="<?php print image_style_url($image_style, $imagen); ?>);">
 						</div>
 					</div>
 			  </div>
