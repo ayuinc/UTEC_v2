6ce7a8f277e1bc9e199a3f066137c12e20d2ece4
carlos calderon <cadermino@gmail.com>
2015-05-06 17:48:24 -0500

    footer administrable

--- sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
+++ sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
@@ -1,122 +1,30 @@
-<?php global $language ?>
+<?php 
+	global $language 
+  $title = $node->title;
+  $cuerpo = $node->body['und']['0']['value'];
+  $carrera = $node->field_nombre['und']['0']['value'];
+  $link = $node->field_carrera_link['und']['0']['value'];
+  $iconos = $node->field_texo['und']['0']['value'];
+
+  $titles = explode(' ', $title)
+  $titles1 = explode(' ', $title, 2)
+  print_r($titles1);
+?>
 <div class="pv-42" id="home-sec-1">
 	<div class="container">
-		<h3 class="h2 lead medium mb-7"><span class="text-gray-darker"><?php print t('Ready to') ?></span> <span class="text-gray"><?php print t('change the world?') ?></span></h3>
-		<h4 class="lead mb-35"><?php print t('These are our undergraduate programs:') ?></h4>
+		<h3 class="h2 lead medium mb-7"><span class="text-gray-darker"><?php print $titles[0].'/'.$titles[1]; ?></span> <span class="text-gray"><?php foreach ($titles as $key => $title){if($title[2]){print $title;}} ?></span></h3>
+		<h4 class="lead mb-35"><?php print $cuerpo; ?></h4>
 		<ul class="grid-list grid-list-5 grid-list-1-xs grid-list-1-landscape pt-21 list-carreras text-center text-left-xs mb-ch-21-xs">
 			<li>
 				<div>
-					<?php if ($language->language == 'en'): ?>
-					<div><a href="/<?php print $language->language ?>/industrial-engineering" class="atm-item-carrera"><i class="icon-ing-industrial"></i></a></div>
+					<div><a href="/<?php print $language->language.'/'.$link; ?>" class="atm-item-carrera"><i class="<?php print $iconos; ?>"></i></a></div>
 					<h2>
-						<a href="/<?php print $language->language ?>/industrial-engineering" class="atm-item-carrera">
-							<span><?php print t('Industrial') ?></span>
-							<span><?php print t('Engineering') ?></span>
-							<span></span>
+						<a href="/<?php print $language->language.'/'.$link; ?>" class="atm-item-carrera">
+							<?php foreach ($carrera as $key => $part) : ?>
+							<span><?php print $part; ?></span>
+							<?php endforeach; ?>
 						</a>
 					</h2>
-					<?php elseif ($language->language  != 'en'): ?>
-					<div><a href="/ingenieria-industrial" class="atm-item-carrera"><i class="icon-ing-industrial"></i></a></div>
-					<h2>
-						<a href="/ingenieria-industrial" class="atm-item-carrera">
-							<span><?php print t('Engineering') ?></span>
-							<span><?php print t('Industrial') ?></span>
-							<span></span>
-						</a>
-					</h2>		
-					<?php endif ?>
-				</div>
-			</li>
-			<li>
-				<div>
-					<?php if ($language->language == 'en'): ?>
-					<div><a href="/<?php print $language->language ?>/energy-engineering" class="atm-item-carrera"><i class="icon-ing-energia"></i></a></div>
-					<h2>
-						<a href="/<?php print $language->language ?>/energy-engineering" class="atm-item-carrera">
-							<span><?php print t('Energy') ?></span>
-							<span><?php print t('Engineering') ?></span>
-							<span></span>
-						</a>
-					</h2>
-							<?php elseif ($language->language  != 'en'): ?>
-					<div><a href="/ingenieria-de-la-energia" class="atm-item-carrera"><i class="icon-ing-energia"></i></a></div>
-					<h2>
-						<a href="/ingenieria-de-la-energia" class="atm-item-carrera">
-								<span><?php print t('Engineering') ?></span>
-								<span><?php print t('Energy') ?></span>
-								<span></span>
-							<?php endif ?>
-						</a>
-					</h2>
-				</div>
-			</li>
-			<li>
-				<div>
-					<?php if ($language->language == 'en'): ?>
-					<div><a href="/<?php print $language->language ?>/industrial-chemical-engineering" class="atm-item-carrera"><i class="icon-ing-quimica"></i></a></div>
-					<h2>
-						<a href="/<?php print $language->language ?>/industrial-chemical-engineering" class="atm-item-carrera">
-							<span><?php print t('Industrial') ?></span>
-							<span><?php print t('Chemical') ?></span>
-							<span><?php print t('Engineering') ?></span>
-						</a>
-					</h2>			
-					<?php elseif ($language->language  != 'en'): ?>
-					<div><a href="/ingenieria-quimica-industrial" class="atm-item-carrera"><i class="icon-ing-quimica"></i></a></div>
-					<h2>
-						<a href="/ingenieria-quimica-industrial" class="atm-item-carrera">
-							<span><?php print t('Engineering') ?></span>
-							<span><?php print t('Chemical') ?></span>
-							<span><?php print t('Industrial') ?></span>
-						</a>
-					</h2>
-					<?php endif ?>
-				</div>
-			</li>
-			<li>
-				<div>
-					<?php if ($language->language == 'en'): ?>
-					<div><a href="/<?php print $language->language ?>/electronic-engineering" class="atm-item-carrera"><i class="icon-ing-electronica"></i></a></div>
-					<h2>
-						<a href="/<?php print $language->language ?>/electronic-engineering" class="atm-item-carrera">
-							<span>Electronic</span>
-							<span>Engineering</span>
-							<span></span>
-						</a>
-					</h2>			
-					<?php elseif ($language->language  != 'en'): ?>
-					<div><a href="/ingenieria-electronica" class="atm-item-carrera"><i class="icon-ing-electronica"></i></a></div>
-					<h2>	
-						<a href="/ingenieria-electronica" class="atm-item-carrera">		
-							<span>Ingeniería</span>
-							<span>Electrónica</span>
-							<span></span>
-						</a>		
-					</h2>			
-					<?php endif ?>
-				</div>
-			</li>
-			<li>
-				<div>
-					<?php if ($language->language == 'en'): ?>
-					<div><a href="/<?php print $language->language ?>/mechanical-engineering" class="atm-item-carrera"><i class="icon-ing-mecanica"></i></a></div>
-					<h2>
-						<a href="/<?php print $language->language ?>/mechanical-engineering" class="atm-item-carrera">
-							<span><?php print t('Mechanical') ?></span>
-							<span><?php print t('Engineering') ?></span>
-							<span></span>
-						</a>
-					</h2>			
-					<?php elseif ($language->language  != 'en'): ?>
-					<div><a href="/ingenieria-mecanica" class="atm-item-carrera"><i class="icon-ing-mecanica"></i></a></div>
-					<h2>
-						<a href="/ingenieria-mecanica" class="atm-item-carrera">
-							<span><?php print t('Engineering') ?></span>
-							<span><?php print t('Mechanical') ?></span>
-							<span></span>
-						</a>
-					</h2>
-					<?php endif ?>
 				</div>
 			</li>
 		</ul>
