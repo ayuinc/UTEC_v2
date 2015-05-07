94d943731df4d196c848c26cd0879d6e63f217b4
carlos calderon <cadermino@gmail.com>
2015-05-06 19:14:53 -0500

    footer administrable

--- sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
+++ sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
@@ -1,10 +1,10 @@
 <?php 
-	global $language 
+	global $language; 
   $title = $node->title;
   $cuerpo = $node->body['und']['0']['value'];
-  $carrera = $node->field_nombre['und']['0']['value'];
-  $link = $node->field_carrera_link['und']['0']['value'];
-  $iconos = $node->field_texo['und']['0']['value'];
+  $carreras = $node->field_nombre['und'];
+  $links = $node->field_carrera_link['und'];
+  $iconos = $node->field_texo['und'];
 
   $titles = explode(' ', $title)
   $titles1 = explode(' ', $title, 2)
@@ -12,14 +12,16 @@
 ?>
 <div class="pv-42" id="home-sec-1">
 	<div class="container">
-		<h3 class="h2 lead medium mb-7"><span class="text-gray-darker"><?php print $titles[0].'/'.$titles[1]; ?></span> <span class="text-gray"><?php foreach ($titles as $key => $title){if($title[2]){print $title;}} ?></span></h3>
+		<h3 class="h2 lead medium mb-7"><span class="text-gray-darker"><?php print $titles[0].'/'.$titles[1].'/'.$titles[2]; ?></span> <span class="text-gray"><?php foreach ($titles as $key => $title){if($title[3]){print $title;}} ?></span></h3>
 		<h4 class="lead mb-35"><?php print $cuerpo; ?></h4>
 		<ul class="grid-list grid-list-5 grid-list-1-xs grid-list-1-landscape pt-21 list-carreras text-center text-left-xs mb-ch-21-xs">
+			<?php $ca ?>
+			<?php foreach ($links as $key => $link) : ?>
 			<li>
 				<div>
-					<div><a href="/<?php print $language->language.'/'.$link; ?>" class="atm-item-carrera"><i class="<?php print $iconos; ?>"></i></a></div>
+					<div><a href="/<?php print $language->language.'/'.$links['value']; ?>" class="atm-item-carrera"><i class="<?php print $iconos[$key]['value']; ?>"></i></a></div>
 					<h2>
-						<a href="/<?php print $language->language.'/'.$link; ?>" class="atm-item-carrera">
+						<a href="/<?php print $language->language.'/'.$links['value']; ?>" class="atm-item-carrera">
 							<?php foreach ($carrera as $key => $part) : ?>
 							<span><?php print $part; ?></span>
 							<?php endforeach; ?>
@@ -27,6 +29,7 @@
 					</h2>
 				</div>
 			</li>
+			<?php endforeach; ?>
 		</ul>
 	</div>
 </div>
