4c679fb41b10f5b23abc03f47335779df46c6bbc
carlos calderon <cadermino@gmail.com>
2015-05-06 19:38:52 -0500

    footer administrable

--- sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
+++ sites/all/themes/utec_theme/node--lista_carreras_block_.tpl.php
@@ -12,18 +12,23 @@
 ?>
 <div class="pv-42" id="home-sec-1">
 	<div class="container">
-		<h3 class="h2 lead medium mb-7"><span class="text-gray-darker"><?php print $titles[0].'/'.$titles[1].'/'.$titles[2]; ?></span> <span class="text-gray"><?php foreach ($titles as $key => $title){if($title[3]){print $title;}} ?></span></h3>
+		<h3 class="h2 lead medium mb-7"><span class="text-gray-darker"><?php print $titles[0].'/'.$titles[1]; ?></span> <span class="text-gray"><?php foreach ($titles as $key => $title){if($title != $titles[0] or $title != $title[1]){print $title;}} ?></span></h3>
 		<h4 class="lead mb-35"><?php print $cuerpo; ?></h4>
 		<ul class="grid-list grid-list-5 grid-list-1-xs grid-list-1-landscape pt-21 list-carreras text-center text-left-xs mb-ch-21-xs">
-			<?php $ca ?>
-			<?php foreach ($links as $key => $link) : ?>
+			<?php foreach ($carreras as $key => $carrera) : ?>
+			<?php 
+				$partes = explode(' ', $carrera); 
+			?>
 			<li>
 				<div>
-					<div><a href="/<?php print $language->language.'/'.$links['value']; ?>" class="atm-item-carrera"><i class="<?php print $iconos[$key]['value']; ?>"></i></a></div>
+					<div><a href="/<?php print $language->language.'/'.$links[$key]['value']; ?>" class="atm-item-carrera"><i class="<?php print $iconos[$key]['value']; ?>"></i></a></div>
 					<h2>
-						<a href="/<?php print $language->language.'/'.$links['value']; ?>" class="atm-item-carrera">
-							<?php foreach ($carrera as $key => $part) : ?>
-							<span><?php print $part; ?></span>
+						<a href="/<?php print $language->language.'/'.$links[$key]['value']; ?>" class="atm-item-carrera">
+							<span><?php print $partes[0]; ?></span>
+							<?php foreach ($partes as $key => $parte) : ?>
+							<?php if ($parte != $partes[0]): ?>
+							<span><?php print $parte; ?></span>
+							<?php endif ?>
 							<?php endforeach; ?>
 						</a>
 					</h2>
