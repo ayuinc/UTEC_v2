0617afcf73079c06068bd74f6d400ecad3649750
carlos calderon <cadermino@gmail.com>
2015-06-09 12:06:18 -0500

    tag noticias

--- sites/all/themes/utec_theme/node--noticias.tpl.php
+++ sites/all/themes/utec_theme/node--noticias.tpl.php
@@ -4,7 +4,7 @@
 	$titulo = $node->title;	 		
 	$cuerpo = $node->body['und']['0']['value'];
 	$images = $node->field_image['und'];
-	kpr($node);
+	// kpr($node);
 	$term = i18n_taxonomy_localize_terms($category);
 	$name = $term->name;
 	kpr($name);
