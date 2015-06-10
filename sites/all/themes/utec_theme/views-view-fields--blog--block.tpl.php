1725cd10ee065244efb96330efe3698438a54f68
carlos calderon <cadermino@gmail.com>
2015-06-10 11:31:39 -0500

    titulos de vistas

--- sites/all/themes/utec_theme/views-view-fields--blog--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--blog--block.tpl.php
@@ -12,30 +12,27 @@
 	$user_name = $blogger->name;
 	$user_image = $blogger->picture->filename;
 	$user_charge = $blogger->field_descripci_n['und']['0']['value'];
-	kpr($view->display['block']);
+	// kpr($view->display['block']->display_options['header']['area']['content']);
 ?>
-<div class="pv-21">
-	<div class="container">			
-		<!--<h4 class="lead mt-0 mb-56">Lorem ipsum sit amet, consectetur adipisicing elit.</h4>-->
-		<div class="banner ban-titulo" data-href="<?php print $path; ?>">
-			<div class="col-sm-3 banner-pic" style="background-image: url(<?php print $image; ?>);"></div>
-			<div class="col-sm-9 banner-content pv-14-xs pt-14-sm">
-				<div>
-	  			<div class="user-card user-card-round">
-	  				<div class="user-pic"><img src="<?php print $pathfile.'pictures/'.$user_image; ?>" alt="<?php print $user_name; ?>"></div>
-	  				<div class="user-info"><h4><?php print $user_name; ?></h4><small><?php print $user_charge ?></small></div>
-	  			</div>
-	  			<h1 class="h2"><a href="<?php print $path; ?>" class="banner-titulo"><?php print $title; ?></a></h1>
-					<p class="lead"><?php print $texto_corto; ?></p>
-					<div class="text-right pv-14">
-					<?php if ($language->language != 'en'): ?>
-					<a href="<?php print $path ?>" class="see-more btn btn-lg btn-primary uppercase banner-titulo">Leer más</a>
-					<?php elseif ($language->language == 'en'): ?>
-					<a href="<?php print $path ?>" class="see-more btn btn-lg btn-primary uppercase banner-titulo">Read more</a>
-					<?php endif ?>							
-					</div>
-				</div>
+
+<div class="banner ban-titulo" data-href="<?php print $path; ?>">
+	<div class="col-sm-3 banner-pic" style="background-image: url(<?php print $image; ?>);"></div>
+	<div class="col-sm-9 banner-content pv-14-xs pt-14-sm">
+		<div>
+			<div class="user-card user-card-round">
+				<div class="user-pic"><img src="<?php print $pathfile.'pictures/'.$user_image; ?>" alt="<?php print $user_name; ?>"></div>
+				<div class="user-info"><h4><?php print $user_name; ?></h4><small><?php print $user_charge ?></small></div>
+			</div>
+			<h1 class="h2"><a href="<?php print $path; ?>" class="banner-titulo"><?php print $title; ?></a></h1>
+			<p class="lead"><?php print $texto_corto; ?></p>
+			<div class="text-right pv-14">
+			<?php if ($language->language != 'en'): ?>
+			<a href="<?php print $path ?>" class="see-more btn btn-lg btn-primary uppercase banner-titulo">Leer más</a>
+			<?php elseif ($language->language == 'en'): ?>
+			<a href="<?php print $path ?>" class="see-more btn btn-lg btn-primary uppercase banner-titulo">Read more</a>
+			<?php endif ?>							
 			</div>
 		</div>
 	</div>
 </div>
+
---
+++ sites/all/themes/utec_theme/views-view-unformatted--blog--block.tpl.php
@@ -0,0 +1,7 @@
+</div>
+<div class="pv-21">
+	<div class="container">
+		<h3 class="h2 lead medium mb-56"><span class="text-gray-darker"><?php print $view->get_title(); ?></span></h3>			
+		<?php print $row; ?>
+	</div>
+</div>
