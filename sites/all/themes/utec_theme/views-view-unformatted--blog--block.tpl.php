be1d3b4c97f6058b8fae948c00cf50305718105b
carlos calderon <cadermino@gmail.com>
2015-06-11 17:25:02 -0500

    titulos de vistas

--- sites/all/themes/utec_theme/views-view-unformatted--blog--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--blog--block.tpl.php
@@ -1,5 +1,6 @@
 
-		<h3 class="h2 lead medium mb-56"><span class="text-gray-darker"><?php print $view->get_title(); ?></span></h3>			
+		<h1 class="h2 lead medium mb-56"><span class="text-gray-darker"><?php print $view->get_title(); ?></span></h1>			
+		<div class="separator-gray separator-sm"></div>
 		<?php foreach ($rows as $id => $row): ?>
 	    <?php print $row;?>
 		<?php endforeach; ?> 
