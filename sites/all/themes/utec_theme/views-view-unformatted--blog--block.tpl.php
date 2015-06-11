495ca85434b5527968198947f219017dd5acee4c
carlos calderon <cadermino@gmail.com>
2015-06-11 17:28:03 -0500

    titulos de vistas

--- sites/all/themes/utec_theme/views-view-unformatted--blog--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--blog--block.tpl.php
@@ -1,6 +1,9 @@
 
-		<h1 class="h2 lead medium mb-56"><span class="text-gray-darker"><?php print $view->get_title(); ?></span></h1>			
-		<div class="separator-gray separator-sm"></div>
+<div class="pv-21">
+	<div class="container">
+		<h3 class="h2 lead medium mb-56"><span class="text-gray-darker"><?php print $view->get_title(); ?></span></h3>			
 		<?php foreach ($rows as $id => $row): ?>
 	    <?php print $row;?>
 		<?php endforeach; ?> 
+	</div>
+</div>
