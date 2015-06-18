166c375650e626df378ee62bf932679e912a12c2
carlos calderon <cadermino@gmail.com>
2015-06-18 12:49:22 -0500

    fixes

--- sites/all/themes/utec_theme/views-view-unformatted--vista-taxonomia--block-1.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--vista-taxonomia--block-1.tpl.php
@@ -1,6 +1,11 @@
 <div class="container-sm">
+	<?php 
+		$term = taxonomy_term_load($view->args[0]);
+		$translated_term = i18n_taxonomy_localize_terms($term);
+		print $translated_term->name; 
+	?>
 	<h1 class="light"><?php print t('Blog posts'); ?></h1>
-	<?php kpr($view->args); ?>
+	<?php kpr($view->args[0]); ?>
 	<?php //kpr(array_keys(context_active_contexts())); ?>
 	<div class="separator-gray separator-sm"></div>
 	<ul class="grid-list grid-list-2 row p-ch-7 mb-ch-21 isotope-grid">
