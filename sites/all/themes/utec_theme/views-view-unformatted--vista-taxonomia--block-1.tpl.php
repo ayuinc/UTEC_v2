744dc127ac94ee948a20bed531fb82f7d7bd44f6
carlos calderon <cadermino@gmail.com>
2015-06-18 12:39:48 -0500

    fixes

--- sites/all/themes/utec_theme/views-view-unformatted--vista-taxonomia--block-1.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--vista-taxonomia--block-1.tpl.php
@@ -1,6 +1,6 @@
 <div class="container-sm">
 	<h1 class="light"><?php print t('Blog posts'); ?></h1>
-	<?php kpr($view->args[0]); ?>
+	<?php kpr($view->args); ?>
 	<?php //kpr(array_keys(context_active_contexts())); ?>
 	<div class="separator-gray separator-sm"></div>
 	<ul class="grid-list grid-list-2 row p-ch-7 mb-ch-21 isotope-grid">
