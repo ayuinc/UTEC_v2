37bc5bc54c2e6e2549999d179d710860f5fdceb8
Chasky <joel.porras@softwinperu.com>
2015-06-22 15:35:04 -0500

    Netsute form builder version 2

---
+++ sites/all/modules/custom/netsuite_form_builder/js/utec_netsuite_datalayer.js
@@ -0,0 +1,41 @@
+//jQuery(document).ready(function ($) {
+  netsuite_form_builder = window.parent.Drupal.settings.netsuite_form_builder;
+  console.log('Uno');
+  console.log('Netsuite Form Builder = ' + netsuite_form_builder);
+  
+  if (typeof netsuite_form_builder != "undefined") {
+    var dataLayer = dataLayer || [{"event": "formSubmit", "estado": "Success"}];
+    var form = window.parent.Drupal.settings.netsuite_form_builder.form;
+    var curso = window.parent.Drupal.settings.netsuite_form_builder.curso;
+    var usuario_estado = window.parent.Drupal.settings.netsuite_form_builder.usuario_estado;
+    var tipo = window.parent.Drupal.settings.netsuite_form_builder.tipo;
+    var carrera = "";
+    if (form != "") {
+      dataLayer[0]["form"] = form;
+    }
+    if (curso != "") {
+      dataLayer[0]["curso"] = curso;
+    }
+    if (usuario_estado != "" && typeof usuario_estado != "undefined") {
+      dataLayer[0]["usuario_estado"] = usuario_estado;
+    }
+    if (carrera != "" && typeof carrera != "undefined") {
+      dataLayer[0]["carrera"] = carrera;
+    }
+    if (tipo != "" && typeof tipo != "undefined") {
+      dataLayer[0]["tipo"] = tipo;
+    }
+
+    //Just to debug
+    console.debug('Curso = ' + dataLayer[0]["curso"]);
+    console.debug('Form = ' + dataLayer[0]["form"]);
+    console.debug('Tipo = ' + dataLayer[0]["tipo"]);
+
+    dataLayer.forEach(function(item) {
+      for (var property in item) {
+        console.log( property+" : "+ item[property]);
+      }
+    });
+  }
+//});
+
