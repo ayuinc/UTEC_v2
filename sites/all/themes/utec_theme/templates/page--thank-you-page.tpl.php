cd674adeb3fb0d8c2d2167fd583211a46f136e78
Chasky <joel.porras@softwinperu.com>
2015-07-08 12:29:52 -0500

    Quitando dataLayer vacio

--- sites/all/themes/utec_theme/templates/page--thank-you-page.tpl.php
+++ sites/all/themes/utec_theme/templates/page--thank-you-page.tpl.php
@@ -1,6 +1,3 @@
-<?php 
-  drupal_add_js("var dataLayer = [];", array('scope' => 'header', 'type' => 'inline'));
-?>
 <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MHF4TM" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script type="text/javascript">var dataLayer = [];(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl+'&nc='+new Date().getTime();f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MHF4TM');</script>
