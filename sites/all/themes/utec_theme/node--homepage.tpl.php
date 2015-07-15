068f7ef5f9a34021657ccaa78f331905bc3adbed
carlos calderon <cadermino@gmail.com>
2015-07-15 17:10:14 -0500

    video

--- sites/all/themes/utec_theme/node--homepage.tpl.php
+++ sites/all/themes/utec_theme/node--homepage.tpl.php
@@ -20,7 +20,7 @@
 		    class="video-js videocover vjs-default-skin"
 		    controls
 		    preload="auto">
-			    <source src="<?php print $video_uri; ?>" type='video/webm' />
+			    <source src="<?php print $video_url; ?>" type='video/webm' />
 			    <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 		  </video>
 			<?php endif; ?>
