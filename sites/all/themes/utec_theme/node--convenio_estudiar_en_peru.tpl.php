548ad56faa2c275eb717374f2d60a59be6597e77
carlos calderon <cadermino@gmail.com>
2015-09-22 18:08:29 -0500

    cabecera

--- sites/all/themes/utec_theme/node--convenio_estudiar_en_peru.tpl.php
+++ sites/all/themes/utec_theme/node--convenio_estudiar_en_peru.tpl.php
@@ -2,14 +2,12 @@
 	
 	$titulo = $node->title;   
 	$descripcion = $node->body['und']['0']['value'];
-
-
 	$imagen = $node->field_image['und']['0']['uri'];
-  kpr($node->field_image);
+  // kpr($node->field_image);
 
 ?>
 <div class="hero-unit"> <!-- HERO -->
-  <div class="scroll-down-white section-scroll-content bg-img-block bg-img-block-lg" style="background-image: url(<?php print image_style_url($image_style, $field_carrera_imagen); ?>);">
+  <div class="scroll-down-white section-scroll-content bg-img-block bg-img-block-lg" style="background-image: url(<?php print image_style_url($image_style, $imagen); ?>);">
     <div class="overlay flex-middle">
       <div class="container-sm hero-text" data-href="<?php print $carrera_link ?>">
         <h2 class="light text-white"><?php print $titulo ?> </h2>
