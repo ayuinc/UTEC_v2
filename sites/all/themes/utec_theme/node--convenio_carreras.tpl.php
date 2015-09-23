4f43bd3467187e76e3f96ec10878b9a7320e0394
carlos calderon <cadermino@gmail.com>
2015-09-23 18:24:16 -0500

    por que estudiar en utec

--- sites/all/themes/utec_theme/node--convenio_carreras.tpl.php
+++ sites/all/themes/utec_theme/node--convenio_carreras.tpl.php
@@ -11,8 +11,8 @@
 	<div class="container-sm">
 		<h2 class="light"><?php print $title ?></h2>
 	  <div class="separator-gray separator-sm"></div>
-		<h4 class="lead mb-35"><?php print $cuerpo; ?></h4>
-		<ul class="grid-list grid-list-5 grid-list-1-xs grid-list-1-landscape pt-21 list-carreras text-center text-left-xs mb-ch-21-xs">
+		<h4 class="mb-35"><?php print $cuerpo; ?></h4>
+		<ul class="grid-list grid-list-3 grid-list-1-xs grid-list-1-landscape pt-21 list-carreras text-center text-left-xs mb-ch-21-xs">
 			<?php foreach ($carreras as $key => $carrera) : ?>
 			<?php 
 				$partes = array();
