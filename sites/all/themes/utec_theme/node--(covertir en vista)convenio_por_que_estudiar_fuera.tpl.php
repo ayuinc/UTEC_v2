1c5bbcaaec4650f3408f9f18faa98882ea4c77d5
carlos calderon <cadermino@gmail.com>
2015-09-23 17:06:40 -0500

    por que estudiar en utec

---
+++ sites/all/themes/utec_theme/node--(covertir en vista)convenio_por_que_estudiar_fuera.tpl.php
@@ -0,0 +1,150 @@
+<?php 
+	
+	$titulo = $node->title;   
+	$descripcion = $node->body['und']['0']['value'];
+	$imagen = $node->field_image['und']['0']['uri'];
+  // kpr($node->field_image);
+
+?>
+<div class="anchor-img-grid anchor-img-grid-1">
+  <div class="col-sm-4">   
+    <div data-href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php" class="anchor-block-1">     
+    <div class="pic" style="background-image: url(http://104.236.86.120/sites/default/files//styles/en_la_utec_bloque_derecho__556x556_/public/pre-utec-imagen_0.jpg?itok=u0OIn_K3);"></div>
+    <div class="overlay">
+      <a href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php">
+        <h4>
+          Inicio 19 de octubre                
+        </h4>
+        <h3 class="h2">
+          ¡Inscríbete ahora!                
+        </h3>
+      </a>
+    </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div data-href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php" class="anchor-block-1">     
+          <div class="pic" style="background-image: url(http://104.236.86.120/sites/default/files//styles/en_la_utec_bloque_derecho__556x556_/public/pre-utec-imagen_0.jpg?itok=u0OIn_K3);"></div>
+          <div class="overlay">
+            <a href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php">
+              <h4>
+                Inicio 19 de octubre                
+              </h4>
+              <h3 class="h2">
+                ¡Inscríbete ahora!                
+              </h3>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-8"> 
+    <div class="row">
+      <div class="col-sm-12">
+        <div data-href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php" class="anchor-block-1">     
+          <div class="pic" style="background-image: url(http://104.236.86.120/sites/default/files//styles/en_la_utec_bloque_derecho__556x556_/public/pre-utec-imagen_0.jpg?itok=u0OIn_K3);"></div>
+          <div class="overlay">
+            <a href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php">
+              <h4>
+                Inicio 19 de octubre                
+              </h4>
+              <h3 class="h2">
+                ¡Inscríbete ahora!                
+              </h3>
+            </a>
+          </div>
+        </div>        
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <div data-href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php" class="anchor-block-1">     
+          <div class="pic" style="background-image: url(http://104.236.86.120/sites/default/files//styles/en_la_utec_bloque_derecho__556x556_/public/pre-utec-imagen_0.jpg?itok=u0OIn_K3);"></div>
+          <div class="overlay">
+            <a href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php">
+              <h4>
+                Inicio 19 de octubre                
+              </h4>
+              <h3 class="h2">
+                ¡Inscríbete ahora!                
+              </h3>
+            </a>
+          </div>
+        </div>        
+      </div>
+      <div class="col-sm-6">
+        <div data-href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php" class="anchor-block-1">     
+          <div class="pic" style="background-image: url(http://104.236.86.120/sites/default/files//styles/en_la_utec_bloque_derecho__556x556_/public/pre-utec-imagen_0.jpg?itok=u0OIn_K3);"></div>
+          <div class="overlay">
+            <a href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php">
+              <h4>
+                Inicio 19 de octubre                
+              </h4>
+              <h3 class="h2">
+                ¡Inscríbete ahora!                
+              </h3>
+            </a>
+          </div>
+        </div>        
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div data-href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php" class="anchor-block-1">     
+          <div class="pic" style="background-image: url(http://104.236.86.120/sites/default/files//styles/en_la_utec_bloque_derecho__556x556_/public/pre-utec-imagen_0.jpg?itok=u0OIn_K3);"></div>
+          <div class="overlay">
+            <a href="http://app.utec.edu.pe/admision/centro-preuniversitario/informacion.php">
+              <h4>
+                Inicio 19 de octubre                
+              </h4>
+              <h3 class="h2">
+                ¡Inscríbete ahora!                
+              </h3>
+            </a>
+          </div>
+        </div>        
+      </div>
+    </div>
+  </div>
+</div>
+
+    <div data-href="http://www.ingenioenaccion.pe/" class="anchor-block-2">                    
+      <div class="pic" style="background-image: url(http://104.236.86.120/sites/default/files//styles/556x264/public/ingenio_accion_utec_0.jpg?itok=OzJEwvt7);"></div>
+      <div class="overlay">
+        <a href="http://www.ingenioenaccion.pe/">
+          <h4>
+            Ingenio en Acción                
+          </h4>
+          <h3 class="h2">
+            Soluciones reales a problemas reales                
+          </h3>
+        </a>        
+      </div>
+    </div>
+    <div data-href="http://utecventures.com" class="anchor-block-3">    
+      <div class="pic" style="background-image: url(http://104.236.86.120/sites/default/files//styles/264x264/public/utec_ventures_1.png?itok=cuJRKRgj);"></div>
+      <div class="overlay">
+        <a href="http://utecventures.com">
+          <h4>
+            UTEC Ventures                
+          </h4>
+          <h3 class="h2">
+            Aceleradora de negocios                
+          </h3>
+        </a>
+      </div>
+    </div>
+    <div data-href="/convenios-de-intercambio" class="anchor-block-4">  
+      <div class="pic" style="background-image: url(http://104.236.86.120/sites/default/files//styles/264x264/public/convenios_0.jpg?itok=7ODX5SbS);"></div>
+      <div class="overlay">
+        <a href="/convenios-de-intercambio">
+          <h4>
+            Convenios                
+          </h4>
+          <h3 class="h2">
+            Convenios y alianzas internacionales                 
+          </h3>
+        </a>  
+      </div>
+    </div>
