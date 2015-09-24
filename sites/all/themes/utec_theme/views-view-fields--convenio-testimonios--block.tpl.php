2ea421a856be58c154c7d189695a14ceb76f8160
carlos calderon <cadermino@gmail.com>
2015-09-24 18:15:11 -0500

    testimonios

--- sites/all/themes/utec_theme/views-view-fields--convenio-testimonios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-fields--convenio-testimonios--block.tpl.php
@@ -1,24 +1,19 @@
 <?php
 
-	$title = $fields['title']->content;
-	$body = $fields['body']->content;
 	$image = $fields['field_image']->content;
-	$category = $fields['field_categor_a']->content;
-	$texto_corto = $fields['field_texto_corto']->content;
-	$path = $fields['path']->content;
-	$user_id = $fields['uid']->content;
-	$blogger_id = $fields['field_blogger']->content;
-	$blogger = user_load($blogger_id);
-	$user_name = $blogger->name;
-	$user_image = $blogger->picture->filename;
-	$user_charge = $blogger->field_descripci_n['und']['0']['value'];
+	$title = $fields['title']->content;
+	$cargo = $fields['field_texo']->content;
+	$curso = $fields['field_nombre']->content;
+	$body = $fields['body']->content;
 	// kpr($view->display['block']->display_options['header']['area']['content']);
 ?>
 
-
-	<img src="/sites/default/files/pictures/picture-26-1419635360.jpg" alt="Eduardo Hochschild Beeck" width="98px" height="auto" class="img-circle">
-
+<div class="p-21">
+	<img src="<?php print $image ?>" class="img-circle">
+</div>
 <div>
-	<a href="/autoridades/eduardo-hochschild-beeck">Eduardo Hochschild Beeck</a>
-	<span>Presidente del Consejo Directivo</span>					
+	<p><?php print $title ?></p>
+	<p><?php print $cargo ?></p>				
+	<p><?php print $curso ?></p>				
+	<p><?php print $body ?></p>				
 </div>
--- sites/all/themes/utec_theme/views-view-unformatted--convenio-testimonios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--convenio-testimonios--block.tpl.php
@@ -1,46 +1,39 @@
 <div class="pv-21">
 	<div class="container-sm">
-		<h3 class="h2 lead medium mb-56"><span class="text-gray-darker"><?php print $view->get_title(); ?></span></h3>			
-
-
-<!-- ----------------------------------------------------- -->
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <?php $count = 0; ?>
-    <?php foreach ($rows as $id => $row): ?>
-			<?php if (fmod($count, 3) == 0) : ?>
-			<div class="row item <?php ($count == 0) ? print 'active' : '' ; ?>">
-			<?php endif; ?>  
-				<div class="col-sm-4">
-			    <?php print $row;?>
-				</div>
-	    <?php if (fmod($count, 3) == 2) : ?> 
-			</div>
-			<?php endif; ?> 
-			<?php $count++; ?>
-    <?php endforeach; ?>
-  </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>	
-
-
+		<h3 class="h2 lead medium mb-56"><span class="text-gray-darker"><?php print $view->get_title(); ?></span></h3>
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		  </ol>
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner text-center" role="listbox">
+		    <?php $count = 0; ?>
+		    <?php foreach ($rows as $id => $row): ?>
+					<?php if (fmod($count, 3) == 0) : ?>
+					<div class="row item <?php ($count == 0) ? print 'active' : '' ; ?>">
+					<?php endif; ?>  
+						<div class="col-sm-4">
+					    <?php print $row;?>
+						</div>
+			    <?php if (fmod($count, 3) == 2) : ?> 
+					</div>
+					<?php endif; ?> 
+					<?php $count++; ?>
+		    <?php endforeach; ?>
+		  </div>
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
 	</div>
 </div>
 
