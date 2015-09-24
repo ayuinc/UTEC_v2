3ec3b7d3291c2f5e3977a0b73a7f978a866ec690
carlos calderon <cadermino@gmail.com>
2015-09-24 16:51:38 -0500

    testimonios

--- sites/all/themes/utec_theme/views-view-unformatted--convenio-testimonios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--convenio-testimonios--block.tpl.php
@@ -1,9 +1,6 @@
 <div class="pv-21">
 	<div class="container">
 		<h3 class="h2 lead medium mb-56"><span class="text-gray-darker"><?php print $view->get_title(); ?></span></h3>			
-		<?php foreach ($rows as $id => $row): ?>
-	    <?php print $row;?>
-		<?php endforeach; ?> 
 	</div>
 </div>
 
@@ -26,7 +23,7 @@
 				<div class="">
 			    <?php print $row;?>
 				</div>
-	    <?php if (fmod($count, 3) == 3) : ?> 
+	    <?php if (fmod($count, 3) == 2) : ?> 
 			</div>
 			<?php endif; ?> 
 			<?php $count++; ?>
