aa2954477f1651dbdd29609f97b928550abb0f5e
carlos calderon <cadermino@gmail.com>
2015-09-24 16:29:31 -0500

    testimonios

--- sites/all/themes/utec_theme/views-view-unformatted--convenio-testimonios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--convenio-testimonios--block.tpl.php
@@ -1,41 +1,41 @@
-<div class="pv-21">
-	<div class="container">
-		<h3 class="h2 lead medium mb-56"><span class="text-gray-darker"><?php print $view->get_title(); ?></span></h3>			
-		<?php foreach ($rows as $id => $row): ?>
-	    <?php print $row;?>
-		<?php endforeach; ?> 
-	</div>
-</div>
 
-<div id="carousel-custom" class="carousel carousel-custom slide mb-42" data-ride="carousel">
-	<!-- Wrapper for slides -->
+
+<!-- ----------------------------------------------------- -->
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
-		<?php $count = 0; ?>
-    <?php foreach ($rows as $id => $row): ?>
-			<?php if (fmod($count, 3) == 0) : ?>
-			<div class="item">
-			<?php endif; ?>  
-				<div class="banner banner-label-bottom mb-7 ml-0">
-			    <?php print $row;?>
-				</div>
-	    <?php if (fmod($count, 3) == 3) : ?> 
-			</div>
-			<?php endif; ?> 
-			<?php $count++; ?>
-    <?php endforeach; ?>
-	</div>
-	<!-- Controls -->
-  <a class="left carousel-control" href="#carousel-custom" role="button" data-slide="prev">
-    <span class="icon-prev hidden-xs" aria-hidden="true"></span>
+    <div class="item active">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="right carousel-control" href="#carousel-custom" role="button" data-slide="next">
-    <span class="icon-next hidden-xs" aria-hidden="true"></span>
-    <span class="sr-only">Siguiente</span>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
   </a>
-</div>
-
-	
+</div>	
 
 
 
