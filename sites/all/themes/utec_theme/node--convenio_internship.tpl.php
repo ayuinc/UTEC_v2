1d45ae5505ff4869764567f740a86e129110b1aa
carlos calderon <cadermino@gmail.com>
2015-09-25 16:33:50 -0500

    como aplicar

---
+++ sites/all/themes/utec_theme/node--convenio_internship.tpl.php
@@ -0,0 +1,29 @@
+<?php 
+	
+	$titulo = $node->title;   
+  $descripcion = $node->body['und']['0']['value'];
+	$imagenes = $node->field_image['und'];
+  // kpr($node->field_image);
+
+?>
+
+<div class="container-sm">
+  <h2 class="light"><?php print $titulo ?></h2>
+  <div class="separator-gray separator-sm"></div>
+  <div class="p-ch-gray-dark">
+    <div class="pv-ch-7">
+      <?php print $descripcion ?>   
+    </div>
+    <!-- Wrapper for slides -->
+    <div class="row">
+      <?php foreach ($$imagenes as $key => $$imagen): ?>
+      <div class="col-md-3">
+        <div class="banner banner-label-bottom mb-7 ml-0">
+          <div class="banner-pic" style="background-image: url(<?php print image_style_url($image_style, $imagen); ?>);">
+          </div>
+        </div>
+      </div>
+      <?php endforeach ?>
+    </div>  
+  </div>
+</div>
