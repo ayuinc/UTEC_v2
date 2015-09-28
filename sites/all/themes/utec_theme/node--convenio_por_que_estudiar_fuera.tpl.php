ac1d8b63dfa1e8f2e82644581072622c2b224445
carlos calderon <cadermino@gmail.com>
2015-09-28 07:41:18 -0500

    tabs

--- sites/all/themes/utec_theme/node--convenio_por_que_estudiar_fuera.tpl.php
+++ sites/all/themes/utec_theme/node--convenio_por_que_estudiar_fuera.tpl.php
@@ -3,26 +3,30 @@
 	$titulo = $node->title;   
   $titulos = $node->field_texo['und'];
   $contenido = $node->field_cuerpo['und'];
-	$titulos = $node->field_texo['und'];
-	$files = $node->field_archivo_pdf['und'];
-  // kpr($node);
+  $imagen1 = $node->field_image['und']['0']['uri'];
+  $imagen2 = $node->field_imagen_mutliple_1['und']['0']['uri'];
+  $imagen3 = $node->field_imagen_3['und']['0']['uri'];
+  $imagen4 = $node->field_imagen_4['und']['0']['uri'];
+  $imagen5 = $node->field_imagen_5['und']['0']['uri'];
+	$imagen6 = $node->field_imagen_6['und']['0']['uri'];
+  kpr($node);
 
 ?>
 
 
 <div class="row">
-  <div class="col-md-4"></div>
-  <div class="col-md-8"></div>
+  <div class="col-md-4"><?php print image_style_url($image_style, $imagen1); ?></div>
+  <div class="col-md-8"><?php print image_style_url($image_style, $imagen2); ?></div>
 </div>
 <div class="row">
-  <div class="col-md-4"></div>
+  <div class="col-md-4"><?php print image_style_url($image_style, $imagen3); ?></div>
   <div class="col-md-8">
     <div class="row">
-      <div class="col-md-6"></div>
-      <div class="col-md-6"></div>
+      <div class="col-md-6"><?php print image_style_url($image_style, $imagen4); ?></div>
+      <div class="col-md-6"><?php print image_style_url($image_style, $imagen5); ?></div>
     </div>
     <div class="row">
-      <div class="col-md-12"></div>
+      <div class="col-md-12"><?php print image_style_url($image_style, $imagen6); ?></div>
     </div>
   </div>
 </div>  
