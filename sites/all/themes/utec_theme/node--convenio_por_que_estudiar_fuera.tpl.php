07d0180db6d88703ae9363fbda4309c4ddacd364
carlos calderon <cadermino@gmail.com>
2015-09-28 07:01:42 -0500

    tabs

---
+++ sites/all/themes/utec_theme/node--convenio_por_que_estudiar_fuera.tpl.php
@@ -0,0 +1,28 @@
+<?php 
+	
+	$titulo = $node->title;   
+  $titulos = $node->field_texo['und'];
+  $contenido = $node->field_cuerpo['und'];
+	$titulos = $node->field_texo['und'];
+	$files = $node->field_archivo_pdf['und'];
+  // kpr($node);
+
+?>
+
+
+<div class="row">
+  <div class="col-md-4"></div>
+  <div class="col-md-8"></div>
+</div>
+<div class="row">
+  <div class="col-md-4"></div>
+  <div class="col-md-8">
+    <div class="row">
+      <div class="col-md-6"></div>
+      <div class="col-md-6"></div>
+    </div>
+    <div class="row">
+      <div class="col-md-12"></div>
+    </div>
+  </div>
+</div>  
