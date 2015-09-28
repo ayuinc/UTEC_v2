1f5d83e389d3bb7f258f0e401cec6610cdcd8456
carlos calderon <cadermino@gmail.com>
2015-09-28 17:37:36 -0500

    imagenes estudiar fuera

---
+++ sites/all/themes/utec_theme/node--convenio_vivir_en_peru.tpl.php
@@ -0,0 +1,19 @@
+<?php 
+	
+	$titulo = $node->title;   
+	$descripcion = $node->body['und']['0']['value'];
+	$imagen = $node->field_image['und']['0']['uri'];
+  kpr($node);
+
+?>
+<div class="hero-unit"> <!-- HERO -->
+  <div class="scroll-down-white section-scroll-content bg-img-block bg-img-block-lg" style="background-image: url(<?php print image_style_url($image_style, $imagen); ?>);">
+    <div class="overlay flex-middle">
+      <div class="container-sm hero-text">
+        <h2 class="light text-white"><?php print $titulo ?> </h2>
+        <div class="separator-white separator-sm"></div>
+        <div class="text-white light h4"><?php print $descripcion ?></div>
+      </div>
+    </div>
+  </div>
+</div> <!-- END:Hero -->
