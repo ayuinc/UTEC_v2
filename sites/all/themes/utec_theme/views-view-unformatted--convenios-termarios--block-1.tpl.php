da0e8aff9a8ec5b7db5eb401beb11029a2d6b659
carlos calderon <cadermino@gmail.com>
2015-10-02 15:14:34 -0500

    temario

---
+++ sites/all/themes/utec_theme/views-view-unformatted--convenios-termarios--block-1.tpl.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<div class="container-sm">
+	<h1 class="light"><?php print $view->get_title(); ?></h1>
+	<div class="separator-gray separator-lg"></div>
+	<h3></h3>
+	<div class="pv-35">
+		<div class="table-responsive table-custom">
+			<table class="table">
+				<thead>
+					<tr>
+						<td><?php print t('Courses') ?></td>
+						<td><?php print t('Credits') ?></td>
+						<td><?php print t('Prerequisites') ?></td>
+						<td><?php print t('Objectives') ?></td>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($rows as $id => $row): ?>
+				    <?php print $row;?>
+					<?php endforeach; ?> 																																																						
+				</tbody>
+			</table>			
+		</div>
+	</div>
+</div>
