058badcb01285e706fdef5fab918a8f21e37cd36
carlos calderon <cadermino@gmail.com>
2015-10-02 12:29:43 -0500

    temario

---
+++ sites/all/themes/utec_theme/views-view-fields--convenios-termarios--block.tpl.php
@@ -0,0 +1,33 @@
+<?php 
+
+	$name = $fields['title']->content;
+	$curso = $fields['field_a_o']->content;
+	$creditos = $fields['field_n_mero_creditos']->content;
+	$prerequisitos = $fields['field_requisitos']->content;
+	$objetivos = $fields['body']->content;
+
+?>
+
+<tr>
+	<td><?php print $curso; ?></td>
+	<td><?php print $creditos ?></td>	
+	<td><?php print $prerequisitos ?></td>	
+	<td><?php print $objetivos ?></td>	
+</tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
