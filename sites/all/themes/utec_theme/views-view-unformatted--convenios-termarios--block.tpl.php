8b31b15478775a97482f4c88de00d6bf79e0d814
carlos calderon <cadermino@gmail.com>
2015-10-02 12:41:54 -0500

    temario

--- sites/all/themes/utec_theme/views-view-unformatted--convenios-termarios--block.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--convenios-termarios--block.tpl.php
@@ -8,8 +8,7 @@
  */
 ?>
 <div class="container-sm">
-	<?php  kpr($view); ?>
-	<h1 class="light"><?php print $view->get_title(); ?></h1>
+	<h1 class="light"><?php print $view->result['0']['field_field_carrera_referencia']['0']['rendered']['#markup']; ?></h1>
 	<div class="separator-gray separator-lg"></div>
 	<h3></h3>
 	<div class="pv-35">
