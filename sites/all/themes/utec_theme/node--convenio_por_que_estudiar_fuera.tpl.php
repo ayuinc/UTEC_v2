fa6037a011e16ad358f551f44134c6c703e0055e
MercedesZubieta <mercedes.zubieta@gmail.com>
2015-10-13 18:46:13 -0500

    cambios de responsive en el grid

--- sites/all/themes/utec_theme/node--convenio_por_que_estudiar_fuera.tpl.php
+++ sites/all/themes/utec_theme/node--convenio_por_que_estudiar_fuera.tpl.php
@@ -22,9 +22,9 @@
       <?php print $descripcion ?>   
     </div> 
   </div>
-  <div class="anchor-img-grid anchor-img-grid-1 ph-21">
+  <div class="anchor-img-grid anchor-img-grid-1 ph-21" id="grid-six">
     <div class="col-md-4">
-      <div class="anchor-block-4" style="width: 245px; height: 242px;">    
+      <div class="anchor-block-4 grid-six-movil" style="width: 245px; height: 242px;">    
         <div class="pic" style="background-image: url(<?php print image_style_url($image_style, $imagen1); ?>);"></div>
         <div class="overlay-grid-six" style="background-color: rgba(0,0,0,0.50); opacity: 1; padding: 14px;">
           <h5 class="medium text-left mb-7 mt-56"><?php print $titulos['0']['value']; ?></h5>
@@ -32,7 +32,7 @@
           <h5 class="thin text-left"><?php print $contenido['0']['value']; ?></h5>
         </div>
       </div>
-      <div class="anchor-block-4" style="width: 245px; height: 484px;">    
+      <div class="anchor-block-4 grid-six-movil" style="width: 245px; height: 484px;">    
         <div class="pic" style="background-image: url(<?php print image_style_url($image_style_3, $imagen3); ?>);"></div>
         <div class="overlay-grid-six" style="background-color: rgba(0,0,0,0.50); opacity: 1; padding: 14px;">
           <h5 class="medium text-left mb-7 mt-70"><?php print $titulos['2']['value']; ?></h5>
@@ -42,7 +42,7 @@
       </div>
     </div>
     <div class="col-md-8">
-      <div class="anchor-block-2" style="width: 487px; height: 242px;">    
+      <div class="anchor-block-2 grid-six-movil" style="width: 487px; height: 242px;">    
         <div class="pic" style="background-image: url(<?php print image_style_url($image_style_mul, $imagen2); ?>);"></div>
         <div class="overlay-grid-six" style="background-color: rgba(0,0,0,0.50); opacity: 1; padding: 14px;">
           <h5 class="medium text-left mb-7 "><?php print $titulos['1']['value']; ?></h5>
@@ -50,7 +50,7 @@
           <h5 class="thin text-left"><?php print $contenido['1']['value']; ?></h5>
         </div>
       </div>
-      <div class="anchor-block-3" style="width: 243px; height: 242px;">    
+      <div class="anchor-block-3 grid-six-movil" style="width: 243px; height: 242px;">    
         <div class="pic" style="background-image: url(<?php print image_style_url($image_style_4, $imagen4); ?>);"></div>
         <div class="overlay-grid-six" style="background-color: rgba(0,0,0,0.50); opacity: 1; padding: 14px;">
           <h5 class="medium text-left mb-7 "><?php print $titulos['3']['value']; ?> s</h5>
@@ -58,7 +58,7 @@
           <h5 class="thin text-left"><?php print $contenido['3']['value']; ?></h5>
         </div>
       </div>
-      <div class="anchor-block-4" style="width: 243px; height: 242px;">    
+      <div class="anchor-block-4 grid-six-movil" style="width: 243px; height: 242px;">    
         <div class="pic" style="background-image: url(<?php print image_style_url($image_style_5, $imagen5); ?>);"></div>
         <div class="overlay-grid-six" style="background-color: rgba(0,0,0,0.50); opacity: 1; padding: 14px;">
           <h5 class="medium text-left mb-7 "><?php print $titulos['4']['value']; ?></h5>
@@ -66,7 +66,7 @@
           <h5 class="thin text-left"><?php print $contenido['4']['value']; ?></h5>
         </div>
       </div>
-      <div class="anchor-block-2" style=" float: left; width: 487px; height: 242px;">    
+      <div class="anchor-block-2 grid-six-movil" style=" float: left; width: 487px; height: 242px;">    
         <div class="pic" style="background-image: url(<?php print image_style_url($image_style_6, $imagen6); ?>);"></div>
         <div class="overlay-grid-six" style="background-color: rgba(0,0,0,0.50); opacity: 1; padding: 14px;">
           <h5 class="medium text-left mb-7 "><?php print $titulos['5']['value']; ?></h5>
