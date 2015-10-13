8959443011520f4999faf2885162d23587550295
carlos calderon <cadermino@gmail.com>
2015-10-13 17:22:42 -0500

    modal messages

---
+++ sites/all/themes/utec_theme/views-view-fields--convenios-termarios--block-1.tpl.php
@@ -0,0 +1,37 @@
+<?php 
+
+	$semestre = $fields['field_tipo_']->content;
+	$name = $fields['title']->content;
+	$curso = $fields['field_a_o']->content;
+	$creditos = $fields['field_n_mero_creditos']->content;
+	$prerequisitos = $fields['field_requisitos']->content;
+	$objetivos = $fields['body']->content;
+	$carrera = $fields['field_carrera_referencia']->content;
+
+
+?>
+
+<tr>
+	<td><?php print $semestre ?></td>
+	<td><?php print $curso ?></td>
+	<td><?php print $creditos ?></td>	
+	<td><?php print $prerequisitos ?></td>	
+	<td><?php print $objetivos ?></td>	
+</tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sites/all/themes/utec_theme/views-view-unformatted--convenios-termarios--block-1.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--convenios-termarios--block-1.tpl.php
@@ -16,6 +16,7 @@
 			<table class="table">
 				<thead>
 					<tr>
+						<td><?php print t('Semester') ?></td>
 						<td><?php print t('Courses') ?></td>
 						<td><?php print t('Credits') ?></td>
 						<td><?php print t('Prerequisites') ?></td>
