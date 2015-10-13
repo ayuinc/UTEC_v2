46f05c8b3de59edd95f3e33c4a790eae8bd18e62
carlos calderon <cadermino@gmail.com>
2015-10-13 15:40:43 -0500

    modal messages

--- sites/all/themes/utec_theme/templates/pane-messages.tpl.php
+++ sites/all/themes/utec_theme/templates/pane-messages.tpl.php
@@ -26,6 +26,7 @@
 	      <div class="modal-body">
 				  <?php //print_r($url); ?>
 	        <?php $path_i18 = drupal_get_path_alias('', $language->language); ?>
+	        <div class="carlos"><?php print $variables['page']['content']['metatags']['global']['og:title']; ?></div>
 	        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>">Share on Facebook</a>
 	      	<a href="https://twitter.com/home?status=<?php print $og_description ?>">Share on Twitter</a>
 	      	<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>&title=<?php print $og_title ?>&summary=&source=">Share on LinkedIn</a>
