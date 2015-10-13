dd34f5618c6557f277c56a55f82d5a72c023471d
carlos calderon <cadermino@gmail.com>
2015-10-13 11:04:33 -0500

    modal messages

--- sites/all/themes/utec_theme/templates/pane-messages.tpl.php
+++ sites/all/themes/utec_theme/templates/pane-messages.tpl.php
@@ -10,8 +10,13 @@
 
 <?php if (!empty($messages)): ?>
   <?php //print $messages; ?>
-  <!-- Modal -->
-	<div class="modal fade" id="thanks" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<!-- Button trigger modal -->
+	<button id="button" type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+	  Launch demo modal
+	</button>
+
+	<!-- Modal -->
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 	  <div class="modal-dialog" role="document">
 	    <div class="modal-content">
 	      <div class="modal-header">
@@ -19,7 +24,7 @@
 	        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
 	      </div>
 	      <div class="modal-body">
-	        <div>Gracias por escribir!</div>
+	        ...
 	      </div>
 	      <div class="modal-footer">
 	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -28,9 +33,6 @@
 	    </div>
 	  </div>
 	</div>
-	<!-- Button trigger modal -->
-	<button id="button" style="display:none;" type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#thanks">
-	</button>
 <?php endif; ?>
 
 <?php if (!empty($help)): ?>
