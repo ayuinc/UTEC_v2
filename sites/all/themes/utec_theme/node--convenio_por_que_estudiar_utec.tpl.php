3c32dd3a01fffe77bcac29684113deae44311e23
carlos calderon <cadermino@gmail.com>
2015-10-14 12:06:33 -0500

    modal messages

--- sites/all/themes/utec_theme/node--convenio_por_que_estudiar_utec.tpl.php
+++ sites/all/themes/utec_theme/node--convenio_por_que_estudiar_utec.tpl.php
@@ -25,7 +25,6 @@
         loop
         muted 
         class="video-js videocover vjs-default-skin"
-        controls
         preload="auto">
           <source src="<?php print $video_url; ?>" type='video/webm' />
           <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
