d50eaee0c007dd881e99fe1ebc58a23df30d93c4
carlos calderon <cadermino@gmail.com>
2015-10-14 11:51:41 -0500

    modal messages

--- sites/all/themes/utec_theme/js/app/lib/videojs.js
+++ sites/all/themes/utec_theme/js/app/lib/videojs.js
@@ -18,22 +18,22 @@
 
     });
 
+    // Event listener for the play/pause button
+    playButton.addEventListener("click", function() {
+      if (video.paused == true) {
+        // Play the video
+        video.play();
+
+        // Update the button text to 'Pause'
+        playButton.innerHTML = "Pause";
+      } else {
+        // Pause the video
+        video.pause();
+
+        // Update the button text to 'Play'
+        playButton.innerHTML = "Play";
+      }
+    });
   }).call(this);
   
-  // Event listener for the play/pause button
-  playButton.addEventListener("click", function() {
-    if (video.paused == true) {
-      // Play the video
-      video.play();
-
-      // Update the button text to 'Pause'
-      playButton.innerHTML = "Pause";
-    } else {
-      // Pause the video
-      video.pause();
-
-      // Update the button text to 'Play'
-      playButton.innerHTML = "Play";
-    }
-  });
 }(jQuery));
