cba190100cae68884856b3cb602d7c15bc217846
carlos calderon <cadermino@gmail.com>
2015-10-15 15:12:02 -0500

    tabs

--- sites/all/themes/utec_theme/js/app/lib/videojs.js
+++ sites/all/themes/utec_theme/js/app/lib/videojs.js
@@ -39,7 +39,7 @@
 
       // Trigger Tabs with slider in living in peru
       $(".carousel-custom a").on("click", function(){
-        var selector = $(this).parent().parent().parent().parent().prev();
+        var selector = $(this).parent();
     
         console.log(selector); 
       });
