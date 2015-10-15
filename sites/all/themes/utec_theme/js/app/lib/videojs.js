a590b3b668900668662a319c8dea7739f3d75b20
carlos calderon <cadermino@gmail.com>
2015-10-15 16:35:16 -0500

    tabs

--- sites/all/themes/utec_theme/js/app/lib/videojs.js
+++ sites/all/themes/utec_theme/js/app/lib/videojs.js
@@ -43,7 +43,7 @@
         console.log(id);
         var selector = $(this).parent().index();
         var click = $(".carousel-custom ol li[data-slide-to='"+selector+"']");
-        var click2 = $("#"+id+" .carousel-custom ol li[data-slide-to='"+selector+"']");
+        var click2 = $("#"+id+" li[data-slide-to='"+selector+"']");
         console.log(click2);
         $(click2).trigger( "click" );
         // console.log(click);
