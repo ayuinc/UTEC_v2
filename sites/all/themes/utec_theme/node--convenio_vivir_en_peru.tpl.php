be971e8aed5b16451b7a35769ab50d4aba24d0e6
carlos calderon <cadermino@gmail.com>
2015-10-16 18:13:14 -0500

    currency exchange

--- sites/all/themes/utec_theme/node--convenio_vivir_en_peru.tpl.php
+++ sites/all/themes/utec_theme/node--convenio_vivir_en_peru.tpl.php
@@ -28,8 +28,8 @@
   $cuerpos_tab_bloque_6 = $node->field_contenido_tab_bloque_6['und'];   
   $imagenes_bloque_6 = $node->field_imagenes_bloque_6['und'];
 
-  $pen = $node->pen['und']['0']['value'];
-  kpr($node);
+  // $pen = $node->pen['und']['0']['value'];
+  // kpr($node);
 
 ?>
 <!-- CABECERA -->
