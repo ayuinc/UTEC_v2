0accc9e9360956e200ebe068c7b5702030f720a9
carlos calderon <cadermino@gmail.com>
2015-10-16 20:16:11 -0500

    currency exchange

--- sites/all/themes/utec_theme/template.php
+++ sites/all/themes/utec_theme/template.php
@@ -210,11 +210,6 @@ function utec_theme_menu_link(array $variables) {
   }
 }
 
-// function utec_theme_preprocess_node(&$variables) {
-  // die('entro');
-  
-// }
-
 function utec_theme_preprocess_node(&$variables){
   print_r($variables);
 	$variables ['front_page'] = url();
