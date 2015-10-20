33b1d4a8bad02a4954ae7343544a07cd548b2d9a
carlos calderon <cadermino@gmail.com>
2015-10-20 10:46:01 -0500

    metatags

--- sites/all/themes/utec_theme/template.php
+++ sites/all/themes/utec_theme/template.php
@@ -50,7 +50,7 @@ function _utec_theme_var($var_name, $new_val = NULL) {
   $og_title = $variables['page']['content']['metatags']['global']['og:title'];
   $og_description = $variables['page']['content']['metatags']['global']['og:description']['#attached']['drupal_add_html_head'][0][0]['#value'];
   // die($og_description);
-  _utec_theme_var('og_title', $og_title);
+  _utec_theme_var('og_title', $og_description);
   // $variables['og_description'] = _utec_theme_var('og_description', $og_description);
 
 
