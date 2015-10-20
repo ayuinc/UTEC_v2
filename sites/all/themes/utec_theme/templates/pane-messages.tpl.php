3b03a8f0c023e13ce7a753b65309b2ca86fbfee5
MercedesZubieta <mercedes.zubieta@gmail.com>
2015-10-20 14:31:22 -0500

    cambios de ruta de las imagenes 2

--- sites/all/themes/utec_theme/templates/pane-messages.tpl.php
+++ sites/all/themes/utec_theme/templates/pane-messages.tpl.php
@@ -17,7 +17,7 @@
 
 	<!-- Modal -->
 	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	  <div class="modal-dialo modal-lg" role="document">
+	  <div class="modal-dialog modal-lg" role="document">
 	    <div class="modal-content">
 	      <div class="modal-header">
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -34,14 +34,14 @@
 				  	// print_r($variables);
 
 				  	print_r($node);
-				  	$path = $directory.'/sites/all/themes/utec_theme/assets/img/';
+				  	$path_img = $directory.'/sites/all/themes/utec_theme/assets/img/';
 				  ?>
 	        <?php $path_i18 = drupal_get_path_alias('', $language->language); ?>
 	        <div class="carlos"><?php print $variables['page']['content']['metatags']['global']['og:title']; ?></div>
-	        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>"><img src="<?php print $path.'facebook.png' ?>"></a>
-	      	<a href="https://twitter.com/home?status=<?php print $og_description ?>"><img src="<?php print $path.'twitter.png' ?>"></a>
-	      	<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>&title=<?php print $og_title ?>&summary=&source="><img src="<?php print $path.'linkedin.png' ?>"></a>
-	      	<a href="https://plus.google.com/share?url=<?php print $url ?>"><img src="<?php print $path.'gplus.png' ?>"></a>
+	        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>"><img src="<?php print $path_img.'facebook.png' ?>"></a>
+	      	<a href="https://twitter.com/home?status=<?php print $og_description ?>"><img src="<?php print $path_img.'twitter.png' ?>"></a>
+	      	<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>&title=<?php print $og_title ?>&summary=&source="><img src="<?php print $path_img.'linkedin.png' ?>"></a>
+	      	<a href="https://plus.google.com/share?url=<?php print $url ?>"><img src="<?php print $path_img.'gplus.png' ?>"></a>
 	      </div>
 	      <div class="modal-footer pb-42">
 	        <a href="#" class="text-gray mh-35 text-uppercase">Lorem ipsum dolor</a>
