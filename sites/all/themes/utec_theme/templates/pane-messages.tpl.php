a2a5c86799317cbfaff152e35bb795652e6ef8b3
MercedesZubieta <mercedes.zubieta@gmail.com>
2015-10-20 14:24:04 -0500

    cambios de ruta de las imagenes

--- sites/all/themes/utec_theme/templates/pane-messages.tpl.php
+++ sites/all/themes/utec_theme/templates/pane-messages.tpl.php
@@ -17,15 +17,16 @@
 
 	<!-- Modal -->
 	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	  <div class="modal-dialog" role="document">
+	  <div class="modal-dialo modal-lg" role="document">
 	    <div class="modal-content">
 	      <div class="modal-header">
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	        <h1 class="modal-title medium text-center" id="myModalLabel">Thanks for contacting us</h1>
+	        <h1 class="modal-title medium text-center mt-56 lead" id="myModalLabel">Thanks for contacting us</h1>
 	      </div>
-	      <div class="modal-body text-center">
-	      	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+	      <div class="modal-body text-center php-21">
+	      	<p class="mb-21">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 	      	<a href="#" class="text-primary">Back to homepage</a>
+	      	<h4 class="text-center mv-21 medium">Share your experience</h4>
 				  <?php 
 				  	$node = node_load(1382);
 				  	$title = $node->title;
@@ -33,16 +34,20 @@
 				  	// print_r($variables);
 
 				  	print_r($node);
+				  	$path = $directory.'/sites/all/themes/utec_theme/assets/img/';
 				  ?>
 	        <?php $path_i18 = drupal_get_path_alias('', $language->language); ?>
 	        <div class="carlos"><?php print $variables['page']['content']['metatags']['global']['og:title']; ?></div>
-	        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>"><img src="$directory/sites/all/themes/utec_theme/assets/img/facebook.png"></a>
-	      	<a href="https://twitter.com/home?status=<?php print $og_description ?>"><img src="$directory/sites/all/themes/utec_theme/assets/img/twitter.png"></a>
-	      	<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>&title=<?php print $og_title ?>&summary=&source="><img src="$directory/sites/all/themes/utec_theme/assets/img/linkedin.png"></a>
-	      	<a href="https://plus.google.com/share?url=<?php print $url ?>"><img src="$directory/sites/all/themes/utec_theme/assets/img/gplus.png"></a>
+	        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>"><img src="<?php print $path.'facebook.png' ?>"></a>
+	      	<a href="https://twitter.com/home?status=<?php print $og_description ?>"><img src="<?php print $path.'twitter.png' ?>"></a>
+	      	<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>&title=<?php print $og_title ?>&summary=&source="><img src="<?php print $path.'linkedin.png' ?>"></a>
+	      	<a href="https://plus.google.com/share?url=<?php print $url ?>"><img src="<?php print $path.'gplus.png' ?>"></a>
 	      </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	      <div class="modal-footer pb-42">
+	        <a href="#" class="text-gray mh-35 text-uppercase">Lorem ipsum dolor</a>
+	        <a href="#" class="text-gray mh-35 text-uppercase">Lorem ipsum dolor</a>
+	        <a href="#" class="text-gray mh-35 text-uppercase">Lorem ipsum dolor</a>
+
 	      </div>
 	    </div>
 	  </div>
