cd9163f0e99d0f1c522e8ffd8b199a02efdbede1
carlos calderon <cadermino@gmail.com>
2015-10-21 11:43:35 -0500

    metatags

--- sites/all/themes/utec_theme/templates/pane-messages.tpl.php
+++ sites/all/themes/utec_theme/templates/pane-messages.tpl.php
@@ -10,6 +10,16 @@
 
 <?php if (!empty($messages)): ?>
   <?php //print $messages; ?>
+  <?php 
+  	$node = node_load(1382);
+  	$title = $node->title;
+  	$description = $node->field_link['und']['0']['value'];
+  	$twitter = $node->field_link['und']['0']['value'];
+  	// print_r($variables);
+
+  	// kpr($node);
+  	$path_img = $directory.'/assets/img/';
+  ?>
 	<!-- Button trigger modal -->
 	<button style="display:none" id="button" type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
 	  Launch demo modal
@@ -21,25 +31,16 @@
 	    <div class="modal-content">
 	      <div class="modal-header">
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	        <h1 class="modal-title medium text-center mt-56 lead" id="myModalLabel">Thanks for contacting us</h1>
+	        <h1 class="modal-title medium text-center mt-56 lead" id="myModalLabel"><?php print $title; ?></h1>
 	      </div>
 	      <div class="modal-body text-center php-21">
-	      	<p class="mb-21">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+	      	<p class="mb-21"><?php print $description; ?></p>
 	      	<a href="#" class="text-primary">Back to homepage</a>
 	      	<h4 class="text-center mv-21 medium">Share your experience</h4>
-				  <?php 
-				  	$node = node_load(1382);
-				  	$title = $node->title;
-				  	$description = $node->body['und']['0']['value'];
-				  	// print_r($variables);
-
-				  	kpr($node);
-				  	$path_img = $directory.'/sites/all/themes/utec_theme/assets/img/';
-				  ?>
+				  
 	        <?php $path_i18 = drupal_get_path_alias('', $language->language); ?>
-	        <div class="carlos"><?php print $variables['page']['content']['metatags']['global']['og:title']; ?></div>
 	        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>"><img src="<?php print $path_img.'facebook.png' ?>"></a>
-	      	<a href="https://twitter.com/home?status=<?php print $og_description ?>"><img src="<?php print $path_img.'twitter.png' ?>"></a>
+	      	<a href="https://twitter.com/home?status=<?php print $twitter; ?>"><img src="<?php print $path_img.'twitter.png' ?>"></a>
 	      	<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php print $url.'/'.$language->language.'/'.$path_i18; ?>&title=<?php print $og_title ?>&summary=&source="><img src="<?php print $path_img.'linkedin.png' ?>"></a>
 	      	<a href="https://plus.google.com/share?url=<?php print $url ?>"><img src="<?php print $path_img.'gplus.png' ?>"></a>
 	      </div>
