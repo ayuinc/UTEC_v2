49ec2e7ebaf73afa9bce12e3b7c42de318416ecc
carlos calderon <cadermino@gmail.com>
2015-10-23 17:04:18 -0500

    testimonios centrados

--- sites/all/themes/utec_theme/js/app/lib/utec-convenios.js
+++ sites/all/themes/utec_theme/js/app/lib/utec-convenios.js
@@ -9,56 +9,56 @@
 
   $(".panel-group a").off("click");
 	$(".panel-group a").unbind("click");
-  
 
-	document.getElementById("webform-client-form-1353").onsubmit=function() { // needs an ID
-    
-    var val = document.getElementById('edit-submitted-name'); // 'this' is the form 
-    var val2 = document.getElementById('edit-submitted-subject');
-    var val3 = document.getElementById('edit-submitted-messaje');
-    var htmlString = "<span id='message-err'>Text required</span>";
-    if (val.value==null || val.value.trim()=="") { 
-	  	var text = document.getElementById('message-err');
-    	if (text != null) {
-	    	text.parentNode.removeChild(text);
-    	}
-      val.insertAdjacentHTML('afterend', htmlString);
-      val.focus();
-      return false; // cancel submission
+  var form = document.getElementById("webform-client-form-1353");
+  if (form!=null) {
+  	document.getElementById("webform-client-form-1353").onsubmit=function() { // needs an ID
+      var val = document.getElementById('edit-submitted-name'); // 'this' is the form 
+      var val2 = document.getElementById('edit-submitted-subject');
+      var val3 = document.getElementById('edit-submitted-messaje');
+      var htmlString = "<span id='message-err'>Text required</span>";
+      if (val.value==null || val.value.trim()=="") { 
+  	  	var text = document.getElementById('message-err');
+      	if (text != null) {
+  	    	text.parentNode.removeChild(text);
+      	}
+        val.insertAdjacentHTML('afterend', htmlString);
+        val.focus();
+        return false; // cancel submission
+      }
+      var text = document.getElementById('message-err');
+      if (text != null) {
+  	    text.parentNode.removeChild(text);
+  	  }
+      if (val2.value==null || val2.value.trim()=="") { 
+      	var text2 = document.getElementById('message-err');
+      	if (text2 != null) {
+  	    	text2.parentNode.removeChild(text2);
+  	    }
+        val2.insertAdjacentHTML('afterend', htmlString);
+        val2.focus();
+        return false; // cancel submission
+      }
+      var text2 = document.getElementById('message-err');
+      if (text2 != null) {
+  	    text2.parentNode.removeChild(text2);
+  	  }
+      if (val3.value==null || val3.value.trim()=="") { 
+      	var text3 = document.getElementById('message-err');
+      	if (text3 != null) {
+  	    	text3.parentNode.removeChild(text3);
+  	    }
+        val3.insertAdjacentHTML('afterend', htmlString);
+        val3.focus();
+        return false; // cancel submission
+      }
+      var text3 = document.getElementById('message-err');
+      if (text3 != null) {
+  	    text3.parentNode.removeChild(text3);
+  	  }
+      return true; // allow submit
     }
-    var text = document.getElementById('message-err');
-    if (text != null) {
-	    text.parentNode.removeChild(text);
-	  }
-    if (val2.value==null || val2.value.trim()=="") { 
-    	var text2 = document.getElementById('message-err');
-    	if (text2 != null) {
-	    	text2.parentNode.removeChild(text2);
-	    }
-      val2.insertAdjacentHTML('afterend', htmlString);
-      val2.focus();
-      return false; // cancel submission
-    }
-    var text2 = document.getElementById('message-err');
-    if (text2 != null) {
-	    text2.parentNode.removeChild(text2);
-	  }
-    if (val3.value==null || val3.value.trim()=="") { 
-    	var text3 = document.getElementById('message-err');
-    	if (text3 != null) {
-	    	text3.parentNode.removeChild(text3);
-	    }
-      val3.insertAdjacentHTML('afterend', htmlString);
-      val3.focus();
-      return false; // cancel submission
-    }
-    var text3 = document.getElementById('message-err');
-    if (text3 != null) {
-	    text3.parentNode.removeChild(text3);
-	  }
-    return true; // allow submit
-  }
-	
+	}
 }(jQuery));
 // add trim to older IEs
 if (!String.trim) {
