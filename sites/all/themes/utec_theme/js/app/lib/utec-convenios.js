77d0b3e35b3efcc581c09a1eb8068ce50412fd59
carlos calderon <cadermino@gmail.com>
2015-10-22 19:05:13 -0500

    validación formulario contacto

--- sites/all/themes/utec_theme/js/app/lib/utec-convenios.js
+++ sites/all/themes/utec_theme/js/app/lib/utec-convenios.js
@@ -16,28 +16,30 @@
 	  $(this).find('.down').toggleClass("hide"); 
 	});
 	
-	  $("webform-client-form-1353").onsubmit=function() { // needs an ID
-	    alert(this.submitted[name]);
-	    var val = this.submitted[name].value; // 'this' is the form 
-	    var val2 = this.submitted[subject].value;
-	    var val3 = this.submitted[messaje].value;
-	    
-	    if (val==null || val.trim()=="") { 
-	      alert('Please enter something');
-	      this.submitted[name].focus();
-	      return false; // cancel submission
-	    }
-	    if (val2==null || val2.trim()=="") { 
-	      alert('Please enter something');
-	      this.submitted[subject].focus();
-	      return false; // cancel submission
-	    }
-	    if (val3==null || val3.trim()=="") { 
-	      alert('Please enter something');
-	      this.submitted[messaje].focus();
-	      return false; // cancel submission
-	    }
-	    return true; // allow submit
-	  }
 	
 }(jQuery));
+window.onload=function() {
+  document.getElementById("webform-client-form-1353").onsubmit=function() { // needs an ID
+    alert(this.submitted[name]);
+    var val = this.submitted[name].value; // 'this' is the form 
+    var val2 = this.submitted[subject].value;
+    var val3 = this.submitted[messaje].value;
+    
+    if (val==null || val.trim()=="") { 
+      alert('Please enter something');
+      this.submitted[name].focus();
+      return false; // cancel submission
+    }
+    if (val2==null || val2.trim()=="") { 
+      alert('Please enter something');
+      this.submitted[subject].focus();
+      return false; // cancel submission
+    }
+    if (val3==null || val3.trim()=="") { 
+      alert('Please enter something');
+      this.submitted[messaje].focus();
+      return false; // cancel submission
+    }
+    return true; // allow submit
+  }
+}
