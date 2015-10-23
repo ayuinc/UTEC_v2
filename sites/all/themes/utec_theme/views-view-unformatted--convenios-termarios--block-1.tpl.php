a99956562ffc56edc9c4b970d1c281bc06de06e5
carlos calderon <cadermino@gmail.com>
2015-10-23 15:03:02 -0500

    temarios

--- sites/all/themes/utec_theme/views-view-unformatted--convenios-termarios--block-1.tpl.php
+++ sites/all/themes/utec_theme/views-view-unformatted--convenios-termarios--block-1.tpl.php
@@ -15,7 +15,7 @@
 		<div class="table-responsive table-custom">
 			<table class="table">
 				<thead>
-					<tr>
+					<tr class="text-center">
 						<td><?php print t('Semester') ?></td>
 						<td><?php print t('Career') ?></td>
 						<td><?php print t('Courses') ?></td>
@@ -33,7 +33,7 @@
 					<?php foreach ($rows as $id => $row): ?>
 				    <?php $total += $view->result[$id]->field_field_n_mero_creditos['0']['rendered']['#markup'];?>
 					<?php endforeach; ?>
-					<tr>
+					<tr class="text-center">
 			      <td><?php print t('Total credits') ?></td>
 			      <td></td>
 			      <td></td>
