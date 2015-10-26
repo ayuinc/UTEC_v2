0e7375a8a16ee9bc4d7a712e0f6bb14cbc7ad400
MercedesZubieta <mercedes.zubieta@gmail.com>
2015-10-26 08:49:34 -0500

    camios de responsive modal 3

--- sites/all/themes/utec_theme/templates/pane-messages.tpl.php
+++ sites/all/themes/utec_theme/templates/pane-messages.tpl.php
@@ -33,7 +33,7 @@
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 	        <h1 class="modal-title medium text-center mt-56 lead" id="myModalLabel"><?php print $title; ?></h1>
 	      </div>
-	      <div class="modal-body text-center php-21">
+	      <div class="modal-body text-center php-21 mb-42">
 	      	<p class="mb-21"><?php print $description; ?></p>
 	      	<a href="/en" class="text-primary">Back to homepage</a>
 	      	<h4 class="text-center mv-21 medium">Share your experience</h4>
